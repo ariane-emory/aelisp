5acf69736aa9576e854ba557e198c89b30584b6a
Ariane Emory <ariane.emory@protonmail.com>
2023-09-06 14:44:45 -0400

    ...

--- src/mylang-data.c
+++ src/mylang-data.c
@@ -6,13 +6,15 @@ void mylang_object_init(mylang_object_t * const mylang_object) {
   mylang_object->c_str = 0;
 }
 
+char * mylang_object_print(const mylang_object_t * const mylang_object) {
 #define BUFF_LEN 256
 
 static char buff[BUFF_LEN] = {0};
 
-char * mylang_object_print(const mylang_object_t * const mylang_object) {
-  printf(
-    "('%s', '%s')\n",
+  snprintf(
+    buff,
+    BUFF_LEN,
+    "('%s', '%s')",
     mylang_type_name(mylang_object->type),
     mylang_object->c_str
   );
