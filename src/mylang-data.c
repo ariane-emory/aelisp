164276e762af16db527e181d8efe0a7db4fd3442
Ariane Emory <ariane.emory@protonmail.com>
2023-09-06 14:29:09 -0400

    ...

--- src/mylang-data.c
+++ src/mylang-data.c
@@ -8,16 +8,17 @@ void mylang_object_init(mylang_object_t * const mylang_object) {
 
 void mylang_object_print(const mylang_object_t * const mylang_object) {
   printf(
-    "(%d, %s, \"%s\")\n",
-    mylang_object->type,
+    "('%s', \"%s\")\n",
     mylang_type_name(mylang_object->type),
     mylang_object->c_str
   );
 }
 
+#define return_str(x) case x: return #x;
+
 const char * mylang_type_name(const mylang_type_t mylang_type) {
   switch (mylang_type) {
-  default:
-    return "Unrecognized type";
+    FOR_LEXED_TYPES_DO(return_str);
+  default: return "UNRECOGNIZED!";
   }
 }
