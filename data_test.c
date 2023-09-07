30c755b92fd88ebf504973f0a93616817d942560
Ariane Emory <ariane.emory@protonmail.com>
2023-09-07 15:49:50 -0400

    ...

--- data_test.c
+++ data_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 #include "ae.h"
 
@@ -8,15 +9,15 @@ static char * one   = "One";
 static char * two   = "Two";
 static char * three = "Three";
 
-void print_ae_object_str(void * ae_list_node_p) {
-  ae_list_node_t * ae_list_node = ae_list_node_p;
-  // printf("%s\n", ae_object_str(ae_list_node->object));
-  printf("Iterate node 0x%x.\n", ae_list_node);
+void print_ae_object_str(void * ae_object_p) {
+  ae_object_t * ae_object = ae_object_p;
+  // printf("Iterate object 0x%x.\n", ae_object);
+  printf("%s\n", ae_object_str(ae_object));
 }
 
 
 int main() {
-  ae_list_t list = 0; // malloc(sizeof(ae_list_t));
+  ae_list_t list = 0;
   ae_list_init(&list);
 
   {
@@ -24,7 +25,8 @@ int main() {
     ae_object_init(obj);
     obj->c_str = zero;
     // printf("%s\n", ae_object_str(obj));
-    printf("Added node 0x%x.\n", ae_list_push_back(&list, obj));
+    ae_list_node_t * new_tail = ae_list_push_back(&list, obj);
+    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
   
   {
@@ -32,7 +34,8 @@ int main() {
     ae_object_init(obj);
     obj->c_str = one;
     // printf("%s\n", ae_object_str(obj));
-    printf("Added node 0x%x.\n", ae_list_push_back(&list, obj));
+    ae_list_node_t * new_tail = ae_list_push_back(&list, obj);
+    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
   {
@@ -40,7 +43,8 @@ int main() {
     ae_object_init(obj);
     obj->c_str = two;
     // printf("%s\n", ae_object_str(obj));
-    printf("Added node 0x%x.\n", ae_list_push_back(&list, obj));
+    ae_list_node_t * new_tail = ae_list_push_back(&list, obj);
+    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
   {
@@ -48,11 +52,18 @@ int main() {
     ae_object_init(obj);
     obj->c_str = three;
     // printf("%s\n", ae_object_str(obj));
-    printf("Added node 0x%x.\n", ae_list_push_back(&list, obj));
+    ae_list_node_t * new_tail = ae_list_push_back(&list, obj);
+    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
-  /* for (ae_list_node_t * position = list; position; position = position->tail) */
-  /*   printf("%s\n", ae_object_str(position->object)); */
-
   ae_list_node_each(list, print_ae_object_str);
+
+  ae_list_t num_list = 0;
+  ae_list_init(&num_list);
+
+  for (int ix = 3; ix < 10; ix++) {
+    int * new_num = malloc(sizeof(int));
+    *new_num = ix;
+    ae_list_push_back(&num_list, new_num);
+  }
 }
