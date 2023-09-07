f24eaf218d1d7c6ed936f2b7e7ac250df4e6aa0f
Ariane Emory <ariane.emory@protonmail.com>
2023-09-07 13:56:48 -0400

    ...

---
+++ include/ae_object.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ae_type.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Object type
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct ae_object_t {
+  ae_type_t                 type;
+  char *                    c_str;
+  ae_data_t                 data;
+} ae_object_t;
+
+//------------------------------------------------------------------------------
+
+void               ae_object_init(ae_object_t * const ae_object);
+const char * const ae_object_str(const ae_object_t * const ae_object);
