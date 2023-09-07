47665c3e0d6097c5fddd5f7b3da903c46a0ab506
Ariane Emory <ariane.emory@protonmail.com>
2023-09-07 17:27:27 -0400

    ...

--- include/ae_list.h
+++ include/ae_list.h
@@ -17,15 +17,15 @@ typedef ae_list_node_t * ae_list_t;
 //------------------------------------------------------------------------------
 
 void               ae_list_init          (      ae_list_t * const this);
-const char * const ae_list_str           (const ae_list_t * const this);
-ae_list_node_t *   ae_list_push_back     (      ae_list_t * const this, void * const object);
 size_t             ae_list_length        (const ae_list_t * const this);
+ae_list_node_t *   ae_list_push_back     (      ae_list_t * const this, void * const object);
+const char * const ae_list_str           (const ae_list_t * const this);
 void               ae_list_each          (      ae_list_t * const this, ae_list_node_each_fun fun);
 ae_list_t          ae_list_map           (const ae_list_t * const this, ae_list_node_map_fun fun);
 void               ae_list_map_into_from (      ae_list_t * const this, const ae_list_t * const that, ae_list_node_map_fun fun);
 
 void               ae_list_node_init     (      ae_list_node_t * const this);
-const char * const ae_list_node_str      (const ae_list_node_t * const this);
-ae_list_node_t *   ae_list_node_push_back(      ae_list_node_t * const this, void * const object);
 size_t             ae_list_node_length   (const ae_list_node_t * const this);
+ae_list_node_t *   ae_list_node_push_back(      ae_list_node_t * const this, void * const object);
+const char * const ae_list_node_str      (const ae_list_node_t * const this);
 void               ae_list_node_each     (      ae_list_node_t * const this, ae_list_node_each_fun fun);
