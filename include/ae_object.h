8e0f51b16b3f4f8c39069d070373515ea0ef220d
Ariane Emory <ariane.emory@protonmail.com>
2023-09-08 20:52:06 -0400

    ...

--- include/ae_object.h
+++ include/ae_object.h
@@ -19,5 +19,9 @@ void               ae_object_init(ae_object_t * const this);
 const char * const ae_object_str(const ae_object_t * const this);
 void               ae_object_move(ae_object_t * const this, ae_object_t * const that);
 ae_object_t *      ae_object_clone(ae_object_t * const this);
+// ugly putses:
 void               ae_object_fputs(const ae_object_t * const this, FILE * stream);
 void               ae_object_puts(const ae_object_t * const this);
+// pretty putses:
+void               ae_object_fputsp(const ae_object_t * const this, FILE * stream);
+void               ae_object_putsp(const ae_object_t * const this);
