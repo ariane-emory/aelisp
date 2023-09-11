269dfbb8d13e241513b34dec9166ea9cbf1df4f4
Ariane Emory <ariane.emory@protonmail.com>
2023-09-11 18:43:59 -0400

    ...

--- data_test.c
+++ data_test.c
@@ -42,36 +42,28 @@ int main() {
     ae_obj_t * obj = malloc(sizeof(ae_obj_t));
     ae_obj_init(obj, AE_INTEGER);
     obj->c_str = zero;
-    // printf("%s\n", ae_obj_str(obj));
     ae_list_node_t * new_tail = ae_list_push_back(&object->list_value, obj);
-    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
   
   {
     ae_obj_t * obj = malloc(sizeof(ae_obj_t));
     ae_obj_init(obj, AE_INTEGER);
     obj->c_str = one;
-    // printf("%s\n", ae_obj_str(obj));
     ae_list_node_t * new_tail = ae_list_push_back(&object->list_value, obj);
-    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
   {
     ae_obj_t * obj = malloc(sizeof(ae_obj_t));
     ae_obj_init(obj, AE_INTEGER);
     obj->c_str = two;
-    // printf("%s\n", ae_obj_str(obj));
     ae_list_node_t * new_tail = ae_list_push_back(&object->list_value, obj);
-    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
   {
     ae_obj_t * obj = malloc(sizeof(ae_obj_t));
     ae_obj_init(obj, AE_INTEGER);
     obj->c_str = three;
-    // printf("%s\n", ae_obj_str(obj));
     ae_list_node_t * new_tail = ae_list_push_back(&object->list_value, obj);
-    // printf("Added node 0x%x containing 0x%x.\n", new_tail, new_tail->object);
   }
 
   ae_list_each(&object->list_value, print_ae_obj_str);
@@ -81,21 +73,4 @@ int main() {
   printf("\nPrint clone\n");
 
   ae_list_each(&clone->list_value, print_ae_obj_str);
-  
-  /* ae_list_t num_list = 0; */
-
-  /* for (int ix = 3; ix < 10; ix++) { */
-  /*   int * new_num = malloc(sizeof(int)); */
-  /*   *new_num = ix; */
-  /*   ae_list_push_back(&num_list, new_num); */
-  /* } */
-
-  /* ae_list_each(&num_list, print_int_p); */
-  /* printf("\n"); */
-  /* ae_list_t doubled_list = ae_list_map(&num_list, double_int_p); */
-  /* ae_list_map_into_from(&doubled_list, &num_list, id_int_p); */
-  /* ae_list_each(&doubled_list, print_int_p); */
-                                             
-  /* printf("Length: %d\n", ae_list_node_length(doubled_list)); */
-  /* printf("Length: %d\n", ae_list_length(&doubled_list)); */
 }
