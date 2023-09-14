4ad4f581d771dbc46399086a49701c669da3d4c1
Ariane Emory <ariane.emory@protonmail.com>
2023-09-14 18:04:02 -0400

    ...

--- include/ae_obj_pool.h
+++ include/ae_obj_pool.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "ae_obj.h"
-
 ////////////////////////////////////////////////////////////////////////////////
 // pool
 ////////////////////////////////////////////////////////////////////////////////
@@ -12,6 +10,6 @@ extern const struct ae_obj_t * const pool_first;
 extern const struct ae_obj_t * const pool_last;
 
 struct ae_obj_t * pool_alloc_ae_obj();
-void       pool_free_ae_obj(struct ae_obj_t * const this);
-void       pool_print(void);
-void       pool_clear(void);
+void              pool_free_ae_obj(struct ae_obj_t * const this);
+void              pool_print(void);
+void              pool_clear(void);
