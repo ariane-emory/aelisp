e5d9fb86d46fe99d82feb3a736cf2531bd86026f
Ariane Emory <ariane.emory@protonmail.com>
2023-09-14 13:32:43 -0400

    ...

--- include/ae_obj.h
+++ include/ae_obj.h
@@ -5,15 +5,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #define ALLOC                 (pool_alloc_ae_obj())
-#define NEW(type)             (ae_obj_init(ALLOC, (type)))
+#define NEW(type)             (ae_obj_init((ALLOC), (type)))
 #define INIT(this, type)      (ae_obj_init((this), (type)))
-#define MOVE_NEW(that)        (ae_obj_unsafe_move(ALLOC, (that)))
+#define MOVE_NEW(that)        (ae_obj_unsafe_move((ALOC), (that)))
 #define PUSH_BACK(this, that) (ae_obj_push_back((this), (that)))
+#define INTERN(sym_list, str) (ae_obj_string_intern((sym_list), (str)))
+#define CONS(head, tail)      (ae_obj_cons((head), (tail)))
 #define CAR(this)             ((this)->head)
 #define CDR(this)             ((this)->tail)
 #define CADR(this)            (CAR(CDR(this)))
-#define CONS(head, tail)      (ae_obj_cons((head), (tail)))
-#define INTERN(sym_list, str) (ae_obj_string_intern((sym_list), (str)))
 
 #define INTEGERP(o)           ((o)->type == AE_INTEGER_)
 #define INVALIDP(o)           ((o)->type == AE_INVALID_)
