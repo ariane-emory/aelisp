6f349590c6d20832e1d99b064e7011289799035b
Ariane Emory <ariane.emory@protonmail.com>
2023-09-14 17:48:22 -0400

    separate pool

--- include/ae_obj.h
+++ include/ae_obj.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ae_obj_pool.h>
+
 ////////////////////////////////////////////////////////////////////////////////
 // convenience macros
 ////////////////////////////////////////////////////////////////////////////////
@@ -15,29 +17,31 @@
 #define CDR(this)             ((this)->tail)
 #define CADR(this)            (CAR(CDR(this)))
 
+#define ATOMP(o)              ((o)->type >= AE_INVALID_)
+#define CHARP(o)              ((o)->type == AE_CHAR____)
+#define CONSP(o)              ((o)->type == AE_CONS____)
+#define FLOATP(o)             ((o)->type == AE_FLOAT___)
+#define INFP(o)               ((o)->type == AE_INF_____)
 #define INTEGERP(o)           ((o)->type == AE_INTEGER_)
 #define INVALIDP(o)           ((o)->type == AE_INVALID_)
-#define CHARP(o)              ((o)->type == AE_CHAR____)
-#define FLOATP(o)             ((o)->type == AE_FLOAT___)
-#define CONSP(o)              ((o)->type == AE_CONS____)
 #define LPARENP(o)            ((o)->type == AE_LPAREN__)
-#define RPARENP(o)            ((o)->type == AE_RPAREN__)
 #define QUOTEP(o)             ((o)->type == AE_QUOTE___)
 #define RATIONALP(o)          ((o)->type == AE_RATIONAL)
-#define INFP(o)               ((o)->type == AE_INF_____)
+#define RPARENP(o)            ((o)->type == AE_RPAREN__)
 #define STRINGP(o)            ((o)->type == AE_STRING__)
 #define SYMBOLP(o)            ((o)->type == AE_SYMBOL__)
 
+#define ASSERT_ATOMP(o)       ((o)->type >= AE_INVALID_)
+#define ASSERT_CHARP(o)       (assert(CHARP(o)))
+#define ASSERT_CONSP(o)       (assert((CONSP(o))))
+#define ASSERT_FLOATP(o)      (assert(FLOATP(o)))
+#define ASSERT_INFP(o)        (assert(INFP(o)))
 #define ASSERT_INTEGERP(o)    (assert(INTEGERP(o)))
 #define ASSERT_INVALIDP(o)    (assert(INVALIDP(o)))
-#define ASSERT_CHARP(o)       (assert(CHARP(o)))
-#define ASSERT_FLOATP(o)      (assert(FLOATP(o)))
-#define ASSERT_CONSP(o)       (assert((CONSP(o))))
 #define ASSERT_LPARENP(o)     (assert(LPARENP(o)))
-#define ASSERT_RPARENP(o)     (assert(RPARENP(o)))
 #define ASSERT_QUOTEP(o)      (assert(QUOTEP(o)))
 #define ASSERT_RATIONALP(o)   (assert(RATIONALP(o)))
-#define ASSERT_INFP(o)        (assert(INFP(o)))
+#define ASSERT_RPARENP(o)     (assert(RPARENP(o)))
 #define ASSERT_STRINGP(o)     (assert(STRINGP(o)))
 #define ASSERT_SYMBOLP(o)     (assert(SYMBOLP(o)))
 
@@ -68,19 +72,19 @@ typedef char * ae_string_t;
 // Types enum
 ////////////////////////////////////////////////////////////////////////////////
 
-#define FOR_EACH_LEXED_TYPE(DO)                                                                                                            \
-  DO(AE_INTEGER_)                                                                                                                           \
+#define FOR_EACH_LEXED_TYPE(DO)                                                                                                             \
+  DO(AE_CONS____)                                                                                                                           \
   DO(AE_INVALID_)                                                                                                                           \
   DO(AE_CHAR____)                                                                                                                           \
-  DO(AE_FLOAT___)                                                                                                                           \
-  DO(AE_CONS____)                                                                                                                           \
-  DO(AE_LPAREN__)                                                                                                                           \
-  DO(AE_RPAREN__)                                                                                                                           \
-  DO(AE_QUOTE___)                                                                                                                           \
+  DO(AE_INTEGER_)                                                                                                                           \
   DO(AE_RATIONAL)                                                                                                                           \
+  DO(AE_FLOAT___)                                                                                                                           \
   DO(AE_INF_____)                                                                                                                           \
   DO(AE_STRING__)                                                                                                                           \
-  DO(AE_SYMBOL__)                                                                                                                             
+  DO(AE_SYMBOL__)                                                                                                                           \
+  DO(AE_LPAREN__)                                                                                                                           \
+  DO(AE_RPAREN__)                                                                                                                           \
+  DO(AE_QUOTE___)
 
 #define enum_node(x) x,
 
@@ -120,7 +124,7 @@ typedef struct ae_obj_t {
 } ae_obj_t; // __attribute__((packed)) ae_obj_t;
 
 //------------------------------------------------------------------------------
-// Obj'ss methods
+// Obj's methods
 //------------------------------------------------------------------------------
 
 ae_obj_t *    ae_obj_init         (      ae_obj_t * const this,        ae_type_t type);
@@ -149,17 +153,3 @@ ae_obj_t *    ae_obj_cons         (      ae_obj_t * const head,        ae_obj_t 
 // Intern
 ae_obj_t *    ae_obj_string_intern(      ae_obj_t ** const sym_list_p, ae_string_t string );
 
-////////////////////////////////////////////////////////////////////////////////
-// pool
-////////////////////////////////////////////////////////////////////////////////
-
-#define POOL_SIZE (1 << 7)
-
-extern ae_obj_t               pool[POOL_SIZE];
-extern const ae_obj_t * const pool_first;
-extern const ae_obj_t * const pool_last;
-
-ae_obj_t * pool_alloc_ae_obj();
-void       pool_free_ae_obj(ae_obj_t * const this);
-void       pool_print(void);
-void       pool_clear(void);
