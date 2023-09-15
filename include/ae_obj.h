efc0740deaf6613c7e0741accf5725cc98284760
Ariane Emory <ariane.emory@protonmail.com>
2023-09-14 21:15:04 -0400

    ...

--- include/ae_obj.h
+++ include/ae_obj.h
@@ -137,15 +137,15 @@ typedef struct ae_obj_t {
 // Obj's methods
 ////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t *    ae_obj_init          (      ae_obj_t * const this,        ae_type_t type);
+ae_obj_t *    ae_obj_init          (      ae_obj_t * const this,        ae_type_t        type);
 ae_obj_t *    ae_obj_unsafe_move   (      ae_obj_t * const this,        ae_obj_t * const that);
 ae_obj_t *    ae_obj_clone         (const ae_obj_t * const this);
 
 // ugly putses:
-void          ae_obj_fput          (const ae_obj_t * const this,        FILE * stream);
+void          ae_obj_fput          (const ae_obj_t * const this,        FILE *           stream);
 void          ae_obj_put           (const ae_obj_t * const this);
 // byte-oriented putses:
-void          ae_obj_fput_bytes    (const ae_obj_t * const this,        FILE * stream);
+void          ae_obj_fput_bytes    (const ae_obj_t * const this,        FILE *           stream);
 void          ae_obj_put_bytes     (const ae_obj_t * const this);
 // write:
 void          ae_obj_fwrite        (const ae_obj_t * const this,        FILE * stream);
@@ -158,6 +158,6 @@ ae_obj_t *    ae_list_remove_member(      ae_obj_t * const list,        ae_obj_t
 bool          ae_list_has_member   (      ae_obj_t * const list,        ae_obj_t * const  member);
 void          ae_list_each         (      ae_obj_t * const list,        ae_list_each_fun  fun);
 ae_obj_t *    ae_list_map          (      ae_obj_t * const list,        ae_list_map_fun   fun);
-ae_obj_t *    ae_obj_cons          (      ae_obj_t * const head,        ae_obj_t *  const tail);
-ae_obj_t *    ae_list_string_intern(      ae_obj_t ** const sym_list_p, ae_string_t string );
+ae_obj_t *    ae_obj_cons          (      ae_obj_t * const head,        ae_obj_t * const  tail);
+ae_obj_t *    ae_list_string_intern(      ae_obj_t ** const sym_list_p, ae_string_t       string );
 
