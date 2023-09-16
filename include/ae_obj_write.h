b06b17ccbb2575d441d5da7596ce210c862f75c6
Ariane Emory <ariane.emory@protonmail.com>
2023-09-15 22:29:43 -0400

    ...

--- include/ae_obj_write.h
+++ include/ae_obj_write.h
@@ -17,13 +17,15 @@
 // Obj's methods writing / printing methods
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void          ae_obj_write         (const ae_obj_t *  const this                                  );
-void          ae_obj_put           (const ae_obj_t *  const this                                  );
-void          ae_obj_put_bytes     (const ae_obj_t *  const this                                  );
 char *        ae_obj_swrite        (const ae_obj_t *  const this                                  );
-char *        ae_obj_sput          (const ae_obj_t *  const this                                  );
-char *        ae_obj_sput_bytes    (const ae_obj_t *  const this                                  );
 void          ae_obj_fwrite        (const ae_obj_t *  const this,  FILE *            stream       );
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void          ae_obj_put           (const ae_obj_t *  const this                                  );
+char *        ae_obj_sput          (const ae_obj_t *  const this                                  );
 void          ae_obj_fput          (const ae_obj_t *  const this,  FILE *            stream       );
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void          ae_obj_put_bytes     (const ae_obj_t *  const this                                  );
 void          ae_obj_fput_bytes    (const ae_obj_t *  const this,  FILE *            stream       );
+char *        ae_obj_sput_bytes    (const ae_obj_t *  const this                                  );
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
