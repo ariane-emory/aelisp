06db5c8dd91205decf406c6e6bbacb149a7bf827
Ariane Emory <ariane.emory@protonmail.com>
2023-09-16 00:09:17 -0400

    ...

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ( (c-backslash-column . 99)
-          (c-backslash-column . 99)))
-  (c-mode . ((look-at-me . "something"))))
+          (c-backslash-column . 99))))
