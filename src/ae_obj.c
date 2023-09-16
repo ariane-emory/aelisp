f2cccd1fd200be792f5457a3088b49e72054659c
Ariane Emory <ariane.emory@protonmail.com>
2023-09-16 04:11:21 -0400

    initializer not constant

--- src/ae_obj.c
+++ src/ae_obj.c
@@ -44,7 +44,14 @@ bool ae_obj_equal (const ae_obj_t * const this,  const ae_obj_t *  const that) {
 // _init method
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t * ae_obj_truth (const bool this);
+ae_obj_t * ae_obj_truth (const bool this) {
+  static ae_obj_t obj_true  = (ae_obj_t){ .type = AE_INTEGER_, .int_val = 1               };
+  static ae_obj_t obj_false = (ae_obj_t){ .type = AE_CONS____, .head = NULL, .tail = NULL };
+
+  (void)this;
+  
+  return NULL;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // _init method
