a53aa0f0269beb62bb75d75affbe4a99b0e83b70
Ariane Emory <ariane.emory@protonmail.com>
2023-09-16 05:05:15 -0400

    ...

--- src/ae_obj.c
+++ src/ae_obj.c
@@ -57,7 +57,7 @@ bool ae_obj_equal (const ae_obj_t * const this,  const ae_obj_t *  const that) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ae_obj_t * ae_obj_truth (const bool this) {
-  /* It appears that OBJ_TRUE and OBJ_FALSE seem to have the same addresses when this is called,
+  /* It appears that OBJ_TRUE and OBJ_FALSE seem to have the same addresses when this is called
      repeatedly, but I'm not sure if the standard gives any assurance that they always will be.
      Possibly UB? */
   
