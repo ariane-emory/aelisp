dbb75c47dcdbd90a36abf6c57f7b97def99cd427
Ariane Emory <ariane.emory@protonmail.com>
2023-09-16 00:09:48 -0400

    ...

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
-;;; Directory Local Variables            -*- no-byte-compile: t; lisp-indent-offset: 2; -*-
-;;; For more information see (info "(emacs) Directory Variables")
+;;; -*- no-byte-compile: t; lisp-indent-offset: 2; -*-
 
 ((nil . ( (c-backslash-column . 99)
           (c-backslash-column . 99))))
