947f9c950668a548cfa41bea378fceb7b2c4224f
Ariane Emory <ariane.emory@protonmail.com>
2023-09-18 11:54:14 -0400

    ...

--- include/ae_preconditions.h
+++ include/ae_preconditions.h
@@ -15,3 +15,11 @@
 #ifdef NIL_IS_AN_UNINTERNED_SYMBOL
 #  define NIL_DESCRIPTION "NIL_IS_AN_UNINTERNED_SYMBOL"
 #endif
+
+// Bookkeeping defines follow.
+
+#if defined(NIL_IS_AN_INTERNED_SYMBOL) || defined(NIL_IS_AN_INTERNED_SYMBOL)
+#  define NIL_EXISTS (1)
+#else
+#  define NIL_EXISTS (0)
+#endif
