a786efc84ef622e26f366f4ba171fadea31b9e48
Ariane Emory <ariane.emory@protonmail.com>
2023-09-18 09:59:53 -0400

    Add #error "Define either NIL_IS_A_NULLLPTR, NIL_IS_AN_INTERNED_SYMBOL, or NIL_IS_AN_UNINTERNED_SYMBOL."

--- include/ae_preconditions.h
+++ include/ae_preconditions.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#if ! (defined(NIL_IS_A_NULLLPTR) || defined(NIL_IS_AN_INTERNED_SYMBOL) || defined(NIL_IS_AN_UNINTERNED_SYMBOL))
+#  error "Define either NIL_IS_A_NULLLPTR, NIL_IS_AN_INTERNED_SYMBOL, or NIL_IS_AN_UNINTERNED_SYMBOL."
+#endif
