d4629b133944a244d725ee40b0cfc9004b0c67e8
Ariane Emory <ariane.emory@protonmail.com>
2023-09-21 17:33:14 -0400

    ...

--- src/ae_obj_env.c
+++ src/ae_obj_env.c
@@ -6,11 +6,11 @@ ae_obj_t *ae_env_find(ae_obj_t * const this, ae_obj_t * const symbol) {
   ae_obj_t * pos = this;
   
   for (; pos != NIL; pos = pos->parent) {
-    ae_obj_t *symbols = pos->symbols;
-    ae_obj_t *values = pos->values;
+    ae_obj_t * symbols = pos->symbols;
+    ae_obj_t * values  = pos->values;
         
     for (; CONSP(symbols); symbols = CDR(symbols), values = CDR(values))
-      if (CAR(symbols) == symbol)
+      if (EQ(CAR(symbols), symbol))
         return CAR(values);
 
     if (EQ(symbols, symbol))
