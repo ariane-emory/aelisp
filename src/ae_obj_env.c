0029c342f6f516f9f37cbb79c2b85bfbd09b1aa3
Ariane Emory <ariane.emory@protonmail.com>
2023-09-21 21:31:44 -0400

    ...

--- src/ae_obj_env.c
+++ src/ae_obj_env.c
@@ -8,10 +8,10 @@ ae_obj_t * ae_env_add(ae_obj_t * this, ae_obj_t * symbol, ae_obj_t * value) {
   ae_obj_t * symbols = CONS_NEW(symbol);
   ae_obj_t * values  = CONS_NEW(value);
   
-  CDR(symbols)   = ENV_SYMS(this);
-  ENV_SYMS(this) = symbols;
-  CDR(values)    = ENV_VALS(this);
-  ENV_VALS(this) = values;
+  CDR(symbols)       = ENV_SYMS(this);
+  ENV_SYMS(this)     = symbols;
+  CDR(values)        = ENV_VALS(this);
+  ENV_VALS(this)     = values;
 
   return value;
 }
@@ -24,8 +24,8 @@ ae_obj_t *ae_env_set(ae_obj_t * this, ae_obj_t * symbol, ae_obj_t * values) {
   ae_obj_t * pos = this;
   
   while (true) {
-    ae_obj_t * symbols = ENV_SYMS(this);
-    ae_obj_t * vals    = ENV_VALS(this);
+    ae_obj_t * symbols   = ENV_SYMS(this);
+    ae_obj_t * vals      = ENV_VALS(this);
 
     for (; CONSP(symbols); symbols = CDR(symbols), vals = CDR(vals)) {
       if (EQ(CAR(symbols), symbol))
