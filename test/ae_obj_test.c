c3462dc54e399237f21794c131519f4289576414
Ariane Emory <ariane.emory@protonmail.com>
2023-09-21 20:40:31 -0400

    ...

--- test/ae_obj_test.c
+++ test/ae_obj_test.c
@@ -347,15 +347,15 @@ void intern_symbols(void) {
   // deliberately avoid initializing symbols_list because INTERN2 should do so itself automatically.
 
   // re-use 'symbols_list' as the symbol list here:
-  T(INTERN2(&symbols_list, "one") == INTERN2(&symbols_list, "one"));
-  T(strcmp(SYM_VAL(INTERN2(&symbols_list, "one")), "one") == 0);
+  T(INTERN("one") == INTERN("one"));
+  T(strcmp(SYM_VAL(INTERN("one")), "one") == 0);
   T(LENGTH(symbols_list) == 1);
     
   // pool_print();
   /* TEST_MSG("Incorrect length %d, expected %d.", LENGTH(symbols_list), 1); */
   
-  T(INTERN2(&symbols_list, "one") != INTERN2(&symbols_list, "two"));
-  T(strcmp(SYM_VAL(INTERN2(&symbols_list, "two")), "two") == 0);
+  T(INTERN("one") != INTERN("two"));
+  T(strcmp(SYM_VAL(INTERN("two")), "two") == 0);
   T(LENGTH(symbols_list) == 2);
     
   // pool_print();
