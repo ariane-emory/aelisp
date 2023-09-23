570907f49bf48c804f53b87e7fc0ef11e569989f
Ariane Emory <ariane.emory@protonmail.com>
2023-09-22 23:26:29 -0400

    ...

--- include/ae_lisp_primitives.h
+++ include/ae_lisp_primitives.h
@@ -4,7 +4,6 @@
 #include "ae_obj_list.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ae_obj_t * ae_lisp_car  (ae_obj_t * const args);
 ae_obj_t * ae_lisp_cdr  (ae_obj_t * const args);
 ae_obj_t * ae_lisp_cons (ae_obj_t * const args);
@@ -14,3 +13,12 @@ ae_obj_t * ae_lisp_atomp(ae_obj_t * const args);
 ae_obj_t * ae_lisp_not  (ae_obj_t * const args);
 ae_obj_t * ae_lisp_print(ae_obj_t * const args);
 ae_obj_t * ae_lisp_princ(ae_obj_t * const args);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#define FOR_EACH_MATH_OP(DO)                                                                       \
+  DO(add, +, 0)                                                                                    \
+  DO(sub, -, 0)                                                                                    \
+  DO(mul, *, 1)                                                                                    \
+  DO(div, /, 1)
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#define DECL_MATH_OPP(name, ...) ae_obj_t * ae_lisp_##name(ae_obj_t * const args);
+FOR_EACH_MATH_OP(DECL_MATH_OPP);
