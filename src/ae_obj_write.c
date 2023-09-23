6ebc4720613116bc71f07504d951a0e2e76447ef
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 04:06:44 -0400

    ...

--- src/ae_obj_write.c
+++ src/ae_obj_write.c
@@ -41,7 +41,7 @@ int ae_obj_fput(const ae_obj_t * const this, FILE * stream) {
     written  += fprintf(stream, "%018p %018p %018p", this->parent, this->symbols, this->values);
     break;
   case AE_CORE_FUN:
-    written  += fprintf(stream, "% -18s %018p %-18s", NAME_VAL(this), FUN_VAL(this), (SPECIAL_FUNP(this) ? "special" : "-"));
+    written  += fprintf(stream, "% -18s %-18s %018p", NAME_VAL(this), (SPECIAL_FUNP(this) ? "special" : "-"), FUN_VAL(this));
     break;
   default:
     written  += FWRITE(this, stream);
