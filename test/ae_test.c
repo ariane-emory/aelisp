a29cf38efc2857b691ed1d971cc0f5de397c80d1
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 12:45:44 -0400

    ...

--- test/ae_test.c
+++ test/ae_test.c
@@ -750,11 +750,11 @@ void root_env_and_eval(void) {
   PR("syms: ");
   PRINC(ENV_SYMS(env));
   NL;
-  PR("vals: ");
-  PRINC(ENV_VALS(env));
-  NL;
   PR("syms: ");
   WRITE(ENV_SYMS(env));
+  NL;
+  PR("vals: ");
+  PRINC(ENV_VALS(env));
   NL;
   PR("vals: ");
   WRITE(ENV_VALS(env));
