3c3a1a148e195a09e99fdf6bb9c9fcb5dc2cfa26
Ariane Emory <ariane.emory@protonmail.com>
2023-09-22 21:16:56 -0400

    ...

--- test/ae_obj_test.c
+++ test/ae_obj_test.c
@@ -580,9 +580,9 @@ void primitive_cons_car_cdr(void) {
   SETUP_TEST;
 
   T(EQ(ae_lisp_car(make_args_containing_one_list()), INTERN("a")                                       ));
+  T(shitty_write_based_equality_predicate(ae_lisp_cons(CONS(INTERN("a"), CONS_NIL(NIL))), "(a)"        ));
   T(shitty_write_based_equality_predicate(ae_lisp_cdr (make_args_containing_one_list() ), "(b c)"      ));
   T(shitty_write_based_equality_predicate(ae_lisp_cons(make_args_for_cons()            ), "(nil a b c)"));
-  T(shitty_write_based_equality_predicate(ae_lisp_cons(CONS(INTERN("a"), CONS_NIL(NIL))), "(a)"        ));
   T(NILP(ae_lisp_car(                     CONS_NIL(NIL))                                               ));
   T(NILP(ae_lisp_cdr(                     CONS_NIL(NIL))                                               ));
   T(NILP(ae_lisp_car(CONS_NIL(ae_lisp_car(CONS_NIL(NIL))))                                             ));
