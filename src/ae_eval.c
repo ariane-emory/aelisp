de4d7a6ebbca924927e1e9f219570d4dcc7aed43
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 06:45:49 -0400

    ...

--- src/ae_eval.c
+++ src/ae_eval.c
@@ -7,6 +7,13 @@
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
+#define DISPATCH(table, obj, args)                                                                 \
+  for (size_t ix = 0; ix < ARRAY_SIZE(table); ix++)                                                \
+    if (table[ix].type == GET_TYPE(obj))                                                           \
+      return (*table[ix].handler)(obj, args);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 static ae_obj_t * self(ae_obj_t * obj, ae_obj_t * env) {
   (void)env;
   return obj;
@@ -16,21 +23,22 @@ static ae_obj_t * lookup(ae_obj_t * obj, ae_obj_t * env) {
   return ENV_FIND(env, obj);
 }
 
-static ae_obj_t * apply(ae_obj_t * obj, ae_obj_t * env) {
-  return ENV_FIND(env, obj);
+static ae_obj_t * apply_car_to_cdr(ae_obj_t * obj, ae_obj_t * env) {
+  (void)obj, (void)env; assert(0); // not yet implemented
 }
 
-static const struct { ae_type_t type; ae_obj_t * (*func)(ae_obj_t *, ae_obj_t *); } eval_dispatch[] = {
-  { AE_INTEGER,  &self           },
-  { AE_RATIONAL, &self           },
-  { AE_FLOAT,    &self           },
-  { AE_INF,      &self           },
-  { AE_CHAR,     &self           },
-  { AE_STRING,   &self           },
-  { AE_LAMBDA,   &self           },
-  { AE_CORE_FUN, &self           },
-  { AE_SYMBOL,   &lookup         },
-  { AE_CONS,     &apply          },
+static const struct { ae_type_t type; ae_obj_t * (*func)(ae_obj_t *, ae_obj_t *); }
+eval_dispatch[] = {
+  { AE_INTEGER,  &self             },
+  { AE_RATIONAL, &self             },
+  { AE_FLOAT,    &self             },
+  { AE_INF,      &self             },
+  { AE_CHAR,     &self             },
+  { AE_STRING,   &self             },
+  { AE_LAMBDA,   &self             },
+  { AE_CORE_FUN, &self             },
+  { AE_SYMBOL,   &lookup           },
+  { AE_CONS,     &apply_car_to_cdr },
 };
 
 static ae_obj_t * apply_core_fun(ae_obj_t * fun, ae_obj_t * args) {
@@ -41,24 +49,16 @@ static ae_obj_t * apply_lambda(ae_obj_t * fun, ae_obj_t * args) {
   (void)fun, (void)args; assert(0); // not yet implemented
 }
 
-static const struct { ae_type_t type; ae_obj_t * (*func)(ae_obj_t *, ae_obj_t *); } apply_dispatch[] = {
-  { AE_CORE_FUN, &apply_core_fun },
-  { AE_LAMBDA,   &apply_lambda   },
+static const struct { ae_type_t type; ae_obj_t * (*handler)(ae_obj_t *, ae_obj_t *); }
+apply_dispatch[] = {
+  { AE_CORE_FUN, &apply_core_fun   },
+  { AE_LAMBDA,   &apply_lambda     },
 };
-
-#define DISPATCH(table, thing, args)                                                                \
-  for (size_t ix = 0; ix < ARRAY_SIZE(table); ix++)                                                 \
-    if (table[ix].type == GET_TYPE(thing))                                                          \
-      return (*table[ix].func)(thing, args);
-
 
 ae_obj_t * ae_eval(ae_obj_t * obj, ae_obj_t * env) {
   ASSERT_ENVP(env);
 
   DISPATCH(eval_dispatch, obj, env);
-  /* for (size_t ix = 0; ix < ARRAY_SIZE(eval_dispatch); ix++) */
-  /*   if (eval_dispatch[ix].type == GET_TYPE(obj)) */
-  /*     return (*eval_dispatch[ix].func)(obj, env); */
 
   fprintf(stderr, "Don't know how to eval a %s.\n", TYPE_STR(GET_TYPE(obj)));
   assert(0);
@@ -67,11 +67,9 @@ ae_obj_t * ae_eval(ae_obj_t * obj, ae_obj_t * env) {
 ae_obj_t * ae_apply(ae_obj_t * fun, ae_obj_t * args) {
   ASSERT_FUNP(fun);
   ASSERT_TAILP(args);
-  
-  for (size_t ix = 0; ix < ARRAY_SIZE(apply_dispatch); ix++)
-    if (apply_dispatch[ix].type == GET_TYPE(fun))
-      return (*apply_dispatch[ix].func)(fun, args);
 
+  DISPATCH(apply_dispatch, fun, args);
+  
   fprintf(stderr, "Don't know how to apply a %s.\n", TYPE_STR(GET_TYPE(fun)));
   assert(0);
 }
