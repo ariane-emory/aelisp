45acdeb94f160c3d57a3ac06e31bcee34fc5d49f
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 05:48:45 -0400

    ...

--- src/ae_eval.c
+++ src/ae_eval.c
@@ -2,3 +2,13 @@
 #include "ae_obj.h"
 #include "ae_list.h"
 #include "ae_env.h"
+
+// static const struct { uint16_t keycode; keycode_handler_fun_t handler; } keycode_handlers[] PROGMEM = {
+
+static ae_obj_t * self(ae_obj_t * arg) {
+  return arg;
+}
+
+static const struct { ae_type_t type; ae_obj_t * (*func)(ae_obj_t *); } eval_dispatch[] = {
+  { AE_INTEGER, &self }
+};
