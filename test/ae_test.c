f9669d2adb050f94e24ffb884ad126a7a646aea6
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 15:16:11 -0400

    ...

--- test/ae_test.c
+++ test/ae_test.c
@@ -755,7 +755,7 @@ void root_env_and_eval(void) {
   NL;
   NL;
   PR("Here:\n");
-  expr = CONS(INTERN("lambda"), CONS(LIST(INTERN("x")), LIST(CONS(INTERN("princ"), LIST(INTERN("x"))))));
+  expr = CONS(INTERN("lambda"), CONS(LIST(INTERN("x")), CONS(CONS(INTERN("princ"), LIST(INTERN("x"))), CONS(INTERN("princ"), LIST(INTERN("x"))))));
   PRINC(expr); // EVAL(env, expr));
   NL;
   
