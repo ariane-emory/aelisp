0cf44a972e6f038621348d728b13b5346c549c0e
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 02:53:30 -0400

    ...

--- include/ae_core.h
+++ include/ae_core.h
@@ -4,16 +4,21 @@
 #include "ae_obj_list.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-ae_obj_t * ae_core_car  (ae_obj_t * const args);
-ae_obj_t * ae_core_cdr  (ae_obj_t * const args);
-ae_obj_t * ae_core_cons (ae_obj_t * const args);
-ae_obj_t * ae_core_eq   (ae_obj_t * const args);
-ae_obj_t * ae_core_eql  (ae_obj_t * const args);
-ae_obj_t * ae_core_atomp(ae_obj_t * const args);
-ae_obj_t * ae_core_not  (ae_obj_t * const args);
-ae_obj_t * ae_core_print(ae_obj_t * const args);
-ae_obj_t * ae_core_princ(ae_obj_t * const args);
-ae_obj_t * ae_core_write(ae_obj_t * const args);
+#define FOR_EACH_CORE_FUN(DO)                                                                      \
+DO(ae_core_car)                                                                                    \
+DO(ae_core_cdr)                                                                                    \
+DO(ae_core_cons)                                                                                   \
+DO(ae_core_eq)                                                                                     \
+DO(ae_core_eql)                                                                                    \
+DO(ae_core_atomp)                                                                                  \
+DO(ae_core_not)                                                                                    \
+DO(ae_core_print)                                                                                  \
+DO(ae_core_princ)                                                                                  \
+DO(ae_core_write) 
+
+#define DECL_CORE_FUN(name) ae_obj_t * name  (ae_obj_t * const args);
+
+FOR_EACH_CORE_FUN(DECL_CORE_FUN);
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #define FOR_EACH_MATH_OP(DO)                                                                       \
   DO(add, +, 0)                                                                                    \
