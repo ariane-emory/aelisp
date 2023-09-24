da042b051e3c2a9f139b9ab78551c92c860801fb
Ariane Emory <ariane.emory@protonmail.com>
2023-09-24 16:58:34 -0400

    ...

--- src/ae_write.c
+++ src/ae_write.c
@@ -258,8 +258,6 @@ static int ae_fwrite_internal(const ae_obj_t * const this) {
 // obj's _princ methods
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static int ae_fprinc_internal(const ae_obj_t * const this);
-
 int ae_princ(const ae_obj_t * const this) {
   return FPRINC(this, stdout);
 }
@@ -268,10 +266,11 @@ int ae_princ(const ae_obj_t * const this) {
 int ae_fprinc(const ae_obj_t * const this, FILE * stream_) {
   ASSERT_NOT_NULLP(this);
 
+  fprinc_quoting = false;
   fprinc_counter = 0;
   fprinc_stream  = stream_;
 
-  return ae_fprinc_internal(this);
+  return ae_fwrite_internal(this);
 }
 
 char * ae_sprinc(const ae_obj_t * const this) {
@@ -283,75 +282,3 @@ char * ae_sprinc(const ae_obj_t * const this) {
   return buff; // free this when you're done with it.
 }
 
-static int ae_fprinc_internal(const ae_obj_t * const this) {
-  switch (GET_TYPE(this)) {
-  case AE_ENV:
-    if (NILP(ENV_PARENT(this)))
-      COUNTED_FPRINTF(fprinc_stream, "env<nil←%018p>", ENV_PARENT(this), this);
-    else
-      COUNTED_FPRINTF(fprinc_stream, "env<%018p←%018p>", ENV_PARENT(this), this);
-    break;
-  case AE_INF:
-    COUNTED_FPUTS("∞", fprinc_stream);
-    break;
-  case AE_CONS:
-    COUNTED_FPUTC('(', fprinc_stream);
-
-    FOR_EACH_CONST(elem, this) {
-      ae_fprinc_internal(elem);
-      fflush(fprinc_stream);
-        
-      if (! NILP(CDR(position)))
-        COUNTED_FPUTC(' ', fprinc_stream);
-    }
-      
-    COUNTED_FPUTC(')', fprinc_stream);
-    break;
-  case AE_SYMBOL:
-    COUNTED_FPUTS(SYM_VAL(this), fprinc_stream);
-    break;
-  case AE_STRING:
-    if (STR_VAL(this) == NULL)
-      COUNTED_FPUTS("(null)", fprinc_stream);
-    else
-      COUNTED_FPUTS(STR_VAL(this), fprinc_stream);
-    break;
-  case AE_INTEGER:
-    COUNTED_FPRINTF(fprinc_stream, "%d", this->int_val);
-    break;
-  case AE_RATIONAL:
-    COUNTED_FPRINTF(fprinc_stream, "%d/%d", this->numerator_val, this->denominator_val);
-    break;
-  case AE_FLOAT:
-    COUNTED_FPRINTF(fprinc_stream, "%g", this->float_val);
-    break;
-  case AE_CHAR:
-  {
-    char tmp[3] = { 0 };
-
-    switch (this->char_val) {
-
-#define escaped_char_case(displayed, unescaped)                                                    \
-      case unescaped:                                                                              \
-        tmp[0] = '\\';                                                                             \
-        tmp[1] = displayed;                                                                        \
-        break;
-      FOR_EACH_ESCAPED_CHARACTER(escaped_char_case);
-#undef escaped_char_case
-
-    default:
-      tmp[0] = this->char_val;
-    }
-
-    COUNTED_FPUTS(tmp, fprinc_stream);
-    
-    break;
-  }
-  default:
-    COUNTED_FPRINTF(fprinc_stream, "??");
-  }
-  
-  fflush(fprinc_stream);
-  
-  return fprinc_counter;
-}
