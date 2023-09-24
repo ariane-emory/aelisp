1c84dd9478b118851aaa2b80e982980d6e3907f1
Ariane Emory <ariane.emory@protonmail.com>
2023-09-23 22:03:10 -0400

    unbreak

--- src/ae_core.c
+++ src/ae_core.c
@@ -49,11 +49,11 @@
 ae_obj_t * ae_core_setq(ae_obj_t * const env_and_args) {
   SPECIAL_FUN_ARGS(env, args, env_and_args);
 
-// #ifdef AE_LOG_CORE
+#ifdef AE_LOG_CORE
   NL; PR("env_and_args "); PUT(env_and_args); SPC; PRINC(env_and_args);
   NL; PR("env          "); PUT(env         ); SPC; PRINC(env         );
   NL; PR("args         "); PUT(args        ); SPC; PRINC(args        );
-// #endif
+#endif
 
   ASSERT_SYMBOLP(CAR(args));
   ASSERT_CONSP(CDR(args));
