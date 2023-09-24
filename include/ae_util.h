abb500915ab04e78c7a3c0937042a83b13b41ec6
Ariane Emory <ariane.emory@protonmail.com>
2023-09-24 14:12:45 -0400

    ...

--- include/ae_util.h
+++ include/ae_util.h
@@ -8,3 +8,17 @@
 #define PR(...)       (fprintf(stdout, __VA_ARGS__))
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
+#define LOG(desc, o)  (obj_log(#desc, o))
+
+struct ae_obj_t;
+int ae_put(const ae_obj_t * const this);
+int ae_write(const ae_obj_t * const this);
+
+inline void obj_log(char * desc, struct ae_obj_t * obj) {
+  NL;
+  PR("%-20s", desc);
+  SPC;
+  ae_put(obj);
+  SPC;
+  ae_write(obj);
+}
