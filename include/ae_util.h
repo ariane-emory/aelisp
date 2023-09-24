d10c242437482b9332165494419741e5627643ac
Ariane Emory <ariane.emory@protonmail.com>
2023-09-24 15:20:00 -0400

    ...

--- include/ae_util.h
+++ include/ae_util.h
@@ -11,10 +11,13 @@
 #define PR(...)       (fprintf(stdout, __VA_ARGS__))
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
-#define LOG(o, desc)  (obj_log((o), (desc)))
 #define OLOG(o)       (obj_log((o), (#o)))
 
 extern char obj_log_buffer[64];
 
 void obj_log(ae_obj_t * obj, char * desc);
 
+#define LOG(o, desc)  \
+({ \
+  (obj_log((o), (desc))); \
+})
