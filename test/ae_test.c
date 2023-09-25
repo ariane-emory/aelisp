30a7d8bc2490ba34ff7ec054761c35091f7b0e3e
Ariane Emory <ariane.emory@protonmail.com>
2023-09-25 10:28:32 -0400

    ...

--- test/ae_test.c
+++ test/ae_test.c
@@ -848,10 +848,8 @@ void root_env_and_eval(void) {
   {
     ae_obj_t* final_expr = ae_generate_macro_defmacro();
     NL;
-    NL;
     PR("Got      "); PRINC(final_expr); NL;
     PR("Wanted   (setq defmacro (macro (name params . body) (list (quote setq) name (list (quote macro) params . body))))");
-    NL;
     NL;
   }
 
@@ -859,66 +857,16 @@ void root_env_and_eval(void) {
 
     ae_obj_t* final_expr = ae_generate_macro_defun();
     NL;
-    NL;
     PR("Got      "); PRINC(final_expr); NL;
     PR("Wanted   (defmacro defun (name params . body) (list (quote setq) name (list (quote lambda) params . body)))");
     NL;
   }
 
   {
-    NL;
-// (defmacro and args (cond ((null args) t) ((null (cdr args)) (car args)) (t (list (quote if) (car args) (cons (quote and) (cdr args))))))
-
-ae_obj_t* args_part = INTERN("args");
-// printf("%-16s", "args_part"); PRINC(args_part); NL;
-
-// (null args)
-ae_obj_t* null_args_expr = CONS(INTERN("null"), CONS(args_part, NIL));
-// printf("%-16s", "null_args_expr"); PRINC(null_args_expr); NL;j
-
-// (quote if)
-ae_obj_t* quote_if = CONS(INTERN("quote"), CONS(INTERN("if"), NIL));
-// printf("%-16s", "quote_if"); PRINC(quote_if); NL;j
-
-// (quote and)
-ae_obj_t* quote_and = CONS(INTERN("quote"), CONS(INTERN("and"), NIL));
-// printf("%-16s", "quote_and"); PRINC(quote_and); NL;j
-
-// (cons (quote and) (cdr args))
-ae_obj_t* cons_quote_and = CONS(INTERN("cons"), CONS(quote_and, CONS(CONS(INTERN("cdr"), CONS(args_part, NIL)), NIL)));
-// printf("%-16s", "cons_quote_and"); PRINC(cons_quote_and); NL;j
-
-// (list (quote if) (car args) (cons (quote and) (cdr args)))
-ae_obj_t* inner_list_expr = CONS(INTERN("t"), CONS(CONS(INTERN("list"), CONS(quote_if, CONS(CONS(INTERN("car"), CONS(args_part, NIL)), CONS(cons_quote_and, NIL)))), NIL));
-// printf("%-16s", "inner_list_expr"); PRINC(inner_list_expr); NL;j
-
-// (null args)
-ae_obj_t* null_args = CONS(CONS(INTERN("null"), CONS(args_part, NIL)), CONS(TRUE, NIL));
-// printf("%-16s", "null_args"); PRINC(null_args); NL;j
-
-// (null (cdr args))
-ae_obj_t* null_cdr_args = CONS(CONS(INTERN("null"), CONS(CONS(INTERN("cdr"), CONS(args_part, NIL)), NIL)), CONS(CONS(INTERN("car"), CONS(args_part, NIL)), NIL));
-// printf("%-16s", "null_cdr_args"); PRINC(null_cdr_args); NL;j
-
-// (list (quote if) (car args) (cons (quote and) (cdr args)))
-ae_obj_t* cond_expr = CONS(INTERN("cond"), CONS(null_args, CONS(null_cdr_args, CONS(inner_list_expr, NIL))));
-// printf("%-16s", "cond_expr"); PRINC(cond_expr); NL;j
-
-// (defmacro and args (cond ...))
-ae_obj_t* final_expr = CONS(
-  INTERN("defmacro"),
-  CONS(INTERN("and"),
-       CONS(args_part,
-            CONS(cond_expr,
-                 NIL))));
-
-// printf("%-16s", "final_expr"); PRINC(final_expr); NL;
-
-    NL;
+    ae_obj_t* final_expr = ae_generate_macro_and();
     NL;
     PR("Got      "); PRINC(final_expr); NL;
     PR("Wanted   (defmacro and args (cond ((null args) t) ((null (cdr args)) (car args)) (t (list (quote if) (car args) (cons (quote and) (cdr args))))))");
-    NL;
     NL;
   }
 
