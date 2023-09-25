4360584200f25eaa4dff387a284389106e279655
Ariane Emory <ariane.emory@protonmail.com>
2023-09-25 09:31:23 -0400

    ...

--- test/ae_test.c
+++ test/ae_test.c
@@ -893,43 +893,43 @@ expr_t* final_expr = CONS(INTERN("defmacro"), CONS(INTERN("defun"), CONS(args_pa
 // (defmacro and args (cond ((null args) t) ((null (cdr args)) (car args)) (t (list (quote if) (car args) (cons (quote and) (cdr args))))))
 
 expr_t* args_part = INTERN("args");
-OLOG(args_part);
+PR("args_part "); PRINC(args_part);
 
 // (null args)
 expr_t* null_args_expr = CONS(CONS(INTERN("null"), CONS(args_part, NIL)), NIL);
-OLOG(null_args_expr);
+PR("null_args_expr "); PRINC(null_args_expr); NL;
 
 // (quote if)
 expr_t* quote_if = CONS(INTERN("quote"), CONS(INTERN("if"), NIL));
-OLOG(quote_if);
+PR("quote_if "); PRINC(quote_if); NL;
 
 // (quote and)
 expr_t* quote_and = CONS(INTERN("quote"), CONS(INTERN("and"), NIL));
-OLOG(quote_and);
+PR("quote_and "); PRINC(quote_and); NL;
 
 // (cons (quote and) (cdr args))
 expr_t* cons_quote_and = CONS(INTERN("cons"), CONS(quote_and, CONS(CONS(INTERN("cdr"), CONS(args_part, NIL)), NIL)));
-OLOG(cons_quote_and);
+PR("cons_quote_and "); PRINC(cons_quote_and); NL;
 
 // (list (quote if) (car args) (cons (quote and) (cdr args)))
 expr_t* inner_list_expr = CONS(INTERN("list"), CONS(quote_if, CONS(CONS(INTERN("car"), CONS(args_part, NIL)), CONS(cons_quote_and, NIL))));
-OLOG(inner_list_expr);
+PR("inner_list_expr "); PRINC(inner_list_expr); NL;
 
 // (null args)
 expr_t* null_args = CONS(CONS(CONS(INTERN("null"), CONS(args_part, NIL)), CONS(NIL, NIL)), NIL);
-OLOG(null_args);
+PR("null_args "); PRINC(null_args); NL;
 
 // (null (cdr args))
 expr_t* null_cdr_args = CONS(CONS(CONS(INTERN("null"), CONS(CONS(INTERN("cdr"), CONS(args_part, NIL)), NIL)), CONS(CONS(INTERN("car"), CONS(args_part, NIL)), NIL)), NIL);
-OLOG(null_cdr_args);
+PR("null_cdr_args "); PRINC(null_cdr_args); NL;
 
 // (list (quote if) (car args) (cons (quote and) (cdr args)))
 expr_t* cond_expr = CONS(CONS(INTERN("cond"), CONS(null_args, CONS(null_cdr_args, CONS(inner_list_expr, NIL)))), NIL);
-OLOG(cond_expr);
+PR("cond_expr "); PRINC(cond_expr); NL;
 
 // (defmacro and args (cond ...))
 expr_t* final_expr = CONS(INTERN("defmacro"), CONS(INTERN("and"), CONS(args_part, CONS(CONS(cond_expr, NIL), NIL))));
-OLOG(final_expr);
+PR("final_expr "); PRINC(final_expr); NL;
 
 
 
