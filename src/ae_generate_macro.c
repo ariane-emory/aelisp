70d94db070643a61cdffbaf7964a1af605cecbb6
Ariane Emory <ariane.emory@protonmail.com>
2023-09-25 10:11:42 -0400

    ...

---
+++ src/ae_generate_macro.c
@@ -0,0 +1,6 @@
+#include "ae_generate_macro.h"
+
+ae_obj_t * ae_generate_macro_defmacro(void);
+ae_obj_t * ae_generate_macro_defun   (void);
+ae_obj_t * ae_generate_macro_and     (void);
+ae_obj_t * ae_generate_macro_or      (void);
