30a0b7d88aa81ba4cee3042babee24fe4df385c8
Ariane Emory <ariane.emory@protonmail.com>
2023-09-25 14:09:09 -0400

    ...

--- src/ae_eval.c
+++ src/ae_eval.c
@@ -52,10 +52,9 @@ static ae_obj_t * apply(ae_obj_t * list, ae_obj_t * env) {
 
 ae_obj_t * apply_core_fun(ae_obj_t * fun, ae_obj_t * env, ae_obj_t * args) {
 #ifdef AE_LOG_EVAL
-  LOG(fun, "[apply core %s]", fun->name);
+  LOG(fun, "\n[apply core %s]", fun->name);
   LOG(env, "apply core env");
   LOG(args, "apply core args");
-  NL;
 #endif
 
   MAYBE_EVAL(SPECIAL_FUNP(fun), args);
@@ -64,7 +63,7 @@ ae_obj_t * apply_core_fun(ae_obj_t * fun, ae_obj_t * env, ae_obj_t * args) {
     : (*FUN_VAL(fun))(args);
   
 #ifdef AE_LOG_EVAL
-  LOG(ret, "appl core %s retrns", fun->name);
+  LOG(ret, "appl core %s rtrns", fun->name);
   NL;
 #endif
   
