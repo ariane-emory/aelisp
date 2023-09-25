1c9a9e5823e641df91e3a12b69d1f6709a773281
Ariane Emory <ariane.emory@protonmail.com>
2023-09-25 03:09:39 -0400

    ...

--- test/ae_test.c
+++ test/ae_test.c
@@ -737,7 +737,7 @@ void root_env_and_eval(void) {
   expr = CONS(INTERN("progn"), CONS(CONS(INTERN("princ"), LIST(NEW_STRING("Hello "))), CONS(CONS(INTERN("princ"), LIST(NEW_STRING("from Ash"))), LIST(CONS(INTERN("princ"), LIST(NEW_STRING("Lisp!")))))));
 
   NL;
-  PR("Printing \"Hello from Ash Lisp!\" on the next line:\n ");
+  PR("Printing \"Hello from Ash Lisp!\" on the next line:\n");
   this = EVAL(env, expr);
   T(EQL(NEW_INT(5), this));
   NL;
@@ -785,7 +785,7 @@ void root_env_and_eval(void) {
   // OLOG(expr);
   // OLOG(subexpr); NL;
 
-  PR("Printing 12 on the next line:\n ");
+  PR("Printing 12 on the next line:\n");
   result = EVAL(env, expr);
   NL;
 
