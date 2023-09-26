1fc5b52c99e219ffd3cda2023baf8951f18da0d4
Ariane Emory <ariane.emory@protonmail.com>
2023-09-26 18:21:00 -0400

    ...

--- src/ae_util.c
+++ src/ae_util.c
@@ -3,8 +3,13 @@
 char obj_log_buffer[64];
 
 void obj_log(const ae_obj_t * const obj, char * desc) {
-  NL;
-  PR("%-24s %018p ", desc, obj );
+  int written = 0;
+
+  NL; written ++;
+
+  written += PR("%-24s ", desc);
+  written += WRITE(obj);
+  while written++ < 40 SPC;
   WRITE(obj);
   fflush(stdout);
 }
