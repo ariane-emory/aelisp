2ae65765e7d7b98c747006021fc9624cb2c47e91
Ariane Emory <ariane.emory@protonmail.com>
2023-09-26 18:19:42 -0400

    ...

--- src/ae_util.c
+++ src/ae_util.c
@@ -2,22 +2,10 @@
 
 char obj_log_buffer[64];
 
-#ifdef OLD_LOG
 void obj_log(const ae_obj_t * const obj, char * desc) {
   NL;
-  PR("%-24s %018p", desc, obj);
-  SPC;
-  PUT(obj);
-  SPC;
+  PR("%-24s %018p ", desc, obj );
   WRITE(obj);
   fflush(stdout);
 }
-#else
-void obj_log(const ae_obj_t * const obj, char * desc) {
-  NL;
-  PR("%-24s %018p [ %s ", desc, obj, TYPE_STR(obj));
-  WRITE(obj);
-  PR(" ]");
-  fflush(stdout);
-}
-#endif
+
