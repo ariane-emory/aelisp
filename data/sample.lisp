e83bbd54ed6c9277a141fea01166046b3023a36b
Ariane Emory <ariane.emory@protonmail.com>
2023-09-27 08:42:47 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -6,43 +6,40 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (print (env :syms))
-
-(nl)
 (nl)
 
-(nl)
 (princ "BEFORE add") (nl)
 (write (setq add   (macro (xxx yyy) (list (quote +) xxx yyy)))) (nl)
 (princ "Params: ") (write (params add)) (nl)
 (princ "Body:   ") (write (body add)) (nl)
 (princ "AFTER") (nl)
-
 (nl)
+ 
 (princ "BEFORE defmacro") (nl)
 (write (setq defmacro (macro (name params . body) (list (quote setq) name (list (quote macro) params . body))))) (nl)
 (princ "Params: ") (write (params defmacro)) (nl)
 (princ "Body:   ") (write (body   defmacro))   (nl)
 (princ "AFTER") (nl)
-
 (nl)
+
 (princ "BEFORE madd1") (nl)
 (write (setq madd (macro (xxx yyy) (list (quote +) xxx yyy)))) (nl)
 (princ "Params: ") (write (params madd)) (nl)
 (princ "Body:   ") (write (body madd)) (nl)
 (princ "AFTER") (nl)
-
 (nl)
+
 (princ "BEFORE madd2") (nl)
 (write (eval (defmacro madd (xxx yyy) (list (quote +) xxx yyy))))
 (princ "Params: ") (write (params madd)) (nl)
 (princ "Body:   ") (write (body madd)) (nl)
 (princ "AFTER") (nl)
+(nl)
 
 
 
 (stop)
 
-(nl)
 (princ "BEFORE defun") (nl)
 (setq defun (macro (name params . body) (list (quote setq) name (list (quote lambda) params . body))))
 (defmacro defun (name params . body) (list (quote setq) name (list (quote lambda) params . body)))
@@ -50,4 +47,6 @@
 (princ "Params: ") (write (params defun)) (nl)
 (princ "Body:   ") (write (body defun)) (nl)
 (princ "AFTER") ; (nl)
+(nl)
+
 
