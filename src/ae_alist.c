24cb649264a7c598fd0eb0c4946e644a831757dd
Ariane Emory <ariane.emory@protonmail.com>
2023-09-29 00:55:30 -0400

    fix alist funs

--- src/ae_alist.c
+++ src/ae_alist.c
@@ -1,27 +1,29 @@
 #include "ae_list.h"
 #include "ae_alist.h"
+#include "ae_util.h"
 
 ae_obj_t * ae_alist_set(ae_obj_t * const alist, ae_obj_t * const key, ae_obj_t * const value) {
-  FOR_EACH(elem, alist)
-    if (CAR(position) == key) {
-      CDR(position) = value;
+  FOR_EACH(elem, alist)    
+    if (CAR(elem) == key) {
+      CDR(elem) = value;
       return alist;
     }
+
   
   return CONS(NEW_CONS(key, value), alist);
 }
 
 ae_obj_t * ae_aelist_get(ae_obj_t * const alist, ae_obj_t * const key) {
   FOR_EACH(elem, alist)
-    if (CAR(position) == key)
-      return CDR(position);
+    if (CAR(elem) == key)
+      return CDR(elem);
   
   return NIL; 
 }
 
 ae_obj_t * ae_aelist_contains_key(ae_obj_t * const alist, ae_obj_t * const key) {
   FOR_EACH(elem, alist)
-    if (CAR(position) == key)
+    if (CAR(elem) == key)
       return TRUE;
   
   return NIL; 
