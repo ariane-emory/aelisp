bcf087f7a9849f8bb958cb3efc5a9a1fc50e2efd
Ariane Emory <ariane.emory@protonmail.com>
2023-09-29 13:55:07 -0400

    ...

--- include/ae_eval.h
+++ include/ae_eval.h
@@ -7,5 +7,8 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t * ae_eval (ae_obj_t * env, ae_obj_t * obj);
-ae_obj_t * ae_apply(ae_obj_t * fun, ae_obj_t * env, ae_obj_t * args);
+/* ae_obj_t * ae_eval (ae_obj_t * env, ae_obj_t * obj); */
+/* ae_obj_t * ae_apply(ae_obj_t * fun, ae_obj_t * env, ae_obj_t * args); */
+
+ae_obj_t * ae_eval (ae_obj_t *, ae_obj_t *);
+ae_obj_t * ae_apply(ae_obj_t *, ae_obj_t *, ae_obj_t *);
