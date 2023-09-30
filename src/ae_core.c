e4c9a2e4e24428c4d765246d952355067f9abe76
Ariane Emory <ariane.emory@protonmail.com>
2023-09-30 10:20:45 -0400

    ...

--- src/ae_core.c
+++ src/ae_core.c
@@ -264,12 +264,12 @@ ae_obj_t * ae_core_env(ae_obj_t * const env, ae_obj_t * const args) {
   if (len == 1) {
     REQUIRE(env, args, (ENVP(CAR(args)) || LAMBDAP(CAR(args)) || MACROP(CAR(args))));
     
-    return ENVP(CAR(args))
-      ? ENV_PARENT(CAR(args))
-      : FUN_ENV(CAR(args));
+    CORE_RETURN("env", ENVP(CAR(args))
+                ? ENV_PARENT(CAR(args))
+                : FUN_ENV(CAR(args)));
   }
   
-  return env;
+  CORE_RETURN("env", env);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
