3a1ac18bc560f5b5405ef9817ef8d5d7f514dc61
Ariane Emory <ariane.emory@protonmail.com>
2023-10-02 02:22:49 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -4,7 +4,7 @@
 (+ x 6) ;; a normal 'list', due to the presence of the 'x' symbol.
 (+ 7 x) ;; a normal 'list', due to the presence of the 'x' symbol. 
 (+ x y) ;; also a normal 'list', due to presence of the the symbols. 
-(8 . (9)) ;; a normal 'list' identical to (8 9).
+(+ 8 . (9)) ;; a normal 'list' identical to (8 9).
 
 ;; This one used to parse as a normal 'list' before the addition of the 'math_list' rule, but now fails to parse.
 ;; I'd compromise for it successfully parsing as a normal 'list', as it used to, it doesn't have to be a 'math_list'.
