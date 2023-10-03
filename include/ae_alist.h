7fc42ca585d8f223815672584ce4116852933f9c
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 00:06:06 -0400

    ...

--- include/ae_alist.h
+++ include/ae_alist.h
@@ -8,7 +8,7 @@
 #define AGET(alist, key)         (ae_alist_get (&(alist), (key)))
 #define AHAS(alist, key)         (ae_alist_contains_key(&(alist), (key)))
 
-ae_obj_t * ae_alist_set          (ae_obj_t ** const   alist, ae_obj_t * const key, ae_obj_t * const value);
-ae_obj_t * ae_alist_get          (ae_obj_t *  const * alist, ae_obj_t * const key);
-bool       ae_alist_contains_key (ae_obj_t *  const * alist, ae_obj_t * const key);
+ae_obj_t * ae_alist_set          (ae_obj_t **        alist, ae_obj_t * const key, ae_obj_t * const value);
+ae_obj_t * ae_alist_get          (ae_obj_t * const * alist, ae_obj_t * const key);
+bool       ae_alist_contains_key (ae_obj_t * const * alist, ae_obj_t * const key);
 
--- src/ae_alist.c
+++ src/ae_alist.c
@@ -2,15 +2,15 @@
 #include "ae_alist.h"
 #include "ae_util.h"
 
-ae_obj_t * ae_alist_set(ae_obj_t ** const alist, ae_obj_t * const key, ae_obj_t * const value) {
+ae_obj_t * ae_alist_set(ae_obj_t ** alist, ae_obj_t * const key, ae_obj_t * const value) {
     if (*alist == NULL)
       *(ae_obj_t **)alist = NIL;
 
     FOR_EACH(elem, *alist)    
-    if (CAR(elem) == key) {
-      CDR(elem) = value;
-      return *alist;
-    }
+      if (CAR(elem) == key) {
+        CDR(elem) = value;
+        return *alist;
+      }
   
   return *alist = CONS(NEW_CONS(key, value), *alist);
 }
