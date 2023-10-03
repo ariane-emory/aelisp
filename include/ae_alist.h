ee399cbcdee555f4ff468890f7dc543d48d25580
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 03:06:11 -0400

    ...

--- include/ae_alist.h
+++ include/ae_alist.h
@@ -2,13 +2,11 @@
 
 #include <stdbool.h>
 
-#include "ae_obj.h"
-
 #define ASET(alist, key, value)  (ae_alist_set (&(alist), (key), (value)))
 #define AGET(alist, key)         (ae_alist_get (&(alist), (key)))
 #define AHAS(alist, key)         (ae_alist_contains_key(&(alist), (key)))
 
-ae_obj_t * ae_alist_set          (ae_obj_t **        alist, ae_obj_t * const key, ae_obj_t * const value);
-ae_obj_t * ae_alist_get          (ae_obj_t * const * alist, ae_obj_t * const key);
-bool       ae_alist_contains_key (ae_obj_t * const * alist, ae_obj_t * const key);
+struct ae_obj_t * ae_alist_set   (struct ae_obj_t **        alist, struct ae_obj_t * const key, struct ae_obj_t * const value);
+struct ae_obj_t * ae_alist_get   (struct ae_obj_t * const * alist, struct ae_obj_t * const key);
+bool       ae_alist_contains_key (struct ae_obj_t * const * alist, struct ae_obj_t * const key);
 
