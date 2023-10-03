bfc1b286007f725866a8cd319472fc9653b4dd95
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 00:00:56 -0400

    ...

--- src/ae_alist.c
+++ src/ae_alist.c
@@ -3,7 +3,10 @@
 #include "ae_util.h"
 
 ae_obj_t * ae_alist_set(ae_obj_t ** const alist, ae_obj_t * const key, ae_obj_t * const value) {
-  FOR_EACH(elem, *alist)    
+    if (*alist == NULL)
+      *(ae_obj_t **)alist = NIL;
+
+    FOR_EACH(elem, *alist)    
     if (CAR(elem) == key) {
       CDR(elem) = value;
       return *alist;
@@ -12,16 +15,22 @@ ae_obj_t * ae_alist_set(ae_obj_t ** const alist, ae_obj_t * const key, ae_obj_t 
   return *alist = CONS(NEW_CONS(key, value), *alist);
 }
 
-ae_obj_t * ae_alist_get(ae_obj_t * const alist, ae_obj_t * const key) {
-  FOR_EACH(elem, alist)
+ae_obj_t * ae_alist_get(ae_obj_t * const * alist, ae_obj_t * const key) {
+  if (*alist == NULL)
+    *(ae_obj_t **)alist = NIL;
+
+  FOR_EACH(elem, *alist)
     if (CAR(elem) == key)
       return CDR(elem);
   
   return NIL; 
 }
 
-bool ae_alist_contains_key(ae_obj_t * const alist, ae_obj_t * const key) {
-  FOR_EACH(elem, alist)
+bool ae_alist_contains_key(ae_obj_t * const * alist, ae_obj_t * const key) {
+  if (*alist == NULL)
+    *(ae_obj_t **)alist = NIL;
+    
+  FOR_EACH(elem, *alist)
     if (CAR(elem) == key)
       return true;
   
