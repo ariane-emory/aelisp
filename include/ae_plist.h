cad5ac74377b384efa7d1388ea460cf17ef8dc1a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 03:08:05 -0400

    ...

--- include/ae_plist.h
+++ include/ae_plist.h
@@ -2,13 +2,10 @@
 
 #include <stdbool.h>
 
-#include "ae_obj.h"
-#include "ae_list.h"
-
 #define PSET(alist, key, value)  (ae_plist_set (&(alist), (key), (value)))
 #define PGET(alist, key)         (ae_plist_get (&(alist), (key)))
 #define PHAS(alist, key)         (ae_plist_contains_key(&(alist), (key)))
 
-ae_obj_t * ae_plist_set(         ae_obj_t **        plist, ae_obj_t * const key, ae_obj_t * const value);
-ae_obj_t * ae_plist_get(         ae_obj_t * const * plist, ae_obj_t * const key);
-bool       ae_plist_contains_key(ae_obj_t * const * plist, ae_obj_t * const key);
+struct ae_obj_t * ae_plist_set         (struct ae_obj_t **        plist, struct ae_obj_t * const key, struct ae_obj_t * const value);
+struct ae_obj_t * ae_plist_get         (struct ae_obj_t * const * plist, struct ae_obj_t * const key);
+bool              ae_plist_contains_key(struct ae_obj_t * const * plist, struct ae_obj_t * const key);
