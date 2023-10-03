d1c375eb1198cae41fcd61d2c5c9e087a421f864
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 17:26:22 -0400

    ...

--- src/ae_alist.c
+++ src/ae_alist.c
@@ -1,36 +1,40 @@
+#include <stdio.h>
+
 #include "ae_list.h"
 #include "ae_alist.h"
+#include "ae_write.h"
 #include "ae_util.h"
 
 ae_obj_t * ae_alist_set(ae_obj_t ** list, ae_obj_t * const key, ae_obj_t * const value) {
   if (*list == NULL)
     *(ae_obj_t **)list = NIL;
 
-  FOR_EACH(elem, *list)    
-    if (CAR(elem) == key) {
-      return (CDR(elem) = value);
+  if (*list != NIL)
+    FOR_EACH(elem, *list)
+      if (CAR(elem) == key)
+        return (CDR(elem) = value);
   
   return *list = CONS(NEW_CONS(key, value), *list);
 }
 
-ae_obj_t * ae_alist_get(ae_obj_t * const * list, ae_obj_t * const key) {
+ae_obj_t * ae_alist_get(ae_obj_t * const list, ae_obj_t * const key) {
   if (list == NULL)
     return NIL;
 
   if (list == NIL)
     return NIL;
 
-  FOR_EACH(elem, *list)
+  FOR_EACH(elem, list)
     if (CAR(elem) == key)
       return CDR(elem);
   
-  return NIL; 
+  return NIL;
 }
 
-bool ae_alist_contains_key(ae_obj_t * const * list, ae_obj_t * const key) {
-#ifdef AE_LOG_ALIST
-  PR("%s got list %8p.\n", __func__, list);
-#endif
+bool ae_alist_contains_key(ae_obj_t * const list, ae_obj_t * const key) {
+/* #ifdef AE_LOG_ALIST */
+/*   PR("%s got list %8p.\n", __func__, list); */
+/* #endif */
 
   if (list == NULL)
     return false;
@@ -38,10 +42,10 @@ bool ae_alist_contains_key(ae_obj_t * const * list, ae_obj_t * const key) {
   if (list == NIL)
     return false;
     
-  FOR_EACH(elem, *list)
+  FOR_EACH(elem, list)
     if (CAR(elem) == key)
       return true;
   
-  return false; 
+  return false;
 }
 
--- src/ae_plist.c
+++ src/ae_plist.c
@@ -18,7 +18,7 @@ ae_obj_t * ae_plist_set(ae_obj_t ** list, ae_obj_t * const key, ae_obj_t * const
         return CADR(position) = value;
     }
 
-  return (*(ae_obj_t **)list = CONS(key, CONS(value, *list)));
+  return *list = CONS(key, CONS(value, *list));
 }
 
 ae_obj_t * ae_plist_get(ae_obj_t * const list, ae_obj_t * const key) {
@@ -39,10 +39,10 @@ ae_obj_t * ae_plist_get(ae_obj_t * const list, ae_obj_t * const key) {
   return NIL;
 }
 
-bool ae_plist_contains_key(ae_obj_t * const * list, ae_obj_t * const key) {
-#ifdef AE_LOG_PLIST
-  PR("%s got list %8p.\n", __func__, list);
-#endif
+bool ae_plist_contains_key(ae_obj_t * const list, ae_obj_t * const key) {
+/* #ifdef AE_LOG_PLIST */
+/*   PR("%s got list %8p.\n", __func__, list); */
+/* #endif */
   
   if (list == NULL)
     return false;
@@ -50,7 +50,7 @@ bool ae_plist_contains_key(ae_obj_t * const * list, ae_obj_t * const key) {
   if (list == NIL)
     return false;
   
-  for (ae_obj_t * position = *list;
+  for (ae_obj_t * position = list;
        position != NIL;
        position = CDR(CDR(position)))
     if (CAR(position) == key)
