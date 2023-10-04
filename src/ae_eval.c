e98c0d921c2816c9d9d2b21388ee5da3b765d029
Ariane Emory <ariane.emory@protonmail.com>
2023-10-04 18:17:27 -0400

    ...

--- src/ae_eval.c
+++ src/ae_eval.c
@@ -85,7 +85,7 @@ static ae_obj_t * apply_core(ae_obj_t * env, ae_obj_t * fun, ae_obj_t * args) {
 
 #ifdef AE_LOG_EVAL
   INDENT;
-  LOG(env,  "in env 0x%08p (%s)", env, GET_TYPE_STR(env));
+  //LOG(env,  "in env 0x%08p (%s)", env, GET_TYPE_STR(env));
 #endif
 
 #if defined(AE_DEBUG_OBJ) && defined(AE_LOG_EVAL)
@@ -99,9 +99,9 @@ static ae_obj_t * apply_core(ae_obj_t * env, ae_obj_t * fun, ae_obj_t * args) {
     LOG(args, "applying core '%s' to %d evaluated args:", CORE_NAME(fun), LENGTH(args));
 #endif
 
-  INDENT;
+  //INDENT;
   ae_obj_t * ret = (*CORE_FUN(fun))(env, args);
-  OUTDENT;
+  //OUTDENT;
 
 #ifdef AE_LOG_EVAL
   OUTDENT;
