f98a18deccd5c75e51f389ccd50075b4628e18d7
Ariane Emory <ariane.emory@protonmail.com>
2023-10-03 21:42:50 -0400

    ...

--- src/ae_pool.c
+++ src/ae_pool.c
@@ -79,7 +79,7 @@ void pool_print(void) {
 
   bool started = false;
   
-  for (size_t ix = 0; ix < AE_OBJ_POOL_SIZE; ix++) {
+  for (int_t ix = 0; ix < AE_OBJ_POOL_SIZE; ix++) {
     int written = 0;
 
     if ((! started) && (! FREEP(&pool[ix])))
