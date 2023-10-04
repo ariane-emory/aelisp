80eb625eada20407231ff4b64a98f2a95587b879
Ariane Emory <ariane.emory@protonmail.com>
2023-10-04 07:52:44 -0400

    ...

--- src/ae_core.c
+++ src/ae_core.c
@@ -162,20 +162,20 @@ ae_obj_t * ae_core_ahas(ae_obj_t * const env, ae_obj_t * const args) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ae_obj_t * ae_core_pset(ae_obj_t * const env, ae_obj_t * const args) {
-  CORE_BEGIN("aset");
+  CORE_BEGIN("pset");
 
   int len = LENGTH(args);
 
-  REQUIRE(env, args, len >= 2, "aset requires at least 2 args");
-  REQUIRE(env, args, len <= 3, "aset requires 2 or 3 args");
+  REQUIRE(env, args, len >= 2, "pset requires at least 2 args");
+  REQUIRE(env, args, len <= 3, "pset requires 2 or 3 args");
 
-  ae_obj_t * alist = CAR(args);
+  ae_obj_t * plist = CAR(args);
   ae_obj_t * key   = CADR(args);
   ae_obj_t * value = CADDR(args);
 
   REQUIRE(env, args, SYMBOLP(key));
 
-  CORE_RETURN("aset", PSET(alist, key, value));
+  CORE_RETURN("pset", PSET(plist, key, value));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -183,18 +183,18 @@ ae_obj_t * ae_core_pset(ae_obj_t * const env, ae_obj_t * const args) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ae_obj_t * ae_core_pget(ae_obj_t * const env, ae_obj_t * const args) {
-  CORE_BEGIN("aget");
+  CORE_BEGIN("pget");
 
   int len = LENGTH(args);
 
-  REQUIRE(env, args, len == 2, "aget requires 2 args");
+  REQUIRE(env, args, len == 2, "pget requires 2 args");
 
-  ae_obj_t * alist = CAR(args);
+  ae_obj_t * plist = CAR(args);
   ae_obj_t * key   = CADR(args);
 
   REQUIRE(env, args, SYMBOLP(key));
 
-  CORE_RETURN("aget", PGET(alist, key));
+  CORE_RETURN("pget", PGET(plist, key));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -202,18 +202,18 @@ ae_obj_t * ae_core_pget(ae_obj_t * const env, ae_obj_t * const args) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ae_obj_t * ae_core_phas(ae_obj_t * const env, ae_obj_t * const args) {
-  CORE_BEGIN("ahas");
+  CORE_BEGIN("phas");
 
   int len = LENGTH(args);
 
-  REQUIRE(env, args, len == 2, "aget requires 2 args");
+  REQUIRE(env, args, len == 2, "pget requires 2 args");
 
-  ae_obj_t * alist = CAR(args);
+  ae_obj_t * plist = CAR(args);
   ae_obj_t * key   = CADR(args);
 
   REQUIRE(env, args, SYMBOLP(key));
 
-  CORE_RETURN("ahas", TRUTH(PHAS(alist, key)));
+  CORE_RETURN("phas", TRUTH(PHAS(plist, key)));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
