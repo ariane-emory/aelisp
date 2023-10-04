bd17a8920ab34bcc295f753c8a014350423a433e
Ariane Emory <ariane.emory@protonmail.com>
2023-10-04 18:52:05 -0400

    this indentation looks okay, maybe?

--- src/ae_eval.c
+++ src/ae_eval.c
@@ -148,11 +148,13 @@ static ae_obj_t * apply_user(ae_obj_t * env, ae_obj_t * fun, ae_obj_t * args) {
   ae_obj_t * body    = CONS(SYM("progn"), FUN_BODY(fun));
 
 #ifdef AE_LOG_EVAL
-  LOG(env,              "apply user fun in new env");
+  LOG(env,              "user fun will be applied in new env");
+  INDENT;
   LOG(ENV_PARENT(env),  "new user fun env parent");
   LOG(ENV_SYMS(env),    "new env symbols");
   LOG(ENV_VALS(env),    "new env values");
   LOG(body,             "new env body");
+  OUTDENT;
 #endif
 
 #ifdef AE_DEBUG_OBJ
