3d15b180aed7f6cb4ff4eaad4759c77982ea2b39
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 02:21:08 -0400

    update _util.c

--- src/ae_util.c
+++ src/ae_util.c
@@ -6,7 +6,7 @@
 // Data
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const int  log_column_default =   72;
+const int  log_column_default =   85;
 /* */ int  log_column         =   log_column_default;
 /* */ bool log_column_auto    =   true;
 /* */ char log_buffer[256]    = { 0 };
