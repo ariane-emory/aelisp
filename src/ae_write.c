9ec2e288441254f3534628dcba23d8b5ffd3aa6a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 04:39:06 -0400

    cover macro case

--- src/ae_write.c
+++ src/ae_write.c
@@ -237,7 +237,7 @@ static int ae_fwrite_internal(const ae_obj_t * const this) {
         else
           COUNTED_FPRINTF(fwrite_stream, "%s<%s, %08p " ARROW " %08p>", GET_TYPE_STR(this), fun_name, this, ENV_PARENT(this));
       }
-      else if (LAMBDAP(DGET(this, "fun"))) {
+      else if (LAMBDAP(DGET(this, "fun")) || MACROP(DGET(this, "fun"))) {
         char * fun_name = SYM_VAL(DGET(DGET(this, "fun"), "last-bound-to"));
         
         if (NILP(ENV_PARENT(this))) 
