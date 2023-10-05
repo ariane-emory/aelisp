f23f5727b82ce96d1d872c28fc78b1505e174a45
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 19:56:00 -0400

    ...

--- include/ae_env.h
+++ include/ae_env.h
@@ -22,10 +22,11 @@ _obj;                                                                           
 #define ENV_FIND(env, sym)     (ae_env_find((env), (sym)))                                        //
 #define ENV_ADD(env, sym, val) (ae_env_add ((env), (sym), (val)))                                 //
 #define ENV_SET(env, sym, val) (ae_env_set ((env), (sym), (val)))                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 #define ENV_NEW_ROOT()         (ae_env_new_root())                                                //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ROOTP(env)             (ENVP((env)) && (NILP(ENV_PARENT((env)))))                         //
 #define ENVP(o)                ((! NULLP((o))) && (GET_TYPE((o)) == AE_ENV))                      //
+#define ROOTP(env)             (ENVP((env)) && (NILP(ENV_PARENT((env)))))                         //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ae_obj_t * ae_env_find    (ae_obj_t * const env, ae_obj_t * const symbol                          );
 void       ae_env_add     (ae_obj_t * const env, ae_obj_t * const symbol, ae_obj_t * const value  );
