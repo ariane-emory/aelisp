346137c9eecdb4cc3a8d3a7e28aa82854b69dafb
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 21:25:12 -0400

    ...

---
+++ src/core/core_setq.c
@@ -0,0 +1,68 @@
+#include "core_includes.h"
+#include "eval.h"
+#include "env.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// _setq
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ae_obj_t * ae_core_setq(ae_obj_t * const env, ae_obj_t * const args) {
+#ifdef AE_LOG_CORE
+  // No CORE_BEGIN!
+  LOG(args, "[core_setq]");
+  INDENT;
+#endif
+
+  int len = LENGTH(args);
+
+  REQUIRE(env, args, len >= 1, "setq requires at least 1 arg");
+  REQUIRE(env, args, len <= 2, "setq requires 1 or 2 args");
+
+  ae_obj_t * sym         = CAR(args);
+  ae_obj_t * val         = CADR(args);
+
+  REQUIRE(env, args, SYMBOLP(sym));
+  REQUIRE(env, args, ! KEYWORDP(sym), "keyword symbols are constant");
+  REQUIRE(env, args, sym != NIL,  "nil is a constant symbol");
+  REQUIRE(env, args, sym != TRUE, "t is a constant symbol");
+
+#ifdef AE_LOG_CORE
+  LOG(sym, "setting symbol");
+  LOG(val, "to value");
+#endif
+
+  ae_obj_t * setq_env = env;
+  
+#ifdef AE_CORE_ENVS
+  if (ENV_PARENT(env) != NIL)
+    setq_env = ENV_PARENT(env);
+#endif
+
+#ifdef AE_LOG_CORE
+  LOG(val, "evaluating 'value' argument");
+  INDENT;
+#endif
+
+  val = EVAL(setq_env, val);
+
+#ifdef AE_LOG_CORE
+  OUTDENT;
+  LOG(val, "evaluated 'value' argument is");
+#endif
+
+#ifdef AE_DEBUG_OBJ
+  if (LAMBDAP(val) || MACROP(val)) {
+    DSET(val, "last-bound-to", sym);
+
+#  ifdef AE_LOG_CORE
+    LOG(DOBJ(val), "core setq val's new debug data");
+#  endif
+
+  }
+#endif
+
+  ENV_SET(setq_env, sym, val);
+
+  CORE_RETURN("setq", val);
+}
+
