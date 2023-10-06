c93314108f110e114a13cad19b293bc1c61d14d5
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 21:10:39 -0400

    separata math ops

---
+++ include/core/core_util_macros.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string.h>
+
+#include "list.h"
+#include "alist.h"
+#include "obj.h"
+#include "free_list.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define REQUIRE(env, args, cond, ...)                                                              \
+  if (! (cond)) {                                                                                  \
+    char * fmt      = ("" __VA_ARGS__)[0]                                                          \
+      ? "%s:%d: \"Error in %s: require " #cond ", " __VA_ARGS__ "!\""                              \
+      : "%s:%d: \"Error in %s: require " #cond "!\"";                                              \
+    char * msg      = free_list_malloc(256);                                                       \
+    sprintf(msg, fmt, __FILE__, __LINE__, __func__);                                               \
+                                                                                                   \
+    ae_obj_t * err_data = NIL;                                                                     \
+                                                                                                   \
+    ASET(err_data, KW("args"), args);                                                              \
+    ASET(err_data, KW("env"),  env);                                                               \
+                                                                                                   \
+    return NEW_ERROR(err_data, msg);                                                               \
+  }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef AE_LOG_CORE
+#  define CORE_BEGIN(name)                                                                         \
+({                                                                                                 \
+  LOG(args, "[core_" name "]");                                                                    \
+  INDENT;                                                                                          \
+  LOG(env,  "in env");                                                                             \
+})
+#else
+#  define CORE_BEGIN(name) ((void)name)
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef AE_LOG_CORE
+#  define CORE_RETURN(name, val)                                                                   \
+({                                                                                                 \
+ OUTDENT;                                                                                          \
+ LOG_RETURN_WITH_TYPE("core_" name, val);                                                          \
+ return val;                                                                                       \
+})
+#else
+#  define CORE_RETURN(name, val) return ((val))
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/core/core.c
+++ src/core/core.c
@@ -13,40 +13,6 @@
 #include "core_util_macros.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// math
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// This only deals with AE_INTEGERS for now. It mutates its first argument.
-#define DEF_MATH_OP(name, oper, default)                                                           \
-ae_obj_t * ae_core_ ## name(ae_obj_t * const env, ae_obj_t * const args) {                         \
-  CORE_BEGIN(#name);                                                                               \
-  assert(CONSP(args));                                                                             \
-                                                                                                   \
-  int        accum = 0;                                                                            \
-  ae_obj_t * rest  = NIL;                                                                          \
-                                                                                                   \
-  if (NILP(CDR(args))) {                                                                           \
-    accum = default;                                                                               \
-    rest  = args;                                                                                  \
-  }                                                                                                \
-  else {                                                                                           \
-    REQUIRE(env, args, INTEGERP(CAR(args)));                                                       \
-                                                                                                   \
-    accum = INT_VAL(CAR(args));                                                                    \
-    rest  = CDR(args);                                                                             \
-  }                                                                                                \
-                                                                                                   \
-  FOR_EACH(elem, rest) {                                                                           \
-    REQUIRE(env, args, INTEGERP(elem));                                                            \
-    accum = accum oper INT_VAL(elem);                                                              \
-  }                                                                                                \
-                                                                                                   \
-  CORE_RETURN(#name, NEW_INT(accum));                                                              \
-}
-
-FOR_EACH_CORE_MATH_OP(DEF_MATH_OP);
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 // numeric comparison
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
