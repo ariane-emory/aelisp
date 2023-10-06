0bc1ecebfccc6711d13505149bf79a87c0b77625
Ariane Emory <ariane.emory@protonmail.com>
2023-10-06 01:46:08 -0400

    tidy

--- include/env.h
+++ include/env.h
@@ -27,31 +27,31 @@ _obj->values    = (values_);                                                    
 _obj;                                                                                                               \
 })                                                                                                                  
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_PARENT(env)          ((env)->parent)                                                                   //
-#define ENV_SYMS(env)            ((env)->symbols)                                                                  //
-#define ENV_VALS(env)            ((env)->values)                                                                   //
+#define    ENV_PARENT(env)          ((env)->parent)                                                                //
+#define    ENV_SYMS(env)            ((env)->symbols)                                                               //
+#define    ENV_VALS(env)            ((env)->values)                                                                //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_ADD(env, sym, val)   (ae_env_add(            (env), (sym), (val)))                                     //
+#define    ENV_ADD(env, sym, val)   (ae_env_add(            (env), (sym), (val)))                                  //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_SET(env, sym, val)   (ae_env_set(   NEAREST, (env), (sym), (val)))                                     //
-#define ENV_SET_G(env, sym, val) (ae_env_set(    GLOBAL, (env), (sym), (val)))                                     //
-#define ENV_SET_L(env, sym, val) (ae_env_set(     LOCAL, (env), (sym), (val)))                                     //
+#define    ENV_SET(env, sym, val)   (ae_env_set(   NEAREST, (env), (sym), (val)))                                  //
+#define    ENV_SET_G(env, sym, val) (ae_env_set(    GLOBAL, (env), (sym), (val)))                                  //
+#define    ENV_SET_L(env, sym, val) (ae_env_set(     LOCAL, (env), (sym), (val)))                                  //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_FIND(env, sym)       (ae_env_lookup(NEAREST, (env), (sym), NULL))                                      //
-#define ENV_FIND_G(env, sym)     (ae_env_lookup( GLOBAL, (env), (sym), NULL))                                      //
-#define ENV_FIND_L(env, sym)     (ae_env_lookup(  LOCAL, (env), (sym), NULL))                                      //
+#define    ENV_FIND(env, sym)       (ae_env_lookup(NEAREST, (env), (sym), NULL))                                   //
+#define    ENV_FIND_G(env, sym)     (ae_env_lookup( GLOBAL, (env), (sym), NULL))                                   //
+#define    ENV_FIND_L(env, sym)     (ae_env_lookup(  LOCAL, (env), (sym), NULL))                                   //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_BOUNDP(env, sym)                                                                                        \
+#define    ENV_BOUNDP(env, sym)                                                                                     \
   ({                                                                                                                \
   bool found = false;                                                                                               \
   ((void)ae_env_lookup(NEAREST, (env), (sym), &found));                                                             \
   found;                                                                                                            \
   })
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENV_NEW_ROOT()         (ae_env_new_root())                                                                 //
+#define    ENV_NEW_ROOT()         (ae_env_new_root())                                                              //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define ENVP(o)                ((! NULLP((o))) && (GET_TYPE((o)) == AE_ENV))                                       //
-#define ROOTP(env)             (ENVP((env)) && (NILP(ENV_PARENT((env)))))                                          //
+#define    ENVP(o)                ((! NULLP((o))) && (GET_TYPE((o)) == AE_ENV))                                    //
+#define    ROOTP(env)             (ENVP((env)) && (NILP(ENV_PARENT((env)))))                                       //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void       ae_env_add     (                        ae_obj_t * const env, ae_obj_t * const symbol, ae_obj_t * value );
 void       ae_env_set     (ae_env_set_mode_t mode, ae_obj_t * const env, ae_obj_t * const symbol, ae_obj_t * value );
