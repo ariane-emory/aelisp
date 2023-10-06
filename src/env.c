57964330ab6d7e44bc18e85441438dd4521c17dc
Ariane Emory <ariane.emory@protonmail.com>
2023-10-06 01:20:38 -0400

    change arg order

--- src/env.c
+++ src/env.c
@@ -34,7 +34,7 @@ void ae_env_add(ae_obj_t * const env, ae_obj_t * const symbol, ae_obj_t * const 
 // _find
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t * ae_env_lookup(ae_obj_t * const env, ae_env_set_mode_t mode, ae_obj_t * const symbol, bool * found) {
+ae_obj_t * ae_env_lookup(ae_env_set_mode_t mode, ae_obj_t * const env, ae_obj_t * const symbol, bool * found) {
 
   assert(ENVP(env));
   assert(SYMBOLP(symbol));
@@ -158,7 +158,7 @@ end:
 // _set
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void ae_env_set(ae_obj_t * const env, ae_env_set_mode_t mode, ae_obj_t * const symbol, ae_obj_t * const value) {
+void ae_env_set(ae_env_set_mode_t mode, ae_obj_t * const env, ae_obj_t * const symbol, ae_obj_t * const value) {
   assert(ENVP(env));
   assert(SYMBOLP(symbol));
   assert(! KEYWORDP(symbol));
