abddacbe415782cdb6b0be9b1f77fa9da9a77b83
Ariane Emory <ariane.emory@protonmail.com>
2023-10-05 22:22:48 -0400

    ...

--- src/core/core.c
+++ src/core/core.c
@@ -26,24 +26,3 @@ ae_obj_t * ae_core_dobj(ae_obj_t * const env, ae_obj_t * const args) {
   CORE_RETURN("dobj", NIL);
 #endif
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// _type
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-ae_obj_t * ae_core_type(ae_obj_t * const env, ae_obj_t * const args) {
-  CORE_BEGIN("type");
-
-  REQUIRE(env, args, (LENGTH(args) == 1));
-
-  const char * type = GET_TYPE_STR(CAR(args));
-  /* */ char * tmp  = free_list_malloc(strlen(type) + 2);
-
-  sprintf(tmp, ":%s", type);
-
-  ae_obj_t   * sym  = SYM(tmp);
-
-  free_list_free(tmp);
-
-  CORE_RETURN("type", sym);
-}
