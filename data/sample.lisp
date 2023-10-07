7c3f561e5e362117600fafaeb332f64932085e80
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 09:32:16 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -12,7 +12,7 @@
 
 ;; (setq cadr     (lambda (x)   (car  (cdr x))))
 
-(print
+;; (print
 (let* ((n 30)
       (*memo*  '(2 1 1 1))
       (memoize  (lambda (k v) (car (cdr (setq *memo* (pset *memo* k v))))))
@@ -23,8 +23,8 @@
                      ;; ((<= n 2) 1)
                      (t (memoize n (+ (fib (- n 1)) (fib (- n 2)))))))))
        (result  (fib n)))
-result)
-)
+  result)
+;;)
 
 ;;(fib 20)
 
