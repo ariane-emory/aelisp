78c0a0020667c08a684f5c18f3c4c743be630c9c
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 15:58:52 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -25,8 +25,8 @@
        (*memo* '((2 . 1) (1 . 1)))
        (memoize (λ (k v) (cdr (car (≔    *memo* (aset *memo* k v))))))
        (𝑓       (λ (n)
-                   (let ((memoized (aget *memo* n)))
-                     (∨   memoized
+                   (let  ((memoized (aget *memo* n)))
+                     (∨    memoized
                           (memoize n (+ (𝑓 (- n 1))
                                         (𝑓 (- n 2))))))))
        (result  (𝑓 n)))
