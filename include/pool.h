a0f16d4bc6a5959e9164bdac31bf24106f9704c2
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 02:04:29 -0400

    ...

--- include/pool.h
+++ include/pool.h
@@ -26,11 +26,11 @@
 extern const struct ae_obj_t * const pool_first;
 extern const struct ae_obj_t * const pool_last;
 
-struct ae_obj_t * pool_alloc_ae_obj   (void);
-void              pool_free_ae_obj    (struct ae_obj_t * const this);
-void              pool_print          (void);
-void              pool_clear          (void);
-struct ae_obj_t * pool_delocalize_ptr (struct ae_obj_t * const ptr);
-struct ae_obj_t * pool_localize_ptr   (struct ae_obj_t * const ptr, struct ae_obj_t * const offset);
+struct ae_obj_t * pool_alloc_ae_obj      (void);
+void              pool_free_ae_obj       (struct ae_obj_t * const this);
+void              pool_print             (void);
+void              pool_clear             (void);
+struct ae_obj_t * pool_delocalize_ptr    (struct ae_obj_t * const ptr);
+struct ae_obj_t * pool_localize_ptr      (struct ae_obj_t * const ptr, struct ae_obj_t * const offset);
 void              pool_dset_all_allocated(struct ae_obj_t * const key, struct ae_obj_t * const value);
-struct ae_obj_t * pool_get_object     (int const index);
+struct ae_obj_t * pool_get_object        (int const index);
