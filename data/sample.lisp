4039c9397950b71138755c4070ad061441a3285f
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 10:20:48 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -5,29 +5,23 @@
 ;;     1
 ;;     (+ (fib (- n 1)) (fib (- n 2))))))
 
-;; (setq cond-fib (lambda (n)
-;;             (cond
-;;               ((<= n 2) 1)
-;;               (t (+ (fib (- n 1)) (fib (- n 2)))))))
-
-;; (setq cadr     (lambda (x)   (car  (cdr x))))
-
 ;; (print
-;;  (let* ((n 30)
-;;         (*memo*  '(2 1 1 1))
-;;         (memoize  (lambda (k v)
-;;                     (let ((r (car (cdr (setq *memo* (pset *memo* k v))))))
-;;                       (print *memo*)
-;;                       (print r)
-;;                       r)))
-;;         (fib      (lambda (n)
-;;                     (let ((memoized (pget *memo* n)))
-;;                       (cond
-;;                         (memoized memoized)
-;;                         ;; ((<= n 2) 1)
-;;                         (t (memoize n (+ (fib (- n 1)) (fib (- n 2)))))))))
-;;         (result  (fib n)))
-;;    result))
+ ;; (let* ((n 30)
+ ;;        (*memo*  '(2 1 1 1))
+ ;;        (memoize  (lambda (k v)
+ ;;                    (let ((r (car (cdr (setq *memo* (pset *memo* k v))))))
+ ;;                      (print *memo*)
+ ;;                      (print r)
+ ;;                      r)))
+ ;;        (fib      (lambda (n)
+ ;;                    (let ((memoized (pget *memo* n)))
+ ;;                      (cond
+ ;;                        (memoized memoized)
+ ;;                        ;; ((<= n 2) 1)
+ ;;                        (t (memoize n (+ (fib (- n 1)) (fib (- n 2)))))))))
+ ;;        (result  (fib n)))
+ ;;   result)
+;; )
 
 ;; (print
  (let* ((n 30)
