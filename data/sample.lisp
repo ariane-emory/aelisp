f9999881c3c95757cbe1f0cb7d9b0528c65d6748
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 09:17:27 -0400

    fastest yet

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,3 +1,5 @@
+;; mcm; time { for i in {1..10000}; do ./bin/ae; done; }
+
 ;; (setq naive-fib (lambda (n)
 ;;   (if (<= n 2)
 ;;     1
@@ -10,15 +12,18 @@
 
 ;; (setq cadr     (lambda (x)   (car  (cdr x))))
 
-(print (let ((n 30)
-             (memoize (lambda (k v) (car (cdr (setq *memo* (pset *memo* k v))))))
-             (fib     (lambda (n)
-                        (let ((memoized  (pget *memo* n)))
-                          (cond
-                            (memoized memoized)
-                            ((<= n 2) 1)
-                            (t (memoize n (+ (fib (- n 1)) (fib (- n 2))))))))))
-         (fib n)))
+;; (print
+(let ((n 30)
+      (*memo* '(2 1 1 1))
+      (memoize (lambda (k v) (car (cdr (setq *memo* (pset *memo* k v))))))
+      (fib     (lambda (n)
+                 (let ((memoized (pget *memo* n)))
+                   (cond
+                     (memoized memoized)
+                     ;; ((<= n 2) 1)
+                     (t (memoize n (+ (fib (- n 1)) (fib (- n 2))))))))))
+  (fib n))
+;; )
 
 ;;(fib 20)
 
