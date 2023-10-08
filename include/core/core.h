12cfa79cd3ff49d7bfe90b22efe41532b3fcb708
Ariane Emory <ariane.emory@protonmail.com>
2023-10-07 20:16:36 -0400

    ...

--- include/core/core.h
+++ include/core/core.h
@@ -2,61 +2,63 @@
 
 #include "obj.h"
 
+#define FUNDEF_END NULL
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define FOR_EACH_CORE_FUN(DO)                                                                                          \
   /*================================================================================================================*/ \
-  DO(exit,     false)                                                                                                  \
-  DO(msleep,   false)                                                                                                  \
-  DO(dobj,     false)                        /* debug data accessor   */                                               \
-  DO(errmsg,   false)                        /* error accessor        */                                               \
-  DO(errobj,   false)                        /* error accessor        */                                               \
-  DO(numer,    false)                        /* rational/int accessor */                                               \
-  DO(denom,    false)                        /* rational/int accessor */                                               \
-  DO(body,     false)                        /* fun accessor          */                                               \
-  DO(params,   false)                        /* fun accessor          */                                               \
-  DO(env,      false)                        /* env/fun accessor      */                                               \
-  DO(vals,     false)                        /* env accessor          */                                               \
-  DO(syms,     false)                        /* env accessor          */                                               \
-  DO(properp,  false)                        /* reduceable            */                                              \
-  DO(tailp,    false)                        /* reduceable            */                                               \
-  DO(boundp,   false)                                                                                                  \
-  DO(nl,       false)                                                                                                  \
+  DO(exit,     false,                                                         FUNDEF_END )                             \
+  DO(msleep,   false,                                                         FUNDEF_END )                             \
+  DO(dobj,     false,                                                         FUNDEF_END ) /* debug data accessor   */ \
+  DO(errmsg,   false,                                                         FUNDEF_END ) /* error accessor        */ \
+  DO(errobj,   false,                                                         FUNDEF_END ) /* error accessor        */ \
+  DO(numer,    false,                                                         FUNDEF_END ) /* rational/int accessor */ \
+  DO(denom,    false,                                                         FUNDEF_END ) /* rational/int accessor */ \
+  DO(body,     false,                                                         FUNDEF_END ) /* fun accessor          */ \
+  DO(params,   false,                                                         FUNDEF_END ) /* fun accessor          */ \
+  DO(env,      false,                                                         FUNDEF_END ) /* env/fun accessor      */ \
+  DO(vals,     false,                                                         FUNDEF_END ) /* env accessor          */ \
+  DO(syms,     false,                                                         FUNDEF_END ) /* env accessor          */ \
+  DO(properp,  false,                                                         FUNDEF_END ) /* reduceable            */ \
+  DO(tailp,    false,                                                         FUNDEF_END ) /* reduceable            */ \
+  DO(boundp,   false,                                                         FUNDEF_END )                             \
+  DO(nl,       false,                                                         FUNDEF_END )                             \
   /*================================================================================================================*/ \
-  DO(put,      false)                                                                                                  \
-  DO(princ,    false)                                                                                                  \
-  DO(print,    false)                                                                                                  \
-  DO(write,    false)                                                                                                  \
+  DO(put,      false,                                                         FUNDEF_END )                             \
+  DO(princ,    false,                                                         FUNDEF_END )                             \
+  DO(print,    false,                                                         FUNDEF_END )                             \
+  DO(write,    false,                                                         FUNDEF_END )                             \
   /*================================================================================================================*/ \
-  DO(type,     false)                             /* GET_TYPE proxy */                                                 \
-  DO(length,   false)                             /* reduceable     */                                                 \
-  DO(rplacd,   false)                                                                                                  \
-  DO(rplaca,   false)                                                                                                  \
-  DO(ahas,     false)                                                                                                  \
-  DO(aset,     false)                                                                                                  \
-  DO(aget,     false)                                                                                                  \
-  DO(phas,     false)                                                                                                  \
-  DO(pset,     false)                                                                                                  \
-  DO(pget,     false)                                                                                                  \
-  DO(not,      false)                             /* reduceable     */                                                 \
-  DO(eql,      false)                             /* reduceable     */                                                 \
-  DO(eq,       false)                                                                                                  \
-  DO(cons,     false)                                                                                                  \
-  DO(cdr,      false)                                                                                                  \
-  DO(car,      false)                                                                                                  \
-  DO(eval,     false)                                                                                                  \
-  DO(list,     false)                                                                                                  \
-  DO(set,      false)                                                                                                  \
-  DO(progn,    true )                                                                                                  \
-  DO(setq,     true )                                                                                                  \
-  DO(quote,    true )                                                                                                  \
-  DO(macro,    true )                                                                                                  \
-  DO(lambda,   true )                                                                                                  \
-  DO(cond,     true )                                                                                                  \
-  DO(let,      true )                                                                                                  \
-  DO(let_star, true )                                                                                                  \
-  DO(if,       true )                        /* reduceable */                                                          \
-  DO(or,       true )                        /* reduceable */                                                          \
-  DO(and,      true )                        /* reduceable */                                                          \
+  DO(type,     false,                                                         FUNDEF_END )        /* GET_TYPE proxy */ \
+  DO(length,   false,                                                         FUNDEF_END )            /* reduceable */ \
+  DO(rplacd,   false,                                                         FUNDEF_END )                             \
+  DO(rplaca,   false,                                                         FUNDEF_END )                             \
+  DO(ahas,     false,                                                         FUNDEF_END )                             \
+  DO(aset,     false,                                                         FUNDEF_END )                             \
+  DO(aget,     false,                                                         FUNDEF_END )                             \
+  DO(phas,     false,                                                         FUNDEF_END )                             \
+  DO(pset,     false,                                                         FUNDEF_END )                             \
+  DO(pget,     false,                                                         FUNDEF_END )                             \
+  DO(not,      false,                                                         FUNDEF_END )            /* reduceable */ \
+  DO(eql,      false,                                                         FUNDEF_END )            /* reduceable */ \
+  DO(eq,       false,                                                         FUNDEF_END )                             \
+  DO(cons,     false,                                                         FUNDEF_END )                             \
+  DO(cdr,      false,                                                         FUNDEF_END )                             \
+  DO(car,      false,                                                         FUNDEF_END )                             \
+  DO(eval,     false,                                                         FUNDEF_END )                             \
+  DO(list,     false,                                                         FUNDEF_END )                             \
+  DO(set,      false,                                                         FUNDEF_END )                             \
+  DO(progn,    true,                                                          FUNDEF_END )                             \
+  DO(setq,     true,                                                          FUNDEF_END )                             \
+  DO(quote,    true,                                                          FUNDEF_END )                             \
+  DO(macro,    true,                                                          FUNDEF_END )                             \
+  DO(lambda,   true,                                                          FUNDEF_END )                             \
+  DO(cond,     true,                                                          FUNDEF_END )                             \
+  DO(let,      true,                                                          FUNDEF_END )                             \
+  DO(let_star, true,                                                          FUNDEF_END )                             \
+  DO(if,       true,                                                          FUNDEF_END )            /* reduceable */ \
+  DO(or,       true,                                                          FUNDEF_END )            /* reduceable */ \
+  DO(and,      true,                                                          FUNDEF_END )            /* reduceable */ \
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
