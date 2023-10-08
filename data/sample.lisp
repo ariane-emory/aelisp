8b3b3b0811729272f74fb4ca674b5b73b38a49fe
Ariane Emory <ariane.emory@protonmail.com>
2023-10-08 17:15:21 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -41,16 +41,33 @@
           obj)))
 
 
-;; (setq l '(2 (4 8)))
-;; (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) l)
-;; (print l) ;; sucessfully prints (4 (8 16)).
 
-;; (setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 8))))
-;; (print l) ;; successfully prints (4 (8 16)).
+(setq transform!
+  (lambda (pred fun obj)
+    (cond
+      ((pred obj) (setf obj (fun obj)))
+      ((eq :CONS (type obj))
+       (let ((head (car obj))
+             (tail (cdr obj)))
+         (cond
+           ((pred head) (rplaca obj (fun head)))
+           ((eq :CONS (type head)) (transform! pred fun head)))         
+         (if tail (rplacd obj (transform! pred fun tail)))
+         (if (and (not (eq :CONS (type tail))) (pred tail))
+             (rplacd obj (fun tail)))))
+      (t obj))
+    obj)) 
+
+(setq l '(2 (4 8)))
+(transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) l)
+(print l) ;; sucessfully prints (4 (8 16)).
+
+(setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 8))))
+(print l) ;; successfully prints (4 (8 16)).
 
 (setq l '(2 (4 . a)))
 (transform! (lambda (obj) (eq :INTEGER (type obj))) (lambda (num) (* 2 num)) l)
-(print l) ;; fails to print (4 (8 . 16)), object pool is filled up somehow!
+(print l) ;; should print (4 (8 . 16))
 
-;; (setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 . 8))))
-;; (print l) ;; fails to print (4 (8 . 16))!
+(setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 . 8))))
+(print l) ;; should (4 (8 . 16))!
