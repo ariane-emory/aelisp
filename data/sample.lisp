5a10822cfd875bbc50bc9f73787f554eb4ef44cf
Ariane Emory <ariane.emory@protonmail.com>
2023-10-08 18:32:29 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -4,58 +4,58 @@
         (if (not (eq :CONS (type obj))) 
             (if (pred obj) 
                 (setf obj (fun obj)))
-          (let ((head (car obj))
-                (tail (cdr obj)))
-            (cond 
-              ((nil? obj) obj)
-              ((pred head)
-               (rplaca obj (fun head)))
-              ((eq :CONS (type head))
-               (transform! pred fun head)))
-            (if tail
-                (rplacd obj (transform! pred fun tail)))
-            (if (and (not (eq :CONS (type tail))) (pred tail))
-                (rplacd obj (fun tail)))))
+            (let ((head (car obj))
+                  (tail (cdr obj)))
+              (cond 
+                ((nil? obj) obj)
+                ((pred head)
+                 (rplaca obj (fun head)))
+                ((eq :CONS (type head))
+                 (transform! pred fun head)))
+              (if tail
+                  (rplacd obj (transform! pred fun tail)))
+              (if (and (not (eq :CONS (type tail))) (pred tail))
+                  (rplacd obj (fun tail)))))
         obj))
 
 ;; verbose version
 (setq transform!
       (lambda (pred fun obj)
         (let ((process-cons
-               (lambda (obj)
-                 (let ((head (car obj))
-                       (tail (cdr obj)))
-                   (when (pred head)
-                     (rplaca obj (fun head)))
-                   (when (eq :CONS (type head))
-                     (transform! pred fun head))
-                   (when tail
-                     (rplacd obj (transform! pred fun tail)))
-                   (when (and (not (eq :CONS (type tail))) (pred tail))
-                     (rplacd obj (fun tail)))))))
+                (lambda (obj)
+                  (let ((head (car obj))
+                        (tail (cdr obj)))
+                    (when (pred head)
+                      (rplaca obj (fun head)))
+                    (when (eq :CONS (type head))
+                      (transform! pred fun head))
+                    (when tail
+                      (rplacd obj (transform! pred fun tail)))
+                    (when (and (not (eq :CONS (type tail))) (pred tail))
+                      (rplacd obj (fun tail)))))))
           (cond 
-           ((and (not (eq :CONS (type obj))) (pred obj))
-            (setf obj (fun obj)))
-           ((eq :CONS (type obj))
-            (process-cons obj)))
+            ((and (not (eq :CONS (type obj))) (pred obj))
+             (setf obj (fun obj)))
+            ((eq :CONS (type obj))
+             (process-cons obj)))
           obj)))
 
 ;; short version
 (setq transform!
-  (lambda (pred fun obj)
-    (cond
-      ((pred obj) (setf obj (fun obj)))
-      ((eq :CONS (type obj))
-       (let ((head (car obj))
-             (tail (cdr obj)))
-         (cond
-           ((pred head) (rplaca obj (fun head)))
-           ((eq :CONS (type head)) (transform! pred fun head)))         
-         (if tail (rplacd obj (transform! pred fun tail)))
-         (if (and (not (eq :CONS (type tail))) (pred tail))
-             (rplacd obj (fun tail)))))
-      (t obj))
-    obj)) 
+      (lambda (pred fun obj)
+        (cond
+          ((pred obj) (setf obj (fun obj)))
+          ((eq :CONS (type obj))
+           (let ((head (car obj))
+                 (tail (cdr obj)))
+             (cond
+               ((pred head) (rplaca obj (fun head)))
+               ((eq :CONS (type head)) (transform! pred fun head)))         
+             (if tail (rplacd obj (transform! pred fun tail)))
+             (if (and (not (eq :CONS (type tail))) (pred tail))
+                 (rplacd obj (fun tail)))))
+          (t obj))
+        obj)) 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -65,20 +65,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq transform!
-  (lambda (pred fun obj)
-    (cond
-      ((pred obj) (setf obj (fun obj)))
-      ((eq :CONS (type obj))
-       (let ((head (car obj))
-             (tail (cdr obj)))
-         (cond
-           ((pred head) (rplaca obj (fun head)))
-           ((eq :CONS (type head))  (transform! pred fun head)))
-         (cond
-           ((pred tail) (rplacd obj (fun tail)))
-           ((eq :CONS (type tail))  (rplacd obj (transform! pred fun tail))))))
-      (t obj))
-    obj))
+      (lambda (pred fun obj)
+        (if (not (eq :CONS (type obj)))
+            (error "obj must be a list")
+            (cond
+              ((pred obj) (setf obj (fun obj)))
+              ((eq :CONS (type obj))
+               (let ((head (car obj))
+                     (tail (cdr obj)))
+                 (cond
+                   ((pred head) (rplaca obj (fun head)))
+                   ((eq :CONS (type head))  (transform! pred fun head)))
+                 (cond
+                   ((pred tail) (rplacd obj (fun tail)))
+                   ((eq :CONS (type tail))  (rplacd obj (transform! pred fun tail))))))
+              (t obj))
+            obj)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
