ae7540233ff65dd5eb3b61e36624b843aad93572
Ariane Emory <ariane.emory@protonmail.com>
2023-10-08 16:05:07 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -16,16 +16,16 @@
               (rplacd lst (fun tail))))
         lst))
 
-(setq l '(2 (4 8)))
-(transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) l)
-(print l) ;; sucessfully prints (4 (8 16)).
-
-(setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 8))))
-(print l) ;; successfully prints (4 (8 16)).
-
-;; (setq l '(2 (4 . 8)))
+;; (setq l '(2 (4 8)))
 ;; (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) l)
-;; (print l) ;; fails to print (4 (8 . 16)), object pool is filled up somehow!
+;; (print l) ;; sucessfully prints (4 (8 16)).
+
+;; (setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 8))))
+;; (print l) ;; successfully prints (4 (8 16)).
+
+(setq l '(2 (4 . 8)))
+(transform! (lambda (obj) (eq :INTEGER (type obj))) (lambda (num) (* 2 num)) l)
+(print l) ;; fails to print (4 (8 . 16)), object pool is filled up somehow!
 
 ;; (setq l (transform! (lambda (x) (eq :INTEGER (type x))) (lambda (x) (* 2 x)) '(2 (4 . 8))))
 ;; (print l) ;; fails to print (4 (8 . 16))!
