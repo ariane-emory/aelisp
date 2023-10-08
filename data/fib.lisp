1a6eeacded16dbb0732d8ba3529a4c743d5c213e
Ariane Emory <ariane.emory@protonmail.com>
2023-10-08 17:16:21 -0400

    ...

---
+++ data/fib.lisp
@@ -0,0 +1,21 @@
+;; mcm; time { for i in {1..10000}; do ./bin/ae; done; }
+
+;; (setq naive-fib (lambda (n)
+;;   (if (<= n 2)
+;;     1
+;;     (+ (fib (- n 1)) (fib (- n 2))))))
+
+(let* ((𝑛 30)
+       (∨ ∨)
+       (*memo* '((2 . 1) (1 . 1)))
+       (memoize (λ (k v) (cdr (car (≔    *memo* (aset *memo* k v))))))
+       (𝑓       (λ (𝑥)
+                  (let  ((memoized (aget *memo*  𝑥)))
+                    (∨    memoized
+                          (memoize  𝑥 (+ (𝑓 (- 𝑥 1))
+                                         (𝑓 (- 𝑥 2)))))))))
+  (𝑓 𝑛))
+
+;; (print (<< 10))
+
+;; (print (¬ t))
