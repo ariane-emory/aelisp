948b04241138ee5e5fb976c1a73e8f63325ce132
Ariane Emory <ariane.emory@protonmail.com>
2023-10-09 03:57:43 -0400

    ...

--- src/core/core_sys_funcs.c
+++ src/core/core_sys_funcs.c
@@ -1,6 +1,21 @@
 #include <unistd.h>
-
+#include <time.h>
 #include "core_includes.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// _time
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ae_obj_t * ae_core_time(ae_obj_t * const env, ae_obj_t * const args) {
+  CORE_BEGIN("time");
+  REQUIRE(env, args, NILP(args), "time takes no arguments");
+
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  long long elapsed = ((long long) ts.tv_sec * 1000LL + ts.tv_nsec / 1000LL);
+
+  CORE_RETURN("time", NEW_INT(elapsed));
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // _msleep
