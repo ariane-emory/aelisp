b2aca9d2ec9a3acec35b9179b3f0bac0e8292d10
Ariane Emory <ariane.emory@protonmail.com>
2023-10-08 21:16:35 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,66 +1,69 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; -*-lisp-indent-function: 1; -*-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;; -*-lisp-indent-function: 1; -*-
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq double   (lambda (x) (* 2 x)))
-(setq integer? (lambda (x) (eq :INTEGER (type x))))
+;; (setq double   (lambda (x) (* 2 x)))
+;; (setq integer? (lambda (x) (eq :INTEGER (type x))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq transform!
-  (lambda (pred fun obj)
-    (if (not (eq :CONS (type obj)))
-        (error "obj must be a list")
-        (cond
-          ((pred obj) (setf obj (fun obj)))
-          ((eq :CONS (type obj))
-           (let ((head (car obj))
-                 (tail (cdr obj)))
-             (cond
-               ((pred head) (rplaca obj (fun head)))
-               ((eq :CONS (type head))  (transform! pred fun head)))
-             (cond
-               ((pred tail) (rplacd obj (fun tail)))
-               ((eq :CONS (type tail))  (rplacd obj (transform! pred fun tail))))))
-          (t obj))
-        obj)))
+;; (setq transform!
+;;   (lambda (pred fun obj)
+;;     (if (not (eq :CONS (type obj)))
+;;         (error "obj must be a list")
+;;         (cond
+;;           ((pred obj) (setf obj (fun obj)))
+;;           ((eq :CONS (type obj))
+;;            (let ((head (car obj))
+;;                  (tail (cdr obj)))
+;;              (cond
+;;                ((pred head) (rplaca obj (fun head)))
+;;                ((eq :CONS (type head))  (transform! pred fun head)))
+;;              (cond
+;;                ((pred tail) (rplacd obj (fun tail)))
+;;                ((eq :CONS (type tail))  (rplacd obj (transform! pred fun tail))))))
+;;           (t obj))
+;;         obj)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq l '(2 (4 8)))
-(transform! integer? double l)
-(print l) ;; sucessfully prints (4 (8 16)).
+;; (setq l '(2 (4 8)))
+;; (transform! integer? double l)
+;; (print l) ;; sucessfully prints (4 (8 16)).
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq l (transform! integer? double '(2 (4 8))))
-(print l) ;; successfully prints (4 (8 16)).
+;; (setq l (transform! integer? double '(2 (4 8))))
+;; (print l) ;; successfully prints (4 (8 16)).
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq l '(2 (4 . a)))
-(transform! (lambda (obj) (eq :INTEGER (type obj))) (lambda (num) (* 2 num)) l)
-(print l) ;; successdully prints (4 (8 . a))
+;; (setq l '(2 (4 . a)))
+;; (transform! (lambda (obj) (eq :INTEGER (type obj))) (lambda (num) (* 2 num)) l)
+;; (print l) ;; successdully prints (4 (8 . a))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq l (transform! integer? double '(2 (4 . 8))))
-(print l) ;; successfully prints (4 (8 . 16))!
+;; (setq l (transform! integer? double '(2 (4 . 8))))
+;; (print l) ;; successfully prints (4 (8 . 16))!
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq l 2)
-(transform! integer? double l)
-(print l) ;; should return an :ERROR
+;; (setq l 2)
+;; (transform! integer? double l)
+;; (print l) ;; should return an :ERROR
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; mcm; time { for i in {1..10000}; do ./bin/ae; done; }
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq memo-fib (lambda (n)
-                 (let ((memoized (pget *memo* n))
-                       (memoize  (lambda (k v) (car (cdr (setq *memo* (pset *memo* k v)))))))
-                   (cond
-                     (memoized  memoized)
-                     ((<= n 2)  1)
-                     (t (progn (memjoize n (+ (memo-fib (- n 1)) (memo-fib (- n 2))))))))))
+(let* ((𝑛 30)
+       (∨ ∨)
+       (*memo* '((2 . 1) (1 . 1)))
+       (memoize (lambda (k v) (cdr (car (≔    *memo* (aset *memo* k v))))))
+       (𝑓       (lambda (𝑥)
+                  (let  ((memoized (aget *memo*  𝑥)))
+                    (∨    memoized
+                          (memoize  𝑥 (+ (𝑓 (- 𝑥 1))
+                                         (𝑓 (- 𝑥 2)))))))))
+  (𝑓 𝑛))
