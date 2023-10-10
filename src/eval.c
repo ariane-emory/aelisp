6c751ee46cf5f477e9ed3b5e84be3e95c3638767
Ariane Emory <ariane.emory@protonmail.com>
2023-10-09 22:23:51 -0400

    ...

--- src/eval.c
+++ src/eval.c
@@ -99,7 +99,15 @@ static ae_obj_t * apply_core(ae_obj_t * env, ae_obj_t * fun, ae_obj_t * args) {
               CORE_NAME(fun),
               CORE_MIN_ARGS(fun),
               LENGTH(args));
-    else
+    else if (CORE_MAX_ARGS(fun) == CORE_MIN_ARGS(fun))
+      sprintf(msg_tmp, "%s:%d: core '%s' requires %d arg%ss, but got %d",
+              __FILE__,
+              __LINE__,
+              CORE_NAME(fun),
+              CORE_MIN_ARGS(fun),
+              s_or_blank(CORE_MIN_ARGS(fun)),
+              LENGTH(args));
+    else 
       sprintf(msg_tmp, "%s:%d: core '%s' requires %d to %d args, but got %d",
               __FILE__,
               __LINE__,
