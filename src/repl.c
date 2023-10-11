b2961f558e9a820b184b1960b1d6fc73592904b7
Ariane Emory <ariane.emory@protonmail.com>
2023-10-11 15:23:55 -0400

    ...

--- src/repl.c
+++ src/repl.c
@@ -4,16 +4,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "common.inc"
 
 ////////////////////////////////////////////////////////////////////////////////
 
+ae_obj_t * root_env = NIL;
+
+////////////////////////////////////////////////////////////////////////////////
+
+char *snip(const char * const buf) {
+  // Get the last word if point is on a word.
+  // This could be shorter but it's good enough for now.
+  
+  const char * const delimiters = " \t\n\r()";
+  const char *       end        = buf + strlen(buf) - 1;
+
+  while (end >= buf && strchr(delimiters, *end)) {
+    if (isspace(*end))
+      return calloc(1, sizeof(char)); 
+    end--;
+  }
+
+  const char * start = end;
+  while (start >= buf && !strchr(delimiters, *start))
+    start--;
+
+  start++; 
+
+  int          length = end - start + 1;
+  char * const result = calloc(length + 1, sizeof(char));
+  strncpy(result, start, length);
+
+  return result;
+}
+
 void completion(const char * buf, bestlineCompletions * lc) {
-  /* if (buf[0] == 'h') { */
-  /*     bestlineAddCompletion(lc,"hello"); */
-  /*     bestlineAddCompletion(lc,"hello there"); */
-  /* } */
+  const char * snipped     = snip(buf);
+  int          snipped_len = strlen(snipped);
+
+  if (snipped_len != 0) 
+    FOR_EACH(sym, ENV_SYMS(root_env))
+      if (! strncmp(snipped, SYM_VAL(sym), strlen(snipped))) {
+
+#ifdef AE_LOG_REPL
+        PR("\nsnipped_len %d\n", snipped_len);
+        PR("Buf     '%s'\nMatched '%s'.\n", buf, SYM_VAL(sym));
+#endif
+        
+        char * const match      = SYM_VAL(sym);
+        int          match_len  = strlen(match);
+        int          prefix_len = strlen(buf) - strlen(snipped);
+        int          needed_len = prefix_len + match_len + 2;
+
+#ifdef AE_LOG_REPL
+        PR("match_len  %d\n", match_len);
+        PR("prefix_len %d\n", prefix_len);
+        PR("needed_len %d\n", needed_len);
+#endif
+        char * const completed    = malloc(needed_len);
+
+        // memset(completed, 0, needed_len);
+      
+        strncpy(completed,              buf,   prefix_len);
+        strncpy(completed + prefix_len, match, match_len);
+        completed[needed_len - 2] = ' ';
+        completed[needed_len - 1] = '\0';
+
+#ifdef AE_LOG_REPL
+        PR("Completed\n%s\n",  completed);
+#endif
+        
+        bestlineAddCompletion(lc, completed);
+      }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -29,26 +93,28 @@ char *hints(const char * buf, const char ** ansi1, const char ** ansi2) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t * load_and_evaluate_file(const char * filename, ae_obj_t * env) {
-    FILE * original_yyin = yyin;
-    yyin = fopen(filename, "r");
+ae_obj_t * load_file(ae_obj_t * env, const char * filename) {
+  FILE * original_yyin = yyin;
+  yyin = fopen(filename, "r");
 
-    if (!yyin) {
-        PR("Failed to open file '%s'.\n", filename);
+  program = NIL;
+
+  if (!yyin) {
+    PR("Failed to open file '%s'.\n", filename);
         
-        return NIL; // maybe return an ERROR instead.
-    }
+    return NIL; // maybe return an ERROR instead.
+  }
 
-    yyrestart(yyin);
-    yyparse();
+  yyrestart(yyin);
+  yyparse();
 
-    ae_obj_t * ret = EVAL(env, program);
+  ae_obj_t * ret = EVAL(env, program);
 
-    fclose(yyin);
+  fclose(yyin);
 
-    yyin = original_yyin;
+  yyin = original_yyin;
 
-    return ret; 
+  return ret; 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -62,9 +128,10 @@ int main(int argc, char **argv) {
 ////////////////////////////////////////////////////////////////////////////////
   
   preface();
+
   NL;
-  ae_obj_t * root_env = setup_root_env();
-  
+  root_env = setup_root_env();
+
 ////////////////////////////////////////////////////////////////////////////////
 // setup bestline stuff
 ////////////////////////////////////////////////////////////////////////////////
@@ -87,22 +154,63 @@ int main(int argc, char **argv) {
    * The call to bestline() will block as long as the user types something
    * and presses enter.
    *
-   * The typed string is returned as a malloc() allocated string by
+   * The typed string is returned aks a malloc() allocated string by
    * bestline, so the user needs to free() it. */
     
   while((line = bestline("Æ> ")) != NULL) {
-    /* Do something with the string. */
-    if (line[0] != '\0' && line[0] != ';') {
+    if (! strncmp(line, "(load", 5)) {
+      char filename[256];
+      
+      if (sscanf(line, "(load \"%255[^\"]\")", filename) == 1)
+        load_file(root_env, filename);
+      else
+        printf("Error: Malformed load command.\n");
+    } 
+    else if (line[0] != '\0' && line[0] != ';') {
       program = NIL;
+
       parse_line(line);
+
       ae_obj_t * ret = EVAL(root_env, program);
-      printf("\n⇒ "), WRITE(ret);
+      
+      printf(" ⇒ ");
+      WRITE(ret);
       NL;
-      bestlineHistoryAdd(line); /* Add to the history. */
-      bestlineHistorySave("repl_history.txt"); /* Save the history on disk. */
-    } else if (line[0] == ';' && line[1] == 'q') {
+      
+      bestlineHistoryAdd(line); 
+      bestlineHistorySave("repl_history.txt");
+    }
+    else if (!strncmp(line, ";l ", 3)) {
+      char filename[256];
+      
+      if (sscanf(line, ";l %255s", filename) == 1)
+        load_file(root_env, filename);
+      else
+        printf("Error: Malformed load command.\n");
+    }
+    else if (! strncmp(line, ";p", 2)) {
+      pool_print();
+      NL;
+    }
+    else if (! strncmp(line, ";q", 2)) {
+      PR("Bye!");
+      NL;
       exit(0);
-    } else if (line[0] == ';') {
+    }
+    else if (! strncmp(line, ";r", 2)) {
+      pool_clear();
+      symbols_list = NIL;
+      root_env = setup_root_env();
+    }
+    else if (! strncmp(line, ";s", 2)) {
+      WRITE(ENV_SYMS(root_env));
+      NL;
+    }
+    else if (! strncmp(line, ";v", 2)) {
+      WRITE(ENV_VALS(root_env));
+      NL;
+    }
+    else if (line[0] == ';') {
       fputs("Unreconized command: ", stdout);
       fputs(line, stdout);
       fputs("\n", stdout);
