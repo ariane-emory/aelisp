5a7107b3e0ac4d46bea5935319fcb2797873b4f2
Ariane Emory <ariane.emory@protonmail.com>
2023-10-12 14:26:15 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,4 +1,4 @@
-;; (filter odd? '(1 2 3 4 5 6 7 8 9 10))   ; Expected result: (1 3 5 7 9)
+o;; (filter odd? '(1 2 3 4 5 6 7 8 9 10))   ; Expected result: (1 3 5 7 9)
 
 ;; (write (filter (lambda (x) (not (nil? x))) '(a nil b c nil d)))   ; Expected result: (a b c d)
 ;; (princ "one:   ") (write (push-back! lst 5))    (nl)
@@ -7,48 +7,45 @@
 
 ;; I added while and apply, they seem to work:
 
-(nil? nil)
-(nil? 1)
-
-;; (setq! ct 8)
-
-;; (while (> ct 0)
-;;   (write ct)
-;;   (setq! ct (- ct 1)))
 
 ;; (write (apply + '(1 2 3)))
 
 ;; ;; I'm trying this nconc!:
 
-;; (setq! last
-;;   (lambda (lst)
-;;     (cond
-;;       ((nil? lst) nil)
-;;       ((atom? lst) lst) ; If it's an atom, just return it
-;;       ((nil? (cdr lst)) lst)
-;;       (t (last (cdr lst))))))
+(setq! last
+  (lambda (lst)
+    (cond
+      ((nil? lst) nil)
+      ((atom? lst) lst) ; If it's an atom, just return it
+      ((nil? (cdr lst)) lst)
+      (t (last (cdr lst))))))
 
-;; (setq! nconc!
-;;   (lambda (lists)
-;;     (let ((result (if (atom? (car lists)) (list (car lists)) (car lists)))
-;;           (remaining (cdr lists)))
-;;       (while (not (nil? remaining))
-;;         (setq! result (nconc2! result (car remaining)))
-;;         (setq! remaining (cdr remaining)))
-;;       result)))
+(setq! nconc2!
+  (lambda (lst1 lst2)
+    (cond 
+      ((nil? lst1) lst2)
+      (t 
+        (rplacd! (last lst1) lst2)
+        lst1))))
 
-;; (setq! nconc2!
-;;   (lambda (lst1 lst2)
-;;     (cond 
-;;       ((nil? lst1) lst2)
-;;       ((atom? lst1) (cons lst1 lst2)) ; If lst1 is an atom, cons it with lst2
-;;       (t 
-;;         (rplacd! (last lst1) lst2)
-;;         lst1))))
+(setq! nconc!
+  (lambda (lists)
+    (let ((result (car lists))
+          (remaining (cdr lists)))
+      (while (not (nil? remaining))
+        (setq! result (nconc2! result (car remaining)))
+        (setq! remaining (cdr remaining)))
+      result)))
 
-;; (setq! lst '(1 2))
-;; (setq! lst2 '(3 4))
-;; (setq! lst3 '(5 6))
+(setq! lst '(1 2))
+(setq! lst2 '(3 4))
+(setq! lst3 '(5 6))
 
-;; (nconc! lst lst2 lst3)
+;(nconc! lst lst2 lst3)
+
+(setq! ct 8)
+
+(while (> ct 0)
+  (write ct) (nl)
+  (setq! ct (- ct 1)))
 
