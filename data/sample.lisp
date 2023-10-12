b5c60e7839bf0864fb2b96775851e005f6b1958c
Ariane Emory <ariane.emory@protonmail.com>
2023-10-12 16:30:11 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -4,39 +4,37 @@
   (write ct) (nl)
   (setq! ct (- ct 1)))
 
-;; (filter odd? '(1 2 3 4 5 6 7 8 9 10))   ; Expected result: (1 3 5 7 9)
+(write (filter odd? '(1 2 3 4 5 6 7 8 9 10))) (nl)
 
-;; (write (filter (lambda (x) (not (nil? x))) '(a nil b c nil d)))   ; Expected result: (a b c d)
-;; (princ "one:   ") (write (push-back! lst 5))    (nl)
+(write (filter (lambda (x) (not (nil? x))) '(a nil b c nil d)))
+
+(setq lst '(1 2 3 4))
+(princ "one:   ") (write (push-back! lst 5))    (nl)
 ;; (princ "two:   ") (write (push!      0   lst))  (nl)
 ;; (princ "three: ") (write (nconc!     lst lst2)) (nl)
 
-;; I added while and apply, they seem to work:
 
-;; (write (apply + '(1 2 3)))
 
-;; ;; I'm trying this nconc!:
+;; (setq! lst '(1 2))
+;; (setq! lst2 '(3 4))
+;; (setq! lst3 '(5 6))
 
-(setq! lst '(1 2))
-(setq! lst2 '(3 4))
-(setq! lst3 '(5 6))
+;; (write (nconc! lst lst2 lst3)) (nl)
 
-(write (nconc! lst lst2 lst3)) (nl)
+;; (write lst) (nl)
 
-(write lst) (nl)
+;; (write (mapconcat         (lambda (x) x) '("a" "b" "c") " "))  (nl)
+;; (write (apply mapconcat '((lambda (x) x) '("a" "b" "c") " "))) (nl)
 
-(write (mapconcat (lambda (x) x) '("a" "b" "c") " ")) (nl)
-(write (apply mapconcat '((lambda (x) x) '("a" "b" "c") " "))) (nl)
+;; ;; (write (equal? '(a (1 2)) '(a (1 2)))) (nl)
 
-;; (write (equal? '(a (1 2)) '(a (1 2)))) (nl)
+;; (equal? '(1) '(1))
 
-(equal? '(1) '(1))
+;; (setq! l '(1 2 (3 4)))
+;; (transform! l integer? double)
+;; (write l) (nl) ;; (2 4 (6 8))
 
-(setq! l '(1 2 (3 4)))
-(transform! l integer? double)
-(write l) (nl) ;; (2 4 (6 8))
-
-(setq! l '(1 2 (3 4)))
-(setq! new-l (transform l integer? double))
-(write l) (nl)     ;; (1 2 (3 4)) remains unchanged
-(write new-l) (nl) ;; (2 4 (6 8)) the transformed list
+;; (setq! l '(1 2 (3 4)))
+;; (setq! new-l (transform l integer? double))
+;; (write l) (nl)     ;; (1 2 (3 4)) remains unchanged
+;; (write new-l) (nl) ;; (2 4 (6 8)) the transformed list
