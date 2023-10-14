a0936ebf6114a7266d5a35d0b83ccf76bbcceb3d
Ariane Emory <ariane.emory@protonmail.com>
2023-10-13 20:40:43 -0400

    ...

--- src/core/core_log.c
+++ src/core/core_log.c
@@ -3,13 +3,13 @@
 #include "core_includes.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// _log_eval
+// _l_eval
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t * ae_core_acc_log_eval(ae_obj_t * const env,
-                                ae_obj_t * const args,
-                                int args_length) {
-  CORE_BEGIN("log_eval");
+ae_obj_t * ae_core_l_eval(ae_obj_t * const env,
+                          ae_obj_t * const args,
+                          int args_length) {
+  CORE_BEGIN("l_eval");
 
   (void)args;
   (void)env;
@@ -27,6 +27,35 @@ ae_obj_t * ae_core_acc_log_eval(ae_obj_t * const env,
   /* /\*               : FUN_ENV(CAR(args))); *\/ */
   /* } */
 
-  CORE_RETURN("log_eval", old_value ? TRUE : NIL);
+  CORE_RETURN("l_eval", old_value ? TRUE : NIL);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// _l_core
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ae_obj_t * ae_core_l_core(ae_obj_t * const env,
+                          ae_obj_t * const args,
+                          int args_length) {
+  CORE_BEGIN("l_core");
+
+  (void)args;
+  (void)env;
+  (void)args_length;
+  
+  bool old_value = false; // log_core;
+
+  /* (void)args;   */
+
+  /* if (args_length == 1) { */
+  /* /\*   REQUIRE(env, args, (ENVP(CAR(args)) || LAMBDAP(CAR(args)) || MACROP(CAR(args)))); *\/ */
+
+  /* /\*   CORE_RETURN("log_core", ENVP(CAR(args)) *\/ */
+  /* /\*               ? ENV_PARENT(CAR(args)) *\/ */
+  /* /\*               : FUN_ENV(CAR(args))); *\/ */
+  /* } */
+
+  CORE_RETURN("l_core", old_value ? TRUE : NIL);
 }
 
