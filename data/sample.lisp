e53d697f7c960bb1207d4a02f8348ede4fed85af
Ariane Emory <ariane.emory@protonmail.com>
2023-10-15 01:19:03 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,10 +1,10 @@
-(write (heads '((1 2 3) (4 5 6) (7 8 9)))) (nl)
-(write (tails '((1 2 3) (4 5 6) (7 8 9)))) (nl)
+(writen '(heads '((1 2 3) (4 5 6) (7 8 9))))
+(writen '(tails '((1 2 3) (4 5 6) (7 8 9))))
 
-(write (all? nil? '(nil nil nil)))         (nl)
-(write (all? nil? '(nil nil 1  )))         (nl)
-(write (not? (all? nil? '(nil nil nil))))  (nl)
-(write (not? (all? nil? '(nil nil 1  ))))  (nl)
+(writen       '(all? nil? '(nil nil nil)))        
+(writen       '(all? nil? '(nil nil 1  )))        
+(writen '(not? (all? nil? '(nil nil nil)))) 
+(writen '(not? (all? nil? '(nil nil 1  )))) 
 
 (princn  "this" "is" "a" "test")
 (princns "this" "is" "a" "test")
