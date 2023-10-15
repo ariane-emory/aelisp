9f0cc5a44b6cfdca2f88a702d4ebbcfd327a3af4
Ariane Emory <ariane.emory@protonmail.com>
2023-10-15 11:02:48 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,22 +1,21 @@
-;; (writen '(heads '((1 2 3) (4 5 6) (7 8 9))))
-;; (writen '(tails '((1 2 3) (4 5 6) (7 8 9))))
+(writen '(heads '((1 2 3) (4 5 6) (7 8 9))))
+(writen '(tails '((1 2 3) (4 5 6) (7 8 9))))
 
-;; (writen       '(all? nil? '(nil nil nil)))        
-;; (writen       '(all? nil? '(nil nil 1  )))        
-;; (writen '(not? (all? nil? '(nil nil nil)))) 
-;; (writen '(not? (all? nil? '(nil nil 1  )))) 
+(writen       '(all? nil? '(nil nil nil)))        
+(writen       '(all? nil? '(nil nil 1  )))        
+(writen '(not? (all? nil? '(nil nil nil)))) 
+(writen '(not? (all? nil? '(nil nil 1  )))) 
 
-;; (princn  "this" "is" "a" "test")
-;; (princns "this" "is" "a" "test")
+(princn  "this" "is" "a" "test")
+(princns "this" "is" "a" "test")
 
-;; (princns 1 2 3 4 5 6 7 8 9)
+(princns 1 2 3 4 5 6 7 8 9)
 
-;; (princn (or?  nil nil 3 8))
-;; (princn (and? 3 8 nil))
-;; (princn (and? 3 7 8))
+(princn (or?  nil nil 3 8))
+(princn (and? 3 8 nil))
+(princn (and? 3 7 8))
 
-
- (princni ", " 1 2 3)
+(princni ", " 1 2 3)
 
 ;; (setq! test
 ;;  (lambda args
@@ -31,7 +30,6 @@
 ;;  (lambda (head . tail)
 ;;   tail))
 
-;; (princn "got here")
 
 ;; (test 1 2 3)
 
@@ -45,5 +43,6 @@
 
 (test 1 2 3 4 5)
 
-(log-eval t)
+(princn "got here")
+
 
