3ba15acd2c84c112f55b99a1bee5f2d2cf8eb353
Ariane Emory <ariane.emory@protonmail.com>
2023-10-15 11:18:40 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -9,7 +9,7 @@
 (princn  "this" "is" "a" "test")
 (princns "this" "is" "a" "test")
 
-(princns 1 2 3 4 5 6 7 8 9)
+(princ "princns: ") (princns 1 2 3 4 5 6 7 8 9)
 
 (princn (or?  nil nil 3 8))
 (princn (and? 3 8 nil))
@@ -24,11 +24,9 @@
 
 (test 1 2 3)
 
-
 (setq! test
  (lambda (head . tail)
   tail))
-
 
 (test 1 2 3)
 
@@ -43,9 +41,10 @@
 (test 1 2 3 4 5)
 
 (princn "got here")
+(princn ''(1 . 2))
 
+(log-eval t)
 
-(princn '(1 . 2))
-
+(princn '(1   2))
 
 
