1e7c16ecc9f10278b1a38538a1e5a2069fccbce4
Ariane Emory <ariane.emory@protonmail.com>
2023-10-15 01:12:42 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -6,6 +6,7 @@
 (write (not? (all? nil? '(nil nil nil))))  (nl)
 (write (not? (all? nil? '(nil nil 1  ))))  (nl)
 
-(princn "this" "is" "a" "test")
+(princn  "this" "is" "a" "test")
+(princns "this" "is" "a" "test")
 
 (write (intercalate " " '(1 2 3 4 5 6 7 8 9))) (nl)
