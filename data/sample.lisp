21b033ac8236d79159fba8fa99eb4425add5f66b
Ariane Emory <ariane.emory@protonmail.com>
2023-10-15 01:04:33 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -7,3 +7,13 @@
 (write (not? (all? nil? '(nil nil 1  ))))  (nl)
 
 (princn "this" "is" "a" "test")
+
+(setq! intercalate
+ (lambda (intercalated lst)
+  (if (or? (nil? lst) (nil? (cdr lst))) ; Check if the list is empty or has only one element.
+   lst
+   (cons (car lst) 
+    (cons intercalated
+     (intercalate intercalated (cdr lst)))))))
+
+(write (intercalate " " '(1 2 3 4 5 6 7 8 9))) (nl)
