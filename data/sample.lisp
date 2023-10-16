838de7db039dc74174df8643780fc5867d693ec3
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 02:08:38 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -55,25 +55,12 @@
 
 (setq! defmacro
  (macro (name params . body)
- (list 'setq! name (list 'macro params . body))))
+  (list 'setq! name (list 'macro params . body))))
 
 (defmacro defun (name params . body)
-  (list (quote setq!) name (list (quote lambda) params . body)))
+ (list (quote setq!) name (list (quote lambda) params . body)))
 
 (defun 2* (x) (* 2 x)) (nl)
 
-(princ "this: " ) (write (2* 3)) (nl) ;; successfuly prints 6.
-
-;; (setq! test
-;;  (macro (name params . body) (list setq! name params body)))
-
-;; (write test) (nl)
-;; (write (test one two three four)) (nl)
-;; (write (defun somefun x (n) (* 2 n))) (nl)
-
-;; (nl) (nl)
-
-;; ;;(log-all t)
-;; (list 1 2 . 3)
-;; (write (list 1 2 . 3)) (nl)
+(princ "this should print 6: " ) (write (2* 3)) (nl) ;; successfuly prints 6.
 
