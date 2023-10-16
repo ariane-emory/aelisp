88d76ba31c590e7bcc89d7a9777e4a366d52fb72
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 02:32:13 -0400

    ...

--- data/playxe.lisp
+++ data/playxe.lisp
@@ -1,3 +1,7 @@
+;; https://github.com/mishoo/SLip/blob/f11078c94bbbbc5b2d55a9a3669edb17bf5bb8fc/tmp/t2.lisp#L9
+;; also: https://www.brics.dk/NS/99/1/BRICS-NS-99-1.pdf
+;;       https://github.com/maolonglong/bogoscheme/blob/main/primitives.scm#L147
+
 (set! qq-expand-list
       (lambda (x)
         (if (consp x)
