e0ecfab65e14b7bc76b5465513ee58052cbec201
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 09:07:07 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -5,18 +5,18 @@
 ;; (log-all t)
 (write (mapcar flatten (left-nested-zip '(1 2 3) '(a b c) '(10 20 30) '(x y z)))) (nl)
 
-(defun zip lists
- "Zip a list of lists."
- (mapcar flatten (apply left-nested-zip lists)))
+;; (defun zip lists
+;;  "Zip a list of lists."
+;;  (mapcar flatten (apply left-nested-zip lists)))
 
-(princ "flat 2: ")
-;; (log-all t)
-(write (zip '(1 2 3) '(a b c) '(10 20 30) '(x y z))) (nl)
+;; (princ "flat 2: ")
+;; ;; (log-all t)
+;; (write (zip '(1 2 3) '(a b c) '(10 20 30) '(x y z))) (nl)
 
 
 (defmacro zipm lists
  "Zip a list of lists."
- (list 'mapcar '('apply 'left-nested-zip lists)))
+ (list 'mapcar 'flatten (list 'left-nested-zip lists)))
 ;; (mapcar flatten (apply left-nested-zip lists)))
 
 (princ "flat m: ")
--- src/core/core_funs.c
+++ src/core/core_funs.c
@@ -8,7 +8,7 @@
 ae_obj_t * ae_core_macro(ae_obj_t * const env, ae_obj_t * const args, __attribute__((unused)) int args_length) {
   CORE_BEGIN("macro");
 
-  REQUIRE(env, args, TAILP(CAR(args)));
+  REQUIRE(env, args, TAILP(CAR(args)) || SYMBOLP(CAR(args)));
   REQUIRE(env, args, TAILP(CDR(args)));
 
   CORE_RETURN("macro", NEW_MACRO(CAR(args), CDR(args), env));
@@ -21,11 +21,7 @@ ae_obj_t * ae_core_macro(ae_obj_t * const env, ae_obj_t * const args, __attribut
 ae_obj_t * ae_core_lambda(ae_obj_t * const env, ae_obj_t * const args, __attribute__((unused)) int args_length) {
   CORE_BEGIN("lambda");
 
-  REQUIRE(env, args, TAILP(CAR(args))
-#ifndef AE_NO_SINGLE_SYM_PARAMS
-          || SYMBOLP(CAR(args))
-#endif
-          );
+  REQUIRE(env, args, TAILP(CAR(args)) || SYMBOLP(CAR(args)));
   REQUIRE(env, args, TAILP(CDR(args)));
 
   CORE_RETURN("lambda", NEW_LAMBDA(CAR(args), CDR(args), env));
