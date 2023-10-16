9261c7968ff51179af079fc2c1723a6730872448
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 08:57:54 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,16 +1,13 @@
 
-
 (setq! left-nested-zip (reduced zip2))
-
-(defun zip lists
- "Zip a list of lists."
- (mapcar flatten (apply left-nested-zip lists)))
-
 
 (princ "flat 1: ")
 ;; (log-all t)
 (write (mapcar flatten (left-nested-zip '(1 2 3) '(a b c) '(10 20 30) '(x y z)))) (nl)
 
+(defun zip lists
+ "Zip a list of lists."
+ (mapcar flatten (apply left-nested-zip lists)))
 
 (princ "flat 2: ")
 ;; (log-all t)
