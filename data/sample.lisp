eb7651d03d76b1fb3a43504604c35a40d19cfd6e
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 19:39:48 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -63,6 +63,33 @@
 (write (mapcar flatten1 (reduce zip2 (car lsts) (cdr lsts)))) (nl)
 (write (zip3 '(a b) '(2 3) '(4 5))) (nl)
 
+(princ "Odd numbers: ") (write (filter (compose-preds integer? odd?) '(1 2 3 4 5 6 7 8 9 10))) (nl)
+(write (any? (compose-preds integer? even?) '(1 3 a 5))) (nl)
+(write (any? (compose-preds integer? even?) '(1 3 a 4 5))) (nl)
+
+(write ((compose-preds integer? odd?)  7)) (nl)
+(write ((compose-preds integer? odd?)  8)) (nl)
+(write ((compose-preds integer? odd?) 'a)) (nl)
+
+(writen '(heads '((1 2 3) (4 5 6) (7 8 9))))
+(writen '(tails '((1 2 3) (4 5 6) (7 8 9))))
+
+(writen       '(all? nil? '(nil nil nil)))        
+(writen       '(all? nil? '(nil nil 1  )))        
+(writen '(not? (all? nil? '(nil nil nil)))) 
+(writen '(not? (all? nil? '(nil nil 1  )))) 
+
+(princn  "this" "is" "a" "test")
+(princns "this" "is" "a" "test")
+
+(princ "princns: ") (princns 1 2 3 4 5 6 7 8 9)
+
+(princn (or?  nil nil 3 8))
+(princn (and? 3 8 nil))
+(princn (and? 3 7 8))
+
+(princni ", " 1 2 3)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (log-macro t)
