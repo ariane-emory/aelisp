22c3b9c0314f052dbc238f73ddb03fa12bbf7a7f
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 04:26:25 -0400

    bad append-many in sample.lisp

--- data/sample.lisp
+++ data/sample.lisp
@@ -3,3 +3,9 @@
 
 (princ "this should print 6: " ) (write (2* 3)) (nl) ;; successfuly prints 6.
 (princ "this should print 6: " ) (write (2* 3)) (nl) ;; does not re-expand.
+
+(defun append-many lists
+ (reduce append lists))
+
+(log-all t)
+(write (append-many '(1 2) '(3 4) '(5 6))) (nl)
