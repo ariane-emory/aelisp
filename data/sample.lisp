74d04c8d0827d169ff30010298bdb707f004654e
Ariane Emory <ariane.emory@protonmail.com>
2023-10-16 04:30:14 -0400

    derp, fix basic accum init error.

--- data/sample.lisp
+++ data/sample.lisp
@@ -5,7 +5,7 @@
 (princ "this should print 6: " ) (write (2* 3)) (nl) ;; does not re-expand.
 
 (defun append-many lists
- (reduce append lists))
+ (reduce append (car lists) (cdr lists)))
 
 (log-all t)
 (write (append-many '(1 2) '(3 4) '(5 6))) (nl)
