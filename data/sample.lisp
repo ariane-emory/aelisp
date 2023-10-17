35d773e52166bc7a89a86657579838ea6bd03ac4
Ariane Emory <ariane.emory@protonmail.com>
2023-10-17 19:47:37 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -8,6 +8,10 @@
 
 (defmacro quotify (x) $('quote x))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; No recursion:
+
 (defmacro xform (obj)
  (cond
   ((and (cons? obj) (cdr obj)) (list 'cons (list 'quote (car obj)) (list 'xform (cdr obj))))
@@ -16,6 +20,92 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; With recursion:
+
+(defmacro xform (obj)
+  (cond
+   ((nil? obj) nil)
+   ((cons? obj)
+    (let ((head (car obj))
+          (tail (cdr obj)))
+      (list 'cons
+            (if (cons? head) 
+                (list 'xform head)
+                (list 'quote head))
+            (list 'xform tail))))
+   (t obj)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro xform (obj)
+  (cond
+   ;; Handle end of list
+   ((nil? obj) nil)
+
+   ;; Handle (unquote something)
+   ((and (cons? obj) (eq? 'unquote (car obj)))
+    (cadr obj))
+
+   ;; Handle list item
+   ((cons? obj) 
+    (list 'cons 
+          (if (and (cons? (car obj)) (eq? 'unquote (car (car obj))))
+              (cadr (car obj))
+              (list 'quote (car obj)))
+          (xform (cdr obj))))
+
+   ;; Handle single items
+   (t obj)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro xform (obj)
+  (cond
+   ;; Handle end of list
+   ((nil? obj) nil)
+   
+   ;; Handle (unquote something)
+   ((and (cons? obj) (eq? 'unquote (car obj)))
+    (cadr obj))
+   
+   ;; Handle list item where the tail is another cons cell
+   ((and (cons? obj) (cons? (cdr obj))) 
+    (list 'cons 
+          (if (and (cons? (car obj)) (eq? 'unquote (car (car obj))))
+              (cadr (car obj))
+              (xform (car obj)))
+          (xform (cdr obj))))
+   
+   ;; Handle list item where the tail is not another cons cell (improper list)
+   ((cons? obj)
+    (list 'cons
+          (if (and (cons? (car obj)) (eq? 'unquote (car (car obj))))
+              (cadr (car obj))
+              (xform (car obj)))
+          (list 'quote (cdr obj))))
+   
+   ;; Handle single items
+   (t (list 'quote obj))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro xform (obj)
+  (cond
+   ;; Handle end of list
+   ((nil? obj) nil)
+
+   ;; Handle (unquote something)
+   ((and (cons? obj) (eq? 'unquote (car obj)))
+    (cadr obj))
+
+   ;; Handle cons cells
+   ((cons? obj)
+    (list 'cons 
+          (xform (car obj))
+          (xform (cdr obj))))
+
+   ;; Handle everything else
+   (t (list 'quote obj))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
