b6dd21294ffd0dfa9a8480cfdc5443c27744d54d
Ariane Emory <ariane.emory@protonmail.com>
2023-10-17 14:44:39 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,17 +1,22 @@
-(setq! lst-base '(1 2 3 4 5 6 ((7 8 (unquote 9) 10 11 12 13))))
+(setq! lst-base '(1 a 2 b 3 4 5 6 ((7 8 (unquote 9) 10 11 12 13))))
 (setq! lst lst-base)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun xform (obj pred? fun-if fun-else)
+(defun xform (obj pred? if-fun else-fun)
  (cond
-  ((pred? obj) (fun-if obj))
-  ((atom? obj) obj)
+  ((pred? obj) (if-fun   obj))
+  ((atom? obj) (else-fun obj))
   (t (cons
-      (xform (car obj) pred? fun-if fun-else)
-      (xform (cdr obj) pred? fun-if fun-else)))))
+      (xform (car obj) pred? if-fun else-fun)
+      (xform (cdr obj) pred? if-fun else-fun)))))
 
 (write (xform lst integer? 2* id))
+(nl)
+
+(defun swap (x) 'xxx)
+
+(write (xform lst integer? 2* swap))
 (nl)
 
 (princ "Done.")
