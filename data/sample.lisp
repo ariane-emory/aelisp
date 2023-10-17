a385dbcefd295a43a494463b7ef09f77cde10cf4
Ariane Emory <ariane.emory@protonmail.com>
2023-10-17 14:03:03 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -71,10 +71,11 @@
 
 ;; (log-eval t)
 
-(setq! lst '(1 2 3 4 5 6 ((7 8 9) 10 11 12 13)))
+(setq! lst-base '(1 2 3 4 5 6 ((7 8 (unquote 9) 10 11 12 13))))
+(setq! lst lst-base)
 
 (write (transform lst integer? 2*))
 (nl)
 
-(write "done.")
-(nl)shove
+(princ "Done.")
+(nl)
