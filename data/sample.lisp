286df6b89883c7886f5fe2a75cb363057d322fef
Ariane Emory <ariane.emory@protonmail.com>
2023-10-17 14:47:08 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,10 +1,12 @@
 (setq! lst-base '(1 a 2 b 3 4 5 6 ((7 8 (unquote 9) 10 11 12 13))))
+(setq! lst-base '(1 a 2 b 3 ((4 (unquote 5) 6))))
 (setq! lst lst-base)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun xform (obj pred? if-fun else-fun)
  (cond
+  ((nil? obj)   nil)
   ((pred? obj) (if-fun   obj))
   ((atom? obj) (else-fun obj))
   (t (cons
@@ -14,7 +16,7 @@
 (write (xform lst integer? 2* id))
 (nl)
 
-(defun quotify (x) 'xxx)
+(defun quotify (x) (quote x))
 
 (write (xform lst integer? 2* quotify))
 (nl)
