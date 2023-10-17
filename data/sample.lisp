77c8d4dde29c4a3f066233eff2975693f5c4c45a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-17 14:34:11 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,7 +1,19 @@
 (setq! lst-base '(1 2 3 4 5 6 ((7 8 (unquote 9) 10 11 12 13))))
 (setq! lst lst-base)
 
-(write (transform lst integer? 2*))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun xform (obj pred? fun)
+  (if (pred? obj)
+   (fun obj)
+   (if (atom? obj)
+    obj
+    (cons
+     (xform (car obj) pred? fun)
+     (xform (cdr obj) pred? fun)))))
+
+
+(write (xform lst integer? 2*))
 (nl)
 
 (princ "Done.")
