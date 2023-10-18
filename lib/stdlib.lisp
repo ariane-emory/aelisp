454f260da16de2b08d6b5802386e9b7bcac337d8
Ariane Emory <ariane.emory@protonmail.com>
2023-10-18 03:53:05 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -93,39 +93,40 @@
    lst2                                                                       ;)
    (cons (car lst1) (append2 (cdr lst1) lst2)))))                             ;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
-(setq! expand-quasiquoted
- (macro (expr)
-  (cond
-   ;; If it's not a cons then it's an atom that we should quote.
-   ((atom? expr)
-     $('quote expr))
-   ;; Directly replace (unquote x) with x.
-   ((eq? (car expr) 'unquote)
-    (car (cdr expr)))
-   ;; If the second element of the list is an unquote-splicing, we want to use
-   ;; append2.
-   ((and
-     (cons? (cdr expr))
-     (cons? (car (cdr expr)))
-     (eq? (car (car (cdr expr))) 'unquote-splicing))
-    $('append2
-      $('list $('expand-quasiquoted (car expr)))
-      (car (cdr (car (cdr expr))))))
-   ;; If the second element of the list is an unquote, use cons but without
-   ;; splicing.
-   ((and
-     (cons? (cdr expr))
-     (eq? (car (cdr expr)) 'unquote))
-    $('cons
-      $('expand-quasiquoted (car expr))
-      (car (cdr (cdr expr)))))
-   ;; Error out for splicing outside of list context
-   ((eq? (car expr) 'unquote-splicing)
-    (error "unquote-splicing can't occur at top level"))
-   ;; If the list is regular, we just recurse on both its parts
-   (t $('cons
-        $('expand-quasiquoted (car expr))
-        $('expand-quasiquoted (cdr expr)))))))
+(defmacro expand-quasiquoted (expr)                                           ;)
+ "Expand a quasiquoted expression and resolve unquotes and"                   ;)
+ "unquote-splicings within."                                                  ;)
+ (cond                                                                        ;)
+  ;; If it's not a cons then it's an atom that we should quote.               ;)
+  ((atom? expr)                                                               ;)
+   $('quote expr))                                                            ;)
+  ;; Directly replace (unquote x) with x.                                     ;)
+  ((eq? (car expr) 'unquote)                                                  ;)
+   (car (cdr expr)))                                                          ;)
+  ;; If the second element of the list is an unquote-splicing, we want to use ;)
+  ;; append2.                                                                 ;)
+  ((and                                                                       ;)
+    (cons? (cdr expr))                                                        ;)
+    (cons? (car (cdr expr)))                                                  ;)
+    (eq? (car (car (cdr expr))) 'unquote-splicing))                           ;)
+   $('append2                                                                 ;)
+     $('list $('expand-quasiquoted (car expr)))                               ;)
+     (car (cdr (car (cdr expr))))))                                           ;)
+  ;; If the second element of the list is an unquote, use cons but without    ;)
+  ;; splicing.                                                                ;)
+  ((and                                                                       ;)
+    (cons? (cdr expr))                                                        ;)
+    (eq? (car (cdr expr)) 'unquote))                                          ;)
+   $('cons                                                                    ;)
+     $('expand-quasiquoted (car expr))                                        ;)
+     (car (cdr (cdr expr)))))                                                 ;)
+  ;; Error out for splicing outside of list context                           ;)
+  ((eq? (car expr) 'unquote-splicing)                                         ;)
+   (error "unquote-splicing can't occur at top level"))                       ;)
+  ;; If the list is regular, we just recurse on both its parts                ;)
+  (t $('cons                                                                  ;)
+       $('expand-quasiquoted (car expr))                                      ;)
+       $('expand-quasiquoted (cdr expr))))))                                  ;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
 (setq! quasiquote expand-quasiquoted)                                         ;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
