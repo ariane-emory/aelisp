4f011369b1e463af37d346c085592ba01c46ade8
Ariane Emory <ariane.emory@protonmail.com>
2023-10-18 02:16:06 -0400

    this one seems to cover all the important cases.

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,28 +1,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro quotify (x) $('quote x))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (nl) (princ "Before.")
-;; `(1 2 ,(- 5 2) (4 5))
-;; (nl) (princ "After.")
-;; (nl)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (setq! lists (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
-
-;; `(cons left-nested-zip ,lists) 
-;; `(mapcar flatten (cons left-nested-zip ,lists))
-;; `(mapcar flatten (cons left-nested-zip ,@lists))
 
 (log-macro t)
 (log-macro nil)
 
-(eval `(mapcar princ ',lists))
+(nl)
+(write `,1)
 
+(nl)
+(eval `(mapcar princ ',lists))
 
 (nl)
 (write (zip '(a b c) '(1 2 3)))
