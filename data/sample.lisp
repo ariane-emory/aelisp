40b7531654fa065607a9e4fa692dda6f75fb5518
Ariane Emory <ariane.emory@protonmail.com>
2023-10-19 02:14:19 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -18,39 +18,6 @@
 
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun sort (lst predicate)
-  (if (or (not lst) (not (cdr lst))) ; If list has 0 or 1 element
-      lst                          ; it's already sorted
-      (let ((middle (half lst)))
-        (merge (sort (car middle) predicate)
-               (sort (cdr middle) predicate)
-               predicate))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun half (lst)
-  "Splits lst into two approximately equal halves."
-  (let ((slow lst)
-        (fast (cdr lst)))
-    (while (and fast (cdr fast))
-      (setq! slow (cdr slow))
-      (setq! fast (cdr (cdr fast))))
-    (let ((second-half (cdr slow)))
-      (rplacd! slow nil) ; Split the list
-      (cons lst second-half))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun merge (lst1 lst2 predicate)
-  "Merges two sorted lists based on predicate."
-  (cond
-   ((not lst1) lst2)
-   ((not lst2) lst1)
-   ((predicate (car lst1) (car lst2))
-    (cons (car lst1) (merge (cdr lst1) lst2 predicate)))
-   (t
-    (cons (car lst2) (merge lst1 (cdr lst2) predicate)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defun even-first-comparator (a b)
@@ -73,7 +40,7 @@
 
 
 (setq! lst '(3 1 13 2 8 4 5 12 7 11 9 6 10 15 14))
-
+(write (syms (env))) (nl)
 (write (sort lst combined-comparator)) (nl)
 
 (exit)
--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -382,6 +382,38 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun sort (lst predicate)
+  (if (or (not lst) (not (cdr lst))) ; If list has 0 or 1 element
+      lst                          ; it's already sorted
+      (let ((middle (half lst)))
+        (merge (sort (car middle) predicate)
+               (sort (cdr middle) predicate)
+               predicate))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun half (lst)
+  "Splits lst into two approximately equal halves."
+  (let ((slow lst)
+        (fast (cdr lst)))
+    (while (and fast (cdr fast))
+      (setq! slow (cdr slow))
+      (setq! fast (cdr (cdr fast))))
+    (let ((second-half (cdr slow)))
+      (rplacd! slow nil) ; Split the list
+      (cons lst second-half))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun merge (lst1 lst2 predicate)
+  "Merges two sorted lists based on predicate."
+  (cond
+   ((not lst1) lst2)
+   ((not lst2) lst1)
+   ((predicate (car lst1) (car lst2))
+    (cons (car lst1) (merge (cdr lst1) lst2 predicate)))
+   (t
+    (cons (car lst2) (merge lst1 (cdr lst2) predicate)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
 ;; list funs (unsorted):                                                      ;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
@@ -564,9 +596,4 @@
   (max 1 (+ (depth (car lst)) (depth (cdr lst))))))                           ;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
-;; to-do/wip, don't trust these yet:                                          ;)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;)
-
-(setq! sort)
 
