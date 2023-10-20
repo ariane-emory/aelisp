1da373ddcd920141a437851db9865eed7d6b007d
Ariane Emory <ariane.emory@protonmail.com>
2023-10-20 12:58:24 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -4,13 +4,14 @@
 (display (unionq'(1 2 3 4) '(4 5 2 2) '(5 6 7 8)))
 (nl)
 
-(write (letrec
- ((factorial
-   (lambda (n)
-    (if (< n 2)
-     1
-     (* n (factorial (- n 1)))))))
- (factorial 5)))
+(write
+ (letrec
+  ((factorial
+    (lambda (n)
+     (if (< n 2)
+      1
+      (* n (factorial (- n 1)))))))
+  (factorial 5)))
 (nl)
 
 (defun mmemql? (x lst)
@@ -21,6 +22,18 @@
       ((eql? x (car lst)) t)
       (lst (mmemql? x (cdr lst)))))))
   (chase x lst)))
+
+(defmacro make-member-pred (pred?)
+ `(lambda (x lst)
+  (letrec
+    ((chase
+      (lambda (x lst)
+       (cond
+        ((,pred? x (car lst)) t)
+        (lst (mmemql? x (cdr lst)))))))
+    (chase x lst))))
+
+(setq! mmemql? (make-member-pred eql?))
 
 (write (mmemql? 4 '(1 2 3 4 5 6 7)))
 (nl)
