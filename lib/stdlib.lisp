f04fdf2ac75f64fb67de97c3876925d7b2b582f1
Ariane Emory <ariane.emory@protonmail.com>
2023-10-20 12:37:07 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -665,30 +665,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(when nil?
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; tiny-clos scheme compat:
+ ;; tiny-clos scheme compat:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! #f            nil)
-(setq! #t            t)
-(setq! ???           'unspecified-result)
-(setq! assoc         ahas) 
-(setq! assq          aget) 
-(setq! collect-if    filter)
-(setq! define        setq!)
-(setq! display       write) ;should should be a macro that avoids re-defining what-scheme-implementation
-(setq! else          t)
-(setq! every         all?)
-(defun funcall args  (eval args))
-(setq! getl          pget)
-(setq! gsort         sort)
-(setq! make-vector   make-list)
-(setq! map           mapcar)
-(setq! map-append    mapcan)
-(setq! null?         nil?)
-(setq! position-of   indexq)
-(setq! remove        removeq)
-(setq! set!          setq!)
-(setq! vector-length list-length)
-(setq! vector-ref    list-ref)
-(setq! vector-set!   list-set!)
+ (setq! #f            nil)
+ (setq! #t            t)
+ (setq! ???           'unspecified-result)
+ (setq! assoc         ahas) 
+ (setq! assq          aget) 
+ (setq! collect-if    filter)
+ (setq! define        setq!)
+ (setq! display       write) ;should should be a macro that avoids re-defining what-scheme-implementation
+ (setq! else          t)
+ (setq! every         all?)
+ (defun funcall args  (eval args))
+ (setq! getl          pget)
+ (setq! gsort         sort)
+ (setq! make-vector   make-list)
+ (setq! map           mapcar)
+ (setq! map-append    mapcan)
+ (setq! null?         nil?)
+ (setq! position-of   indexq)
+ (setq! remove        removeq)
+ (setq! set!          setq!)
+ (setq! vector-length list-length)
+ (setq! vector-ref    list-ref)
+ (setq! vector-set!   list-set!)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
