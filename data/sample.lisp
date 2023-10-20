9647d482286aa4664320bd5df0cef74e6b517b41
Ariane Emory <ariane.emory@protonmail.com>
2023-10-20 10:31:17 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -121,13 +121,38 @@
              (cons x acc)))
     union1 list2)))
 
+(defun union2 (memp? list1 list2)
+  (let* ((combine (reduced (lambda (acc x) (if (memp? x acc) acc (cons x acc))))))
+    (combine (combine '() list1) list2)))
+
+(defun union2 (memp? list1 list2)
+  (let* ((combine (lambda (acc x) (if (memp? x acc) acc (cons x acc))))
+         (union1 (reduce combine '() list1)))
+    (reduce combine union1 list2)))
+
 (defun union (memp? list1 list2)
-  (let* ((combine (reduced (lambda (acc x) (if (memp? x acc) acc (cons x acc)))))
-         (union1 (combine '() list1)))
-    (combine union1 list2)))
+  (let ((combine (reduced (lambda (acc x) (if (memp? x acc) acc (cons x acc))))))
+    (combine list1 list2 '())))
+
+;; (defun reduced (fun init)
+;;   "Return a function that is a reduction of the binary function fun with initial value init."
+;;   (lambda lsts (reduce fun init lsts)))
+
+(defun union (memp? list1 list2)
+  (let ((combine (reduced (lambda (acc x) (if (memp? x acc) acc (cons x acc))) '())))
+    (combine list1 list2)))
+
+(defun union (memp? list1 list2)
+  (let ((combine (lambda (acc x)
+                   (if (memp? x acc) acc (cons x acc)))))
+    ((reduced combine) (cons list1 list2))))
+(defun union (memp? list1 list2)
+  (let ((combine (lambda (acc x)
+                   (if (memp? x acc) acc (cons x acc)))))
+    ((reduced combine) (append list1 list2))))
 
 (log-eval t)
-(display (union memql? '(1 2 3 4) '(4 5 2 2)))
+(display (union2 memql? '(1 2 3 4) '(4 5 2 2)))
 
 
  
