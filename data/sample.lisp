47e072e6b118a0c1e7ca17b8100501b5c365964b
Ariane Emory <ariane.emory@protonmail.com>
2023-10-20 12:33:00 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,7 +1,6 @@
 (display (union2 memql? '(1 2 3 4) '(4 5 2 2)))    (nl)
 (display (unionq'(1 2 3 4) '(4 5 2 2) '(5 6 7 8))) (nl)
 
-
 (write (letrec
  ((factorial
    (lambda (n)
@@ -10,13 +9,12 @@
      (* n (factorial (- n 1)))))))
  (factorial 5)))
 
-(nl)
-
 (defun curry1 (fun arg1)
  (lambda args
   (apply fun arg1 args)))
 
 (setq! 2+ (curry1 + 2))
 
+(log-eval t)
 (write (2+ 3)) (nl)
 
