9c9bf113a749f051f1ad1a013fc04eb77355af80
Ariane Emory <ariane.emory@protonmail.com>
2023-10-21 17:56:29 -0400

    ...

--- src/env.c
+++ src/env.c
@@ -331,8 +331,8 @@ ae_obj_t * ae_env_new_root(void) {
   /* ENV_SET(env, SYM("≤"), ENV_FIND(env, SYM("<="))); */
   /* ENV_SET(env, SYM("≥"), ENV_FIND(env, SYM(">="))); */
   FOR_EACH_CORE_FUN_GROUP_2(load_fun);
-  FOR_EACH_CORE_CMP_OP(add_core_op);
   FOR_EACH_CORE_MATH_OP(add_core_op);
+  FOR_EACH_CORE_CMP_OP(add_core_op);
   ENV_SET(env, SYM("="), ENV_FIND(env, SYM("==")));
   FOR_EACH_CORE_FUN_GROUP_1(load_fun);
 
