63535b804a875d61462bbadd65167d6eb6e7d8c2
Ariane Emory <ariane.emory@protonmail.com>
2023-10-21 16:33:49 -0400

    ...

--- src/env.c
+++ src/env.c
@@ -336,15 +336,15 @@ ae_obj_t * ae_env_new_root(void) {
   FOR_EACH_CORE_MATH_OP(add_core_op);
   FOR_EACH_CORE_FUN_GROUP_1(load_fun);
 
-/* #if AE_PREFER_ALIST */
-/*   ENV_SET(env, SYM("has?"), ENV_FIND(env, SYM("d-ahas?"))); */
-/*   ENV_SET(env, SYM("put!"), ENV_FIND(env, SYM("d-aset!"))); */
-/*   ENV_SET(env, SYM("get"),  ENV_FIND(env, SYM("d-aget"))); */
-/* #else */
-/*   ENV_SET(env, SYM("has?"), ENV_FIND(env, SYM("d-phas?"))); */
-/*   ENV_SET(env, SYM("put!"), ENV_FIND(env, SYM("d-pset!"))); */
-/*   ENV_SET(env, SYM("get"),  ENV_FIND(env, SYM("d-pget"))); */
-/* #endif */
+#if AE_PREFER_ALIST
+  ENV_SET(env, SYM("khas?"), ENV_FIND(env, SYM("ahas?")));
+  ENV_SET(env, SYM("kput!"), ENV_FIND(env, SYM("aset!")));
+  ENV_SET(env, SYM("kget"),  ENV_FIND(env, SYM("aget")));
+#else
+  ENV_SET(env, SYM("khas?"), ENV_FIND(env, SYM("phas?")));
+  ENV_SET(env, SYM("kput!"), ENV_FIND(env, SYM("pset!")));
+  ENV_SET(env, SYM("kget"),  ENV_FIND(env, SYM("pget")));
+#endif
   
   return env;
 }
