663f7f64daf98dae568793e85d214dc57e354fed
Ariane Emory <ariane.emory@protonmail.com>
2023-10-21 16:42:02 -0400

    ...

--- include/core/core.h
+++ include/core/core.h
@@ -10,6 +10,8 @@ extern bool log_core;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define FOR_EACH_CORE_FUN_GROUP_1(DO)                                                                                     \
   /*================================================================================================================*/    \
+  DO(elapsed,      false,          1,          1,                              FUNDEF_END)                                \
+  DO(now,          false,          0,          0,                              FUNDEF_END)                                \
   DO(phas,         false,          2,          2, "phas?",                     FUNDEF_END)                                \
   DO(pset,         false,          3,          3,                              FUNDEF_END)                                \
   DO(pget,         false,          2,          2,                              FUNDEF_END)                                \
@@ -19,7 +21,7 @@ extern bool log_core;
   DO(has_prop,     false,          2,          2, "has?",                      FUNDEF_END)                                \
   DO(set_prop,     false,          3,          3, "put!",                      FUNDEF_END)                                \
   DO(get_prop,     false,          2,          2, "get",                       FUNDEF_END)                                \
-  DO(eql,          false,  UNLIMITED,  UNLIMITED, "eql?",                      FUNDEF_END) /* reduceable            */    \
+  DO(eql,          false,  UNLIMITED,  UNLIMITED, "eql?",                      FUNDEF_END)                                \
   DO(eq,           false,  UNLIMITED,  UNLIMITED, "eq?",                       FUNDEF_END)                                \
   DO(cond,         true,           1,  UNLIMITED,                              FUNDEF_END)                                \
   DO(progn,        true,   UNLIMITED,  UNLIMITED,                              FUNDEF_END)                                \
@@ -29,22 +31,19 @@ extern bool log_core;
   DO(cons,         false,          2,          2,                              FUNDEF_END)                                \
   DO(nilp,         false,          1,          1, "nil?",                      FUNDEF_END)                                \
   DO(apply,        true,           2,  UNLIMITED,                              FUNDEF_END)                                \
-  DO(apply,        true,           2,  UNLIMITED, "apply*",                    FUNDEF_END)                                \
-  DO(if,           true,           2,  UNLIMITED,                              FUNDEF_END) /* reduceable            */    \
-  DO(when,         true,           2,  UNLIMITED,                              FUNDEF_END) /* reduceable            */    \
-  DO(unless,       true,           2,  UNLIMITED,                              FUNDEF_END) /* reduceable            */    \
-  DO(while,        true,           2,  UNLIMITED,                              FUNDEF_END) /* reduceable            */    \
-  DO(until,        true,           2,  UNLIMITED,                              FUNDEF_END) /* reduceable            */    \
-  DO(not,          false,          1,  UNLIMITED, "not", "¬",                  FUNDEF_END) /* reduceable            */    \
-  DO(or,           true,           2,  UNLIMITED, "or", "∨",                   FUNDEF_END) /* reduceable            */    \
-  DO(and,          true,           2,  UNLIMITED, "and", "∧",                  FUNDEF_END) /* reduceable            */    \
+  DO(if,           true,           2,  UNLIMITED,                              FUNDEF_END)                                \
+  DO(when,         true,           2,  UNLIMITED,                              FUNDEF_END)                                \
+  DO(unless,       true,           2,  UNLIMITED,                              FUNDEF_END)                                \
+  DO(while,        true,           2,  UNLIMITED,                              FUNDEF_END)                                \
+  DO(until,        true,           2,  UNLIMITED,                              FUNDEF_END)                                \
+  DO(not,          false,          1,  UNLIMITED, "not", "¬",                  FUNDEF_END)                                \
+  DO(or,           true,           2,  UNLIMITED, "or", "∨",                   FUNDEF_END)                                \
+  DO(and,          true,           2,  UNLIMITED, "and", "∧",                  FUNDEF_END)                                \
   DO(let_star,     true,           1,  UNLIMITED, "let*",                      FUNDEF_END)                                \
   DO(letrec,       true,           1,  UNLIMITED,                              FUNDEF_END)                                \
   DO(let,          true,           1,  UNLIMITED,                              FUNDEF_END)                                \
   DO(cdr,          false,          1,          1,                              FUNDEF_END)                                \
   DO(car,          false,          1,          1,                              FUNDEF_END)                                \
-  DO(elapsed,      false,          1,          1,                              FUNDEF_END)                                \
-  DO(now,          false,          0,          0,                              FUNDEF_END)                                \
   DO(repeat,       true,           2,  UNLIMITED,                              FUNDEF_END)                                \
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -70,11 +69,11 @@ extern bool log_core;
   DO(log_all,      false,          0,          1, "log-all",                   FUNDEF_END)                                \
   DO(exit,         false,          0,          1,                              FUNDEF_END)                                \
   DO(sleep,        false,          1,          1,                              FUNDEF_END)                                \
-  DO(props,         false,          1,          1, "props", "debug-object",      FUNDEF_END) /* debug data accessor   */    \
+  DO(props,        false,          1,          1,                              FUNDEF_END)                                \
   DO(error,        false,          1,          2,                              FUNDEF_END)                                \
-  DO(errmsg,       false,          1,          1, "errmsg", "error-message",   FUNDEF_END) /* error accessor        */    \
-  DO(name,         false,          1,          1, "sym-name",                  FUNDEF_END) /* symbol accessor       */    \
-  DO(errobj,       false,          1,          1, "errobj", "error-object",    FUNDEF_END) /* error accessor        */    \
+  DO(errmsg,       false,          1,          1, "error-message",             FUNDEF_END) /* error accessor        */    \
+  DO(name,         false,          1,          1, "symbol-name",               FUNDEF_END) /* symbol accessor       */    \
+  DO(errobj,       false,          1,          1, "error-object",              FUNDEF_END) /* error accessor        */    \
   DO(type,         false,          1,          1,                              FUNDEF_END) /* GET_TYPE proxy        */    \
   DO(body,         false,          1,          1,                              FUNDEF_END) /* fun accessor          */    \
   DO(params,       false,          1,          1,                              FUNDEF_END) /* fun accessor          */    \
