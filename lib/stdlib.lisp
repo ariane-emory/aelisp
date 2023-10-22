bd73fee36c6de8e3fd807d61fa5d823e8922e2e4
Ariane Emory <ariane.emory@protonmail.com>
2023-10-22 19:07:26 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -41,7 +41,7 @@
    $('elapsed 'begin)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro report-time (msg head-expr . tail-exprs)
- $('let $($('time-taken (cons 'progn (cons head-expr tail-exprs))))
+ $('let $($('time-taken $('time (cons 'progn (cons head-expr tail-exprs)))))
    $('princ msg)
    $('princ '" in ")
    $('princ 'time-taken)
@@ -50,148 +50,150 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(report-time "doing everything else"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; simple aliases:                                                            ;;
+ ;; simple aliases:                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! s setq!)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; type predicates:                                                           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun type?     (typ o) (eq? typ       (type           o)))
-(defun atom?     (o)     (not (type?    :CONS           o)))
-(defun char?     (o)          (type?    :CHAR           o))
-(defun cons?     (o)          (type?    :CONS           o))
-(defun core?     (o)          (type?    :CORE           o))
-(defun env?      (o)          (type?    :ENV            o))
-(defun error?    (o)          (type?    :ERROR          o))
-(defun float?    (o)          (type?    :FLOAT          o))
-(defun integer?  (o)          (type?    :INTEGER        o))
-(defun lambda?   (o)          (type?    :LAMBDA         o))
-(defun λ?        (o)          (type?    :LAMBDA         o))
-(defun macro?    (o)          (type?    :MACRO          o))
-(defun rational? (o)          (type?    :RATIONAL       o))
-(defun string?   (o)          (type?    :STRING         o))
-(defun symbol?   (o)          (type?    :SYMBOL         o))
-(defun improper? (o)     (and (tail? o) (not (proper?   o))))
+ (setq! s setq!)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; compound car/cdrs:                                                         ;;
+ ;; type predicates:                                                           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun caar     (lst)               (car (car lst)))
-(defun cadr     (lst)               (car (cdr lst)))
-(defun cdar     (lst)               (cdr (car lst)))
-(defun cddr     (lst)               (cdr (cdr lst)))
-(defun caaar    (lst)          (car (car (car lst))))
-(defun caadr    (lst)          (car (car (cdr lst))))
-(defun cadar    (lst)          (car (cdr (car lst))))
-(defun caddr    (lst)          (car (cdr (cdr lst))))
-(defun cdaar    (lst)          (cdr (car (car lst))))
-(defun cdadr    (lst)          (cdr (car (cdr lst))))
-(defun cddar    (lst)          (cdr (cdr (car lst))))
-(defun cdddr    (lst)          (cdr (cdr (cdr lst))))
-(defun caaaar   (lst)     (car (car (car (car lst)))))
-(defun caaadr   (lst)     (car (car (car (cdr lst)))))
-(defun caadar   (lst)     (car (car (cdr (car lst)))))
-(defun caaddr   (lst)     (car (car (cdr (cdr lst)))))
-(defun cadaar   (lst)     (car (cdr (car (car lst)))))
-(defun cadadr   (lst)     (car (cdr (car (cdr lst)))))
-(defun caddar   (lst)     (car (cdr (cdr (car lst)))))
-(defun cadddr   (lst)     (car (cdr (cdr (cdr lst)))))
-(defun cdaaar   (lst)     (cdr (car (car (car lst)))))
-(defun cdaadr   (lst)     (cdr (car (car (cdr lst)))))
-(defun cdadar   (lst)     (cdr (car (cdr (car lst)))))
-(defun cdaddr   (lst)     (cdr (car (cdr (cdr lst)))))
-(defun cddaar   (lst)     (cdr (cdr (car (car lst)))))
-(defun cddadr   (lst)     (cdr (cdr (car (cdr lst)))))
-(defun cdddar   (lst)     (cdr (cdr (cdr (car lst)))))
-(defun cddddr   (lst)     (cdr (cdr (cdr (cdr lst)))))
+ (defun type?     (typ o) (eq? typ       (type           o)))
+ (defun atom?     (o)     (not (type?    :CONS           o)))
+ (defun char?     (o)          (type?    :CHAR           o))
+ (defun cons?     (o)          (type?    :CONS           o))
+ (defun core?     (o)          (type?    :CORE           o))
+ (defun env?      (o)          (type?    :ENV            o))
+ (defun error?    (o)          (type?    :ERROR          o))
+ (defun float?    (o)          (type?    :FLOAT          o))
+ (defun integer?  (o)          (type?    :INTEGER        o))
+ (defun lambda?   (o)          (type?    :LAMBDA         o))
+ (defun λ?        (o)          (type?    :LAMBDA         o))
+ (defun macro?    (o)          (type?    :MACRO          o))
+ (defun rational? (o)          (type?    :RATIONAL       o))
+ (defun string?   (o)          (type?    :STRING         o))
+ (defun symbol?   (o)          (type?    :SYMBOL         o))
+ (defun improper? (o)     (and (tail? o) (not (proper?   o))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; quasiquotation:                                                            ;;
+ ;; compound car/cdrs:                                                         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! append2
- (lambda (lst1 lst2)
-  "Append two lists."
-  (if (nil? lst1)
-   lst2
-   (cons (car lst1) (append2 (cdr lst1) lst2)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro expand-quasiquoted (expr)
- ;; "Expand a quasiquoted expression and resolve unquotes and
- ;;  unquote-splicings within."
- (cond
-  ;; If it's not a cons then it's an atom that we should quote.
-  ((atom? expr)
-   $('quote expr))
-  ;; Directly replace (unquote x) with x.
-  ((eq? (car expr) 'unquote)
-   (car (cdr expr)))
-  ;; If the second element of the list is an unquote-splicing, we want to use
-  ;; append2.
-  ((and
-    (cons? (cdr expr))
-    (cons? (cadr expr))
-    (eq?   (caadr expr) 'unquote-splicing))
-   $('append2
-     $('list $('expand-quasiquoted (car expr)))
-     (cadadr expr)))
-  ;; If the second element of the list is an unquote, use cons but without
-  ;; splicing.
-  ((and
-    (cons? (cdr expr))
-    (eq?   (cadr expr) 'unquote))
-   $('cons
-     $('expand-quasiquoted (car expr))
-     (caddr expr)))
-  ;; Error out for splicing outside of list context
-  ((eq? (car expr) 'unquote-splicing)
-   (error "unquote-splicing can't occur at top level"))
-  ;; If the list is regular, we just recurse on both its parts
-  (t $('cons
-       $('expand-quasiquoted (car expr))
-       $('expand-quasiquoted (cdr expr))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! quasiquote expand-quasiquoted)
+ (defun caar     (lst)               (car (car lst)))
+ (defun cadr     (lst)               (car (cdr lst)))
+ (defun cdar     (lst)               (cdr (car lst)))
+ (defun cddr     (lst)               (cdr (cdr lst)))
+ (defun caaar    (lst)          (car (car (car lst))))
+ (defun caadr    (lst)          (car (car (cdr lst))))
+ (defun cadar    (lst)          (car (cdr (car lst))))
+ (defun caddr    (lst)          (car (cdr (cdr lst))))
+ (defun cdaar    (lst)          (cdr (car (car lst))))
+ (defun cdadr    (lst)          (cdr (car (cdr lst))))
+ (defun cddar    (lst)          (cdr (cdr (car lst))))
+ (defun cdddr    (lst)          (cdr (cdr (cdr lst))))
+ (defun caaaar   (lst)     (car (car (car (car lst)))))
+ (defun caaadr   (lst)     (car (car (car (cdr lst)))))
+ (defun caadar   (lst)     (car (car (cdr (car lst)))))
+ (defun caaddr   (lst)     (car (car (cdr (cdr lst)))))
+ (defun cadaar   (lst)     (car (cdr (car (car lst)))))
+ (defun cadadr   (lst)     (car (cdr (car (cdr lst)))))
+ (defun caddar   (lst)     (car (cdr (cdr (car lst)))))
+ (defun cadddr   (lst)     (car (cdr (cdr (cdr lst)))))
+ (defun cdaaar   (lst)     (cdr (car (car (car lst)))))
+ (defun cdaadr   (lst)     (cdr (car (car (cdr lst)))))
+ (defun cdadar   (lst)     (cdr (car (cdr (car lst)))))
+ (defun cdaddr   (lst)     (cdr (car (cdr (cdr lst)))))
+ (defun cddaar   (lst)     (cdr (cdr (car (car lst)))))
+ (defun cddadr   (lst)     (cdr (cdr (car (cdr lst)))))
+ (defun cdddar   (lst)     (cdr (cdr (cdr (car lst)))))
+ (defun cddddr   (lst)     (cdr (cdr (cdr (cdr lst)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; list funs (retrieving by position):                                        ;;
+ ;; quasiquotation:                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun first    (lst)                    (car lst))
-(defun second   (lst)                   (cadr lst))
-(defun third    (lst)                  (caddr lst))
-(defun fourth   (lst)                 (cadddr lst))
-(defun fifth    (lst)            (car (cddddr lst)))
-(defun sixth    (lst)           (cadr (cddddr lst)))
-(defun seventh  (lst)          (caddr (cddddr lst)))
-(defun eighth   (lst)         (cadddr (cddddr lst)))
-(defun ninth    (lst)    (car (cddddr (cddddr lst))))
-(defun tenth    (lst)   (cadr (cddddr (cddddr lst))))
-(defun eleventh (lst)  (caddr (cddddr (cddddr lst))))
-(defun twelfth  (lst) (cadddr (cddddr (cddddr lst))))
+ (setq! append2
+  (lambda (lst1 lst2)
+   "Append two lists."
+   (if (nil? lst1)
+    lst2
+    (cons (car lst1) (append2 (cdr lst1) lst2)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun zero?     (n)   (= n 0))
+ (defmacro expand-quasiquoted (expr)
+  ;; "Expand a quasiquoted expression and resolve unquotes and
+  ;;  unquote-splicings within."
+  (cond
+   ;; If it's not a cons then it's an atom that we should quote.
+   ((atom? expr)
+    $('quote expr))
+   ;; Directly replace (unquote x) with x.
+   ((eq? (car expr) 'unquote)
+    (car (cdr expr)))
+   ;; If the second element of the list is an unquote-splicing, we want to use
+   ;; append2.
+   ((and
+     (cons? (cdr expr))
+     (cons? (cadr expr))
+     (eq?   (caadr expr) 'unquote-splicing))
+    $('append2
+      $('list $('expand-quasiquoted (car expr)))
+      (cadadr expr)))
+   ;; If the second element of the list is an unquote, use cons but without
+   ;; splicing.
+   ((and
+     (cons? (cdr expr))
+     (eq?   (cadr expr) 'unquote))
+    $('cons
+      $('expand-quasiquoted (car expr))
+      (caddr expr)))
+   ;; Error out for splicing outside of list context
+   ((eq? (car expr) 'unquote-splicing)
+    (error "unquote-splicing can't occur at top level"))
+   ;; If the list is regular, we just recurse on both its parts
+   (t $('cons
+        $('expand-quasiquoted (car expr))
+        $('expand-quasiquoted (cdr expr))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun nth (index lst)
- "Get the nth item in a list."
- (cond
-  ((zero? index) (car lst))
-  (lst          (nth (- index 1) (cdr lst)))))
+ (setq! quasiquote expand-quasiquoted)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun last (lst)
- "Get last item in a list."
- (cond
-  ((nil? (cdr lst)) lst)
-  (lst              (last (cdr lst)))))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ;; list funs (retrieving by position):                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defun first    (lst)                    (car lst))
+ (defun second   (lst)                   (cadr lst))
+ (defun third    (lst)                  (caddr lst))
+ (defun fourth   (lst)                 (cadddr lst))
+ (defun fifth    (lst)            (car (cddddr lst)))
+ (defun sixth    (lst)           (cadr (cddddr lst)))
+ (defun seventh  (lst)          (caddr (cddddr lst)))
+ (defun eighth   (lst)         (cadddr (cddddr lst)))
+ (defun ninth    (lst)    (car (cddddr (cddddr lst))))
+ (defun tenth    (lst)   (cadr (cddddr (cddddr lst))))
+ (defun eleventh (lst)  (caddr (cddddr (cddddr lst))))
+ (defun twelfth  (lst) (cadddr (cddddr (cddddr lst))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defun zero?     (n)   (= n 0))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defun nth (index lst)
+  "Get the nth item in a list."
+  (cond
+   ((zero? index) (car lst))
+   (lst          (nth (- index 1) (cdr lst)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defun last (lst)
+  "Get last item in a list."
+  (cond
+   ((nil? (cdr lst)) lst)
+   (lst              (last (cdr lst)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
 
 (print-loaded) (exit)
 
