946cf7d2b758f8116a2719f5f01fc25057350eee
Ariane Emory <ariane.emory@protonmail.com>
2023-10-22 19:16:02 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -118,57 +118,59 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  )
 
-(print-loaded) (exit)
 
+(report-time "def quasiquote"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; quasiquotation:                                                            ;;
+ ;; quasiquotation:                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! append2
- (lambda (lst1 lst2)
-  "Append two lists."
-  (if (nil? lst1)
-   lst2
-   (cons (car lst1) (append2 (cdr lst1) lst2)))))
+ (setq! append2
+  (lambda (lst1 lst2)
+   "Append two lists."
+   (if (nil? lst1)
+    lst2
+    (cons (car lst1) (append2 (cdr lst1) lst2)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro expand-quasiquoted (expr)
- ;; "Expand a quasiquoted expression and resolve unquotes and
- ;;  unquote-splicings within."
- (cond
-  ;; If it's not a cons then it's an atom that we should quote.
-  ((atom? expr)
-   $('quote expr))
-  ;; Directly replace (unquote x) with x.
-  ((eq? (car expr) 'unquote)
-   (car (cdr expr)))
-  ;; If the second element of the list is an unquote-splicing, we want to use
-  ;; append2.
-  ((and
-    (cons? (cdr expr))
-    (cons? (cadr expr))
-    (eq?   (caadr expr) 'unquote-splicing))
-   $('append2
-     $('list $('expand-quasiquoted (car expr)))
-     (cadadr expr)))
-  ;; If the second element of the list is an unquote, use cons but without
-  ;; splicing.
-  ((and
-    (cons? (cdr expr))
-    (eq?   (cadr expr) 'unquote))
-   $('cons
-     $('expand-quasiquoted (car expr))
-     (caddr expr)))
-  ;; Error out for splicing outside of list context
-  ((eq? (car expr) 'unquote-splicing)
-   (error "unquote-splicing can't occur at top level"))
-  ;; If the list is regular, we just recurse on both its parts
-  (t $('cons
-       $('expand-quasiquoted (car expr))
-       $('expand-quasiquoted (cdr expr))))))
+ (defmacro expand-quasiquoted (expr)
+  ;; "Expand a quasiquoted expression and resolve unquotes and
+  ;;  unquote-splicings within."
+  (cond
+   ;; If it's not a cons then it's an atom that we should quote.
+   ((atom? expr)
+    $('quote expr))
+   ;; Directly replace (unquote x) with x.
+   ((eq? (car expr) 'unquote)
+    (car (cdr expr)))
+   ;; If the second element of the list is an unquote-splicing, we want to use
+   ;; append2.
+   ((and
+     (cons? (cdr expr))
+     (cons? (cadr expr))
+     (eq?   (caadr expr) 'unquote-splicing))
+    $('append2
+      $('list $('expand-quasiquoted (car expr)))
+      (cadadr expr)))
+   ;; If the second element of the list is an unquote, use cons but without
+   ;; splicing.
+   ((and
+     (cons? (cdr expr))
+     (eq?   (cadr expr) 'unquote))
+    $('cons
+      $('expand-quasiquoted (car expr))
+      (caddr expr)))
+   ;; Error out for splicing outside of list context
+   ((eq? (car expr) 'unquote-splicing)
+    (error "unquote-splicing can't occur at top level"))
+   ;; If the list is regular, we just recurse on both its parts
+   (t $('cons
+        $('expand-quasiquoted (car expr))
+        $('expand-quasiquoted (cdr expr))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! quasiquote expand-quasiquoted)
+ (setq! quasiquote expand-quasiquoted)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
 
 
+(report-time "get numbered access"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; list funs (retrieving by position):                                        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -199,6 +201,7 @@
   ((nil? (cdr lst)) lst)
   (lst              (last (cdr lst)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
 
 
 (report-time "def tail-chaser macros"
@@ -630,8 +633,10 @@
   "Take shallow copy of the given list."
   (when lst (cons (car lst) (copy-list (cdr lst)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
 
 
+(report-time "def list predicates"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; list funs (more unsorted):                                                ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -765,7 +770,7 @@
  )
 
 
-(report-time "def remainder"
+(report-time "def fancy prints"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; fancy output funs:                                                        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -789,8 +794,10 @@
  (defun putns   args       (apply putni   (cons " " args)))
  (defun writens args       (apply writeni (cons " " args)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
 
 
+(report-time "def unsorted"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; random unsorted stuff:                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -883,7 +890,9 @@
     $('put! 't ':constant $('quote sym))
     value))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+)
 
+(report-time "def scheme compat"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; tiny-clos scheme compat:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
