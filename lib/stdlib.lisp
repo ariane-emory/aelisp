70cec8b669cf2d4f76e16e3170f3eb0e64e41aa8
Ariane Emory <ariane.emory@protonmail.com>
2023-10-22 02:46:59 -0400

    stdlib

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -853,7 +853,7 @@
   (setq! display       write) ;should should be a macro that avoids re-defining what-scheme-implementation
   (setq! else          t)
   (setq! every         all?)
-  (defun funcall args  (eval args))
+  ;;(defun funcall args  (eval args))
   (setq! getl          pget)
   (setq! gsort         sort)
   (setq! make-vector   make-list)
