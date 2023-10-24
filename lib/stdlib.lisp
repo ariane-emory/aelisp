7ceb8859017c1459910ebedd42628b77e0bd4938
Ariane Emory <ariane.emory@protonmail.com>
2023-10-23 21:25:49 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -637,7 +637,7 @@
    (error "push! expects a symbol referring to a list"))
   `(setq! ,list-sym (cons ,val ,list-sym)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  )
+ )
 
 
 (report-time-us "def union                      "
@@ -964,43 +964,43 @@
 
 (report-time-us "def list split funs            "
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun split-list-alternate! (pred? lst)
- "Destructively split the LST into two sublists:
+ (defun split-list-alternate! (pred? lst)
+  "Destructively split the LST into two sublists:
    1. The longest initial sublist of elements satisfying PRED?
    2. The rest of the elements."
- (let ((front nil)
-       (back lst))
-  (while (and back (pred? (car back)))
-   (push! (pop! back) front))
-  $((reverse front) back)))
+  (let ((front nil)
+        (back lst))
+   (while (and back (pred? (car back)))
+    (push! (pop! back) front))
+   $((reverse front) back)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun split-list (pred? lst)
- "Destructivly split LST into two sublists:
+ (defun split-list (pred? lst)
+  "Destructivly split LST into two sublists:
    1. The longest initial sublist of elements satisfying PRED?
    2. The rest of the elements."
- (let ((prev nil)
-       (current lst))
-  (while (and current (pred? (car current)))
-   (setq! prev current)
-   (setq! current (cdr current)))
-  (if prev
-   (progn
-    (rplacd! prev nil)
-    $(lst current))
-   $(nil lst))))
+  (let ((prev nil)
+        (current lst))
+   (while (and current (pred? (car current)))
+    (setq! prev current)
+    (setq! current (cdr current)))
+   (if prev
+    (progn
+     (rplacd! prev nil)
+     $(lst current))
+    $(nil lst))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun split-list (pred? lst)
- "Destructivly split LST into two sublists:
+ (defun split-list (pred? lst)
+  "Split LST into two sublists:
    1. The longest initial sublist of elements satisfying PRED?
    2. The rest of the elements."
- (let ((front nil)
-       (current lst))
-  (while (and current (pred? (car current)))
-   (setq front (cons (car current) front))
-   (setq current (cdr current)))
-  $((reverse front) current)))
+  (let ((front nil)
+        (current lst))
+   (while (and current (pred? (car current)))
+    (setq front (cons (car current) front))
+    (setq current (cdr current)))
+   $((reverse front) current)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-)
+ )
 
 
 (report-time-us "def selection sort parts       "
