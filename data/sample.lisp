009a013e6b6a9ab5142589d1483f42ac27fa17db
Ariane Emory <ariane.emory@protonmail.com>
2023-10-23 21:42:40 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -1,31 +1,32 @@
+(ignore
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq! lst $(2 4 1 5 3 7 9 6 8))
+ (setq! lst $(2 4 1 5 3 7 9 6 8))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(princ (select-and-move-to-front! (lambda (o) (eql? o 9)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 8)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 7)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 6)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 5)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 4)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 3)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 2)) lst)) (spc) (write lst) (nl)
-(princ (select-and-move-to-front! (lambda (o) (eql? o 1)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 9)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 8)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 7)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 6)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 5)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 4)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 3)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 2)) lst)) (spc) (write lst) (nl)
+ (princ (select-and-move-to-front! (lambda (o) (eql? o 1)) lst)) (spc) (write lst) (nl)
 
-(select-and-move-to-front! (lambda (o) (eql? o 2)) (list 2))
+ (select-and-move-to-front! (lambda (o) (eql? o 2)) (list 2))
 
-(princ (nthcdr 0 lst)) (nl)
-(princ (nthcdr 1 lst)) (nl)
-(princ (nthcdr 2 lst)) (nl)
-(setq! c (curry1 nthcdr 3))
-(princ (c lst)) (nl)
+ (princ (nthcdr 0 lst)) (nl)
+ (princ (nthcdr 1 lst)) (nl)
+ (princ (nthcdr 2 lst)) (nl)
+ (setq! c (curry1 nthcdr 3))
+ (princ (c lst)) (nl)
 
-(princ (pop! lst)) (nl)
-(princ lst) (nl)
-(princ (push! 99 lst)) (nl)
-(princ lst) (nl)
+ (princ (pop! lst)) (nl)
+ (princ lst) (nl)
+ (princ (push! 99 lst)) (nl)
+ (princ lst) (nl))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -46,21 +47,23 @@
 ;;    (setq current (cdr current)))
 ;;   $((fake-reverse front) current)))
 
-(s lst   '("asdw" "erer" "rerw" 1 nil (lambda (x) x) "zoop" z (1 2 . 3) 8))
-(s split (split-list string? lst))
-(princ "front: ") (write (apply concat (intercalate " " (car split))))  (nl)
-(princ "back:  ") (write (cadr split)) (nl)
+(ignore
+ (s lst   '("asdw" "erer" "rerw" 1 nil (lambda (x) x) "zoop" z (1 2 . 3) 8))
+ (s split (split-list string? lst))
+ (princ "front: ") (write (apply concat (intercalate " " (car split))))  (nl)
+ (princ "back:  ") (write (cadr split)) (nl))
 
 (defmacro defun-doc (name params . docs-and-body)
- (let ((splitted (split-list string? docs-and-body))
-       (docs     (apply concat (intercalate " " (car splitted))))
-       (body     (cadr split)))
+ (let* ((split (split-list string? docs-and-body))
+        (docs  (apply concat (intercalate " " (car split))))
+        (body  (cadr split)))
   $('progn 
     $('setq! name $('lambda params . body))
-    $('put! docs :docs name))))
+    $('put! docs ':doc name))))
 
 (defun-doc somefun (x y)
  "Multiply two"
  "numbers."
  (* x y))
 
+(write (get :doc somefun))
