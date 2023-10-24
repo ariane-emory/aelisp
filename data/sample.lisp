f41ec81b7632e1e0fac73239c1fb750690f1be97
Ariane Emory <ariane.emory@protonmail.com>
2023-10-23 23:33:04 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -73,7 +73,7 @@
  (let* ((doc       (or (get :doc obj) "This object has no documentation."))
         (binding   (get :last-bound-to obj))
         (is-fun    (or (lambda? obj) (macro? obj)))
-        (name      (if binding (symbol-name binding) (string obj)))
+        (name      (or binding (string obj)))
         (params    (when is-fun
                     (string (cons name (params obj)))))
         (docstring (if is-fun
