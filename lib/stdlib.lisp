af9af2a4160e17a0a7476a2a8d0b3a33e73c4b64
Ariane Emory <ariane.emory@protonmail.com>
2023-10-24 16:04:49 -0400

    ...

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -270,51 +270,59 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; list funs (reduction):                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(letrec
- ((reduce-inner
-   (lambda (fun lst)
-    (cond
-     ((nil? lst)        nil)
-     ((nil? (cdr lst))  (car lst))
-     (t                 (reduce-inner fun (cons (fun (car lst) (cadr lst)) (cddr lst))))))))
- (defun reduce (fun lst . init-val)
-  "Left-reduce ('foldl' in Haskell) LST by applying FUN to successive pairs."
-  (cond
-   ((nil? init-val)   (reduce-inner fun lst))
-   ((cdr init-val)    (error "init-val must be a single object"))
-   (t                 (reduce-inner fun (cons (car init-val) lst))))))
+ (letrec
+  ((reduce-inner
+    (lambda (fun lst)
+     (cond
+      ((nil? lst)        nil)
+      ((nil? (cdr lst))  (car lst))
+      (t                 (reduce-inner fun (cons (fun (car lst) (cadr lst)) (cddr lst))))))))
+  (defun reduce (fun lst . init-val)
+   "Left-reduce ('foldl' in Haskell) LST by applying FUN to successive pairs."
+   (cond
+    ((nil? init-val)   (reduce-inner fun lst))
+    ((cdr init-val)    (error "init-val must be a single object"))
+    (t                 (reduce-inner fun (cons (car init-val) lst))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(letrec
- ((rreduce-inner
-   (lambda (fun lst)
-    (cond
-     ((nil? lst)        nil)
-     ((nil? (cdr lst))  (car lst))
-     (t                 (fun (car lst) (rreduce-inner fun (cdr lst))))))))
- (defun rreduce (fun lst . init-val)
-  "Right-reduce ('foldr' in Haskell) LST by applying FUN to successive pairs."
-  (cond
-   ((nil? init-val)   (rreduce-inner fun lst))
-   ((cdr init-val)    (error "init-val must be a single object"))
-   (t                 (rreduce-inner fun (append2 lst (list (car init-val))))))))
+ (letrec
+  ((rreduce-inner
+    (lambda (fun lst)
+     (cond
+      ((nil? lst)        nil)
+      ((nil? (cdr lst))  (car lst))
+      (t                 (fun (car lst) (rreduce-inner fun (cdr lst))))))))
+  (defun rreduce (fun lst . init-val)
+   "Right-reduce ('foldr' in Haskell) LST by applying FUN to successive pairs."
+   (cond
+    ((nil? init-val)   (rreduce-inner fun lst))
+    ((cdr init-val)    (error "init-val must be a single object"))
+    (t                 (rreduce-inner fun (append2 lst (list (car init-val))))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun reduced (fun . init-val)
   "Return a function that is a left reduction of the binary function FUN."
-  (lambda (lst) (reduce fun lst . init-val)))
+  (if (and init-val (cdr init-val))
+   (error "init-val must be a single object")
+   (lambda (lst) (reduce fun lst . init-val))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun reduced* (fun . init-val)
   "Return a function that is a left reduction of the binary function FUN."
   "which takes loose args."
-  (lambda args ((reduced fun . init-val) args)))
+  (if (and init-val (cdr init-val))
+   (error "init-val must be a single object")
+   (lambda args ((reduced fun . init-val) args))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun rreduced (fun . init-val)
   "Return a function that is a right reduction of the binary function FUN."
-  (lambda (lst) (rreduce fun lst . init-val)))
+  (if (and init-val (cdr init-val))
+   (error "init-val must be a single object")
+   (lambda (lst) (rreduce fun lst . init-val))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun rreduced* (fun . init-val)
   "Return a function that is a right reduction of the binary function FUN."
   "which takes loose args."
-  (lambda args ((rreduced fun . init-val) args)))
+  (if (and init-val (cdr init-val))
+   (error "init-val must be a single object")
+   (lambda args ((rreduced fun . init-val) args))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  )
 
