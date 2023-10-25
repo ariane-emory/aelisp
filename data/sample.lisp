40308c38970cbcd9aa55ba5ad2bcf5679b4cd651
Ariane Emory <ariane.emory@protonmail.com>
2023-10-24 22:27:03 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -237,6 +237,7 @@
 (confirm that (mapconcat string '(These  are  words) " ")           returns "These are words")
 (confirm that (mapconcat string '(These "are" words) " ")           returns "These are words")
 
+(confirm that (append      '(1 2) '(3 4) '(5 6))                    returns '(1 2 3 4 5 6))
 (confirm that (mapcan id  '((1 2) (3 4) (5 6)))                     returns '(1 2 3 4 5 6))
 (confirm that (mapcan cdr '((1 2 3) (4 5 6) (7 8 9)))               returns '(2 3 5 6 8 9))
 
