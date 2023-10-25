26482b33672ac2898c0d6486bd44e4f9121beeaa
Ariane Emory <ariane.emory@protonmail.com>
2023-10-25 17:18:54 -0400

    ...

--- src/core/core_string.c
+++ src/core/core_string.c
@@ -20,3 +20,20 @@ ae_obj_t * ae_core_string(ae_obj_t * const env, ae_obj_t * const args, __attribu
   CORE_RETURN("string", ret);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// _intern
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ae_obj_t * ae_core_intern(ae_obj_t * const env, ae_obj_t * const args, __attribute__((unused)) int args_length) {
+  CORE_BEGIN("intern");
+
+  if (! STRINGP(CAR(args)))
+    LOG(CAR(args), "not a string");
+  
+  REQUIRE(env, args, STRINGP(CAR(args)), "intern's 1st arg must be a string");
+
+  ret = SYM(STR_VAL(CAR(args)));
+  
+  CORE_RETURN("intern", ret);
+}
+
