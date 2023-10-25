446503ea0b468fafd078e80ccb6b6a7c345f8278
Ariane Emory <ariane.emory@protonmail.com>
2023-10-24 21:32:27 -0400

    ...

--- data/sample.lisp
+++ data/sample.lisp
@@ -193,6 +193,10 @@
 
 (nl)
 
+(confirm that ((reduced   +  )  $(1 2 3 4))    returns 10)
+(confirm that ((reduced   + 4)  $(1 2 3  ))    returns 10)
+(confirm that ((rreduced  +  )  $(1 2 3 4))    returns 10)
+(confirm that ((rreduced  + 4)  $(1 2 3  ))    returns 10)
 (confirm that ((reduced   +  )  '(1 2 3 4))    returns 10)
 (confirm that ((reduced   + 4)  '(1 2 3  ))    returns 10)
 (confirm that ((rreduced  +  )  '(1 2 3 4))    returns 10)
