5fad66d2635d6ea0953717216b5f4a849c0a6ba3
Ariane Emory <ariane.emory@protonmail.com>
2023-10-25 23:38:31 -0400

    ...

--- src/common.c
+++ src/common.c
@@ -266,8 +266,9 @@ ae_obj_t * load_file(const char * filename, bool * const failed_to_open) {
     *failed_to_open = false;
   }
 
-  char * const file_basename = basename((char *)filename);
+  char * const file_basename     = basename((char *)filename);
   char * const file_basename_str = free_list_malloc(strlen(file_basename) + 1);
+  
   strcpy(file_basename_str, file_basename);
 
   ae_obj_t * loaded_file = NEW_STRING(file_basename_str);
