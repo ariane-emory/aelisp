3d61ace8bbc0440cbe1c3f9d6d55af258660865d
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 13:07:02 -0400

    ...

--- src/core/core_props.c
+++ src/core/core_props.c
@@ -10,17 +10,21 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ae_obj_t * ae_core_set_props(ae_obj_t * const env,
-                         ae_obj_t * const args,
-                         __attribute__((unused)) int args_length) {
-  CORE_BEGIN("set_props");
+                            ae_obj_t * const args,
+                            __attribute__((unused)) int args_length) {
+  CORE_BEGIN("props!");
 
-  ae_obj_t * obj       = RETURN_IF_ERRORP(MAYBE_EVAL(CAR(args)));
-  ae_obj_t * prop_list = PROPS(obj);
-  ret                  = prop_list;               
+  ae_obj_t * value         = RETURN_IF_ERRORP(EVAL(env, CAR(args))); // this could be unsafe if value is NIL, maybe?
+  ae_obj_t * key           = RETURN_IF_ERRORP(EVAL(env, CADR(args)));
+  ae_obj_t * obj           = RETURN_IF_ERRORP(MAYBE_EVAL(CADDR(args)));
+  ae_obj_t * prop_list     = PROPS(obj);
+  ae_obj_t * new_prop_list = KSET(prop_list, key, value);
+  PROPS(obj)               = new_prop_list;
+  ret                      = value;
 
 end:
   
-  CORE_RETURN("set_props", ret);
+  CORE_RETURN("props!", ret);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,7 +52,7 @@ end:
 ae_obj_t * ae_core_put_prop(ae_obj_t * const env,
                             ae_obj_t * const args,
                             __attribute__((unused)) int args_length) {
-  CORE_BEGIN("pus!");
+  CORE_BEGIN("put!");
 
   ae_obj_t * value         = RETURN_IF_ERRORP(EVAL(env, CAR(args))); // this could be unsafe if value is NIL, maybe?
   ae_obj_t * key           = RETURN_IF_ERRORP(EVAL(env, CADR(args)));
