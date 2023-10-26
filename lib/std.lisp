ba128735b31197d04a88cef26162a8bdf31d4207
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 19:37:57 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -1363,38 +1363,6 @@
  )
 
 
-(report-time-us "def string funs                "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun make-string (size init-val)
-  "Make a new string of length SIZE with it's chars set to INIT-VAL."
-  (unless (integer? size)         (error "SIZE must be an integer."))
-  (unless (string? init-val)      (error "INIT-VAL must be a string."))
-  (unless (= 1 (length init-val)) (error "INIT-VAL must be a string of length 1."))
-  (apply concat (make-list size init-val)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun pad-string-right (size init-val str)
-  "Pad STR to SIZE with INIT-VAL."
-  (unless (integer? size)         (error "SIZE must be an integer."))
-  (unless (string? init-val)      (error "INIT-VAL must be a string."))
-  (unless (= 1 (length init-val)) (error "INIT-VAL must be a string of length 1."))
-  (let ((pad (make-string (- size (length str)) init-val)))
-   (concat str pad)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun pad-string-left (size init-val str)
-  "Pad STR to SIZE with INIT-VAL."
-  (unless (integer? size)         (error "SIZE must be an integer."))
-  (unless (string? init-val)      (error "INIT-VAL must be a string."))
-  (unless (= 1 (length init-val)) (error "INIT-VAL must be a string of length 1."))
-  (let ((pad (make-string (- size (length str)) init-val)))
-   (concat pad str)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun left-justify  (size str) (pad-string-right size " " str))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun right-justify (size str) (pad-string-left size " " str))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
-
 (report-time-us "def feature?/provide           "
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun feature? (feature)
@@ -1414,8 +1382,9 @@
 
 
 (mapc* require
- 'prog-macros
+ 'strings
  'prop-removeb
+ 'prog-macros
  'plist-funs
  'prop-removeb
  'scheme-compat
