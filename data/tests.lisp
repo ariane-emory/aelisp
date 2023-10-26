09ba61a0d8c00352f39ce4e7656e03f146b93f85
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 12:06:46 -0400

    ...

--- data/tests.lisp
+++ data/tests.lisp
@@ -305,25 +305,11 @@
 (when (and (bound? 'provide) (fun? provide)) (provide :tests))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmacro princn  exprs           $('progn (cons 'princ exprs) $('nl)))
-(defmacro printn  exprs           $('progn (cons 'print exprs) $('nl)))
-(defmacro putn    exprs           $('progn (cons 'put  exprs)  $('nl)))
-(defmacro writen  exprs           $('progn (cons 'write exprs) $('nl)))
 
-(defmacro princni (delim . exprs) $('apply 'princn $('intercalate delim $('quote exprs))))
-(defmacro printi  (delim . exprs) $('apply 'print  $('intercalate delim $('quote exprs))))
-(defmacro putni   (delim . exprs) $('apply 'putn   $('intercalate delim $('quote exprs))))
-(defmacro writeni (delim . exprs) $('apply 'writen $('intercalate delim $('quote exprs))))
-
-(defmacro princns exprs           $('princni " " . exprs))
-(defmacro prints  exprs           $('princti " " . exprs))
-(defmacro putns   exprs           $('putni   " " . exprs))
-(defmacro writens exprs           $('writeni " " . exprs))
-
-;; (log-macro t)
+;;(log-macro t)
 
 (princni ", " 1 2 3)
 
-(log-macro nil)
+;;(log-macro nil)
 
 (princns 1 2 3)
