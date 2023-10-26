5a360ca16e61fb903958b8e49f3e58d6d31078ed
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 19:16:11 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -1469,24 +1469,11 @@
  )
 
 
-(report-time-us "def aliases for std funs        "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- ;; simple aliases:                                                                      ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (setq! pkeys     plist-keys)
- (setq! pvals     plist-values)
- (setq! premove   plist-remove)
- (setq! premoveq  plist-removeq)
- (setq! premoveql plist-removeql)
- (setq! ljust     left-justify)
- (setq! rjust     right-justify)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
 
 
 (require 'scheme-compat)
 (require 'elisp-compat)
-
+(require 'std-aliases)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (print-load-time-us)
