0e5279a4b6197747e8adfaa004c0e73fbffb536b
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 17:21:08 -0400

    ...

--- src/eval.c
+++ src/eval.c
@@ -544,7 +544,7 @@ static ae_obj_t * lookup(ae_obj_t * env, ae_obj_t * sym) {
     ae_obj_t * const err = NEW_ERROR("%s:%d: unbound symbol '%s'", __FILE__, __LINE__, SYM_VAL(sym));
 
     PUT_PROP(env, "error-env",             err);
-    PUT_PROP(sym, "errror-unbound-symbol", err);
+    PUT_PROP(sym, "error-unbound-symbol", err);
     
     RETURN(err);
   }
