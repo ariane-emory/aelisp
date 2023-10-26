b52ef04cd6ac6b053cd37243ad7d7eb670d23d98
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 19:40:12 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -1229,36 +1229,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  )
 
-(report-time-us "def prime?/primes              "
- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun prime? (num)
-  "Check if a number is prime."
-  (unless (integer? num) (error "NUM must be an integer"))
-  (if (or (= num 0) (= num 1))
-   nil
-   (let ((limit (/ num 2))
-         (divisor 2)
-         (is-prime t))
-    (while (and (<= divisor limit) is-prime)
-     (if (= (% num divisor) 0)
-      (setq! is-prime nil)
-      (setq! divisor (+ divisor 1))))
-    is-prime)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun primes (n)
-  "Return the first n prime numbers."
-  (unless (integer? n) (error "N must be an integer"))
-  (let ((count 0)
-        (num 2)
-        (primes '()))
-   (while (< count n)
-    (when (prime? num)
-     (setq! count (+ count 1))
-     (setq! primes (append2 primes (list num))))
-    (setq! num (+ num 1)))
-   primes))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
 
 (report-time-us "def selection sort parts       "
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -1335,6 +1305,7 @@
 
 
 (mapc* require
+ 'primes
  'confirm
  'strings
  'prop-removeb
