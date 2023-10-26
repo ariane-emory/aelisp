21ca51226edaaf32bac5079364e10ae331a33b04
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 19:34:36 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -1395,26 +1395,6 @@
  )
 
 
-(report-time "def prog1/prog2                "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro prog1 (expr1 . exprs)
-  "Evaluate EXPR1, then evaluate EXPRS in order, and return the value of EXPR1."
-  $('let $($('result $('eval expr1)))
-    $('progn . exprs)
-    'result))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro prog2 (expr1 expr2 . exprs)
-  "Evaluate EXPR1, then evaluate EXPR2, then evaluate EXPRS in order, and return the value of"
-  "EXPR2."
-  $('progn
-    expr1
-    $('let $($('result2 $('eval expr2)))
-      $('progn . exprs)
-      'result2)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
-
 (report-time-us "def prop remove!               "
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defmacro remove! (prop obj)
@@ -1443,7 +1423,8 @@
  )
 
 
-(mapc* require 
+(mapc* require
+ 'prog-macros
  'plist-funs
  'prop-removeb
  'scheme-compat
