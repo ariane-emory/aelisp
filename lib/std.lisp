ccf8f7400e734c40f6aa5a9e2b428f3a007d9e14
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 19:17:32 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -1415,32 +1415,6 @@
  )
 
 
-(report-time-us "def plist funs                 " 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun plist-keys (plist)
-  "Extracts the keys from a plist PLIST."
-  (unless (list? plist)          (error "PLIST must be a list"))
-  (when plist (cons (car plist) (plist-keys (cddr plist)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun plist-values (plist)
-  "Extracts the values from a plist PLIST."
-  (when plist (plist-keys (cdr plist))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun plist-remove (pred? prop plist)
-  (unless (list? plist)          (error "PLIST must be a list"))
-  (unless (even? (length plist)) (error "PLIST must have an even number of elements"))
-  (when plist
-   (if (pred? prop (car plist))
-    (plist-remove pred? prop (cddr plist))
-    (cons (car plist) (cons (cadr plist) (plist-remove pred? prop (cddr plist)))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun plist-removeq  (prop plist) (plist-remove eq?  prop plist))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun plist-removeql (prop plist) (plist-remove eql? prop plist))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
-
 (report-time-us "def remove!                    "
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defmacro remove! (prop obj)
@@ -1471,6 +1445,7 @@
 
 
 
+(require 'plist-funs)
 (require 'scheme-compat)
 (require 'elisp-compat)
 (require 'std-aliases)
