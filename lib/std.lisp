c68d1cd20cd7c5060cc4f21499cf2749eb672017
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 16:43:06 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -138,21 +138,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (progn
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro time exprs
-  "Return how long it takes to evaluate EXPRS in milliseconds."
-  $('let $($('begin $('now)))
-    (cons 'progn exprs)
-    $('elapsed 'begin)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro report-time (msg . exprs)
-  (unless (eq? :STRING (type msg)) (error "MSG must be a string"))
-  $('progn
-    $('princ msg)
-    $('let $($('time-taken (cons 'time exprs)))
-      $('princ '" in ")
-      $('princ 'time-taken)
-      $('princ '" us.")
-      $('nl))))
+;;  (defmacro time exprs
+;;   "Return how long it takes to evaluate EXPRS in milliseconds."
+;;   $('let $($('begin $('now)))
+;;     (cons 'progn exprs)
+;;     $('elapsed 'begin)))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  (defmacro report-time (msg . exprs)
+;;   (unless (eq? :STRING (type msg)) (error "MSG must be a string"))
+;;   $('progn
+;;     $('princ msg)
+;;     $('let $($('time-taken (cons 'time exprs)))
+;;       $('princ '" in ")
+;;       $('princ 'time-taken)
+;;       $('princ '" us.")
+;;       $('nl))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defmacro time-us exprs
   "Return how long it takes to evaluate EXPRS in microseconds."
@@ -164,13 +164,13 @@
   (unless (eq? :STRING (type msg)) (error "MSG must be a string"))
   $('progn
     $('let $($('time-taken (cons 'time-us exprs)))
-      $('nl)
-      $('princ '"Completed ")
-      $('princ msg)
-      $('princ '" in ")
-      $('princ 'time-taken)
-      $('princ '" us.")
-      $('nl)
+      ;; $('nl)
+      ;; $('princ '"Completed ")
+      ;; $('princ msg)
+      ;; $('princ '" in ")
+      ;; $('princ 'time-taken)
+      ;; $('princ '" us.")
+      ;; $('nl)
       )))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (when t
