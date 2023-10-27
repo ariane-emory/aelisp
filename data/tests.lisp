de5ce460aa1d3093d582c3a8d9ab845d70b41e8b
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 14:46:21 -0400

    ...

--- data/tests.lisp
+++ data/tests.lisp
@@ -347,27 +347,27 @@
  "Return the zero-based index of the first occurrence of ELEM in LST, or nil if"
  "ELEM does not appear in the list. Comparison done with 'eq?'."
  (unless (list? lst) (error "LST must be a list"))
- (let ((found nil)
-       (idx 0))
+ (let ((idx 0)
+       (found nil))
   (while (and lst (not found))
    (if (eq? elem (car lst))
-    (setq! found t)
+    (setq! found idx)
     (setq! idx (+ 1 idx))
     (setq! lst (cdr lst))))
-  (if found idx nil)))
+  found))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun indexql (elem lst)
  "Return the zero-based index of the first occurrence of ELEM in LST, or nil if"
  "ELEM does not appear in the list. Comparison done with 'eql?'."
  (unless (list? lst) (error "LST must be a list"))
- (let ((found nil)
-       (idx 0))
+ (let ((idx 0)
+       (found nil))
   (while (and lst (not found))
    (if (eql? elem (car lst))
-    (setq! found t)
+    (setq! found idx)
     (setq! idx (+ 1 idx))
     (setq! lst (cdr lst))))
-  (if found idx nil)))
+  found))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun removeq (elem lst)
  "Non-destructively remove ELEM from LST. Comparison done with 'eq?'."
