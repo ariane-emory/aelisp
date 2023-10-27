3f376d2e9bb3d548e5cbafe85614c7e38d4c312c
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 15:21:10 -0400

    ...

--- lib/deprecated.lisp
+++ lib/deprecated.lisp
@@ -110,7 +110,30 @@
 (setq! memql?   (make-member-pred eql?))
 (setq! removeql (make-remove-fun  eql?))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'tail-chaser-funs)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; deprecated list funs (vector-style list API):
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq! list-set!
+ (make-chase-fun (lst index)
+  ((zero? index) (rplaca! position (car rest)))
+  (position (chase (1- index) (car rest)))
+  (t (error "INDEX out of range"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq! list-ref
+ (make-chase-fun (lst index)
+  ((zero? index) head)
+  (position (chase (1- index)))
+  (t (error "INDEX out of range"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun make-list (size init-val)
+ "Make a new list of length SIZE with it's cars set to INIT-VAL."
+ (unless (integer? size) (error "SIZE must be an integer"))
+ (cond
+  ((zero? size)  nil)
+  (t            (cons init-val (make-list (1- size) init-val)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (provide 'deprecated)
