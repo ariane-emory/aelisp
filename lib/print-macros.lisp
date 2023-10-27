03b2d890ea98462c8dc9d30a686c666eef7c8b71
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 23:24:10 -0400

    ...

---
+++ lib/print-macros.lisp
@@ -0,0 +1,26 @@
+(require 'std-minimal)
+(require 'measure-time)
+(require 'list-funs)
+
+(report-time-us "def fancy prints               "
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ;; fancy output macros:                                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defmacro princn  exprs           $('progn (cons 'princ exprs) $('nl)))
+ (defmacro printn  exprs           $('progn (cons 'print exprs) $('nl)))
+ (defmacro putn    exprs           $('progn (cons 'put  exprs)  $('nl)))
+ (defmacro writen  exprs           $('progn (cons 'write exprs) $('nl)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defmacro princni (delim . exprs) $('apply 'princn $('intercalate delim $('quote exprs))))
+ (defmacro printi  (delim . exprs) $('apply 'print  $('intercalate delim $('quote exprs))))
+ (defmacro putni   (delim . exprs) $('apply 'putn   $('intercalate delim $('quote exprs))))
+ (defmacro writeni (delim . exprs) $('apply 'writen $('intercalate delim $('quote exprs))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ (defmacro princns exprs           $('princni " " . exprs))
+ (defmacro prints  exprs           $('princti " " . exprs))
+ (defmacro putns   exprs           $('putni   " " . exprs))
+ (defmacro writens exprs           $('writeni " " . exprs))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ )
+
+(provide 'print-macros)
