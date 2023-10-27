2afc21e3d0c3dc73e06445b1ffe1e3cc00a487af
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 15:18:30 -0400

    ...

--- data/tests.lisp
+++ data/tests.lisp
@@ -331,35 +331,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun list-set! (lst index obj)
-  "Destructively set the element at INDEX of LST to OBJ."
-  (unless (list? lst) (error "LST must be a list"))
-  (unless (and (integer? index) (>= index 0)) (error "INDEX must be a non-negative integer"))
-  (let ((current-index 0)
-        (done nil))
-    (while (and lst (not done))
-      (if (= current-index index) ; <- changed eq? to =
-          (progn
-            (rplaca! lst obj)
-            (setq! done t))
-          (setq! lst (cdr lst))
-          (setq! current-index (+ 1 current-index))))
-    (unless done (error "INDEX out of bounds")))
-  obj) ; return the set object for convenience, similar to setq!
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun list-ref (lst index)
-  "Return the element at INDEX of LST."
-  (unless (list? lst) (error "LST must be a list"))
-  (unless (and (integer? index) (>= index 0)) (error "INDEX must be a non-negative integer"))
-  (let ((current-index 0))
-    (while (and lst (not (= current-index index)))
-      (setq! lst (cdr lst))
-      (setq! current-index (+ 1 current-index)))
-    (if lst
-        (car lst)
-        (error "INDEX out of bounds"))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (setq! l (make-list 10 10)) ;; l is now '(10 10 10 10 10 10 10 10 10 10)
--- lib/std.lisp
+++ lib/std.lisp
@@ -1243,6 +1243,34 @@
    (position (chase (1- index)))
    (t (error "INDEX out of range"))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun list-set! (lst index obj)
+  "Destructively set the element at INDEX of LST to OBJ."
+  (unless (list? lst) (error "LST must be a list"))
+  (unless (and (integer? index) (>= index 0)) (error "INDEX must be a non-negative integer"))
+  (let ((current-index 0)
+        (done nil))
+    (while (and lst (not done))
+      (if (= current-index index) ; <- changed eq? to =
+          (progn
+            (rplaca! lst obj)
+            (setq! done t))
+          (setq! lst (cdr lst))
+          (setq! current-index (+ 1 current-index))))
+    (unless done (error "INDEX out of bounds")))
+  obj) ; return the set object for convenience, similar to setq!
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun list-ref (lst index)
+  "Return the element at INDEX of LST."
+  (unless (list? lst) (error "LST must be a list"))
+  (unless (and (integer? index) (>= index 0)) (error "INDEX must be a non-negative integer"))
+  (let ((current-index 0))
+    (while (and lst (not (= current-index index)))
+      (setq! lst (cdr lst))
+      (setq! current-index (+ 1 current-index)))
+    (if lst
+        (car lst)
+        (error "INDEX out of bounds"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (setq! list-length length)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun make-list (size init-val)
