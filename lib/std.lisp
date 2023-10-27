947b0743da9f613cc495e07dedd032c9b37297b7
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 13:33:12 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -81,9 +81,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; some basic funs/macros:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro ignore args
-  "Ignores ARGS and do nothing."
-  nil)
+(defmacro ignore args
+ "Ignores ARGS and do nothing."
+ nil)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro funcall (fun . args)
  "Apply FUN to ARGS. This only exists to make porting code from other Lisps easier."
@@ -1027,6 +1027,49 @@
  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; benchmark:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(report-time-us "def benchmark                   "
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun benchmark (repetitions print-interval qexpr)
+ "Benchmark QEXPR by running it REPETITIONS times and returning the"
+ "total/average time in ms, printing updates ever PRINT-INTERVAL iterations."
+
+ "THIS PROBABLY NEEDS AN UPDATE!"
+ (unless (integer? repetitions)   (error "REPETITIONS must be an integer"))
+ (unless (integer? print-interval)(error "PRINT-INTERVAL must be an integer"))
+ (nl)
+ (let ((ctr   0)
+       (total 0))
+  (repeat repetitions
+   (setq! ctr (1+ ctr))
+   (let ((before (time)))
+    (eval qexpr)
+    (setq! total (+ total (elapsed before))))
+   (when (zero? (% ctr print-interval))
+    (nl)
+    (princ "Iteration #")
+    (princ ctr)
+    (princ ", ")
+    (princ (÷ total 1000))
+    (princ " ms so far.")))
+  (nl)
+  (princ "total ums: ")
+  (princ total) (nl)
+  (princ "total ms: ")
+  (princ (÷ total 1000)) (nl)
+  (princ "total s: ")
+  (princ (÷ total 1000000)) (nl)
+  (let ((each-ms (÷ total repetitions 1000)))
+   (princ "each ms: ")
+   (princ (÷ total repetitions 1000))
+   (nl)
+   each-ms)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'benchmark))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (report-time-us "def unsorted                   "
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; random unsorted stuff:                                                               ;;
@@ -1072,41 +1115,6 @@
   (<< n 1))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (setq! 2*     double)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun benchmark (repetitions print-interval qexpr)
-  "Benchmark QEXPR by running it REPETITIONS times and returning the"
-  "total/average time in ms, printing updates ever PRINT-INTERVAL iterations."
-
-  "THIS PROBABLY NEEDS AN UPDATE!"
-  (unless (integer? repetitions)   (error "REPETITIONS must be an integer"))
-  (unless (integer? print-interval)(error "PRINT-INTERVAL must be an integer"))
-  (nl)
-  (let ((ctr   0)
-        (total 0))
-   (repeat repetitions
-    (setq! ctr (1+ ctr))
-    (let ((before (time)))
-     (eval qexpr)
-     (setq! total (+ total (elapsed before))))
-    (when (zero? (% ctr print-interval))
-     (nl)
-     (princ "Iteration #")
-     (princ ctr)
-     (princ ", ")
-     (princ (÷ total 1000))
-     (princ " ms so far.")))
-   (nl)
-   (princ "total ums: ")
-   (princ total) (nl)
-   (princ "total ms: ")
-   (princ (÷ total 1000)) (nl)
-   (princ "total s: ")
-   (princ (÷ total 1000000)) (nl)
-   (let ((each-ms (÷ total repetitions 1000)))
-    (princ "each ms: ")
-    (princ (÷ total repetitions 1000))
-    (nl)
-    each-ms)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defun add-logging-to (fun)
   "Add logging to a function FUN."
