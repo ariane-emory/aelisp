b5d86900cceb55dcd5049e97e26aee92073f57a2
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 22:30:55 -0400

    ...

--- lib/quasiquote.lisp
+++ lib/quasiquote.lisp
@@ -5,15 +5,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; quasiquotation:                                                                      ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (setq! append2
-  (lambda (lst1 lst2)
-   "Append two LST1 and LST2."
-   (unless (list? lst1) (error "LST1 must be a list"))
-   (unless (list? lst2) (error "LST2 must be a list"))
-   (if (nil? lst1)
-    lst2
-    (cons (car lst1) (append2 (cdr lst1) lst2)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defmacro expand-quasiquoted (expr)
   "Expand a quasiquoted expression and resolve unquotes and"
   "unquote-splicings within."
