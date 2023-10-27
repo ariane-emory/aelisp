f3010ae43ff0fd1a25911cb2456d2e96b57e1151
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 22:26:18 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -105,35 +105,8 @@
 (require 'map)
 (require 'nconc)
 (require 'push-funs)
+(require 'flatten)
 
-
-(report-time-us "def flatten funs               "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- ;; list funs (flattening):                                                              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun flatten1 (lst)
-  (unless (list? lst) (error "LST must be a list"))
-  (cond
-   ((nil? lst) nil)
-   ((list? (car lst))
-    (append (car lst) (flatten1 (cdr lst))))
-   (t (cons (car lst) (flatten1 (cdr lst))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun flatten-left (lst)
-  "Flatten a left-nested list structure LST."
-  (unless (list? lst) (error "LST must be a list"))
-  (if (cons? (car lst))i)
-  (append (flatten-left (car lst)) $(cadr lst))
-  lst)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun flatten (lst)
-  (unless (list? lst) (error "LST must be a list"))
-  (when lst
-   (if (cons? (car lst))
-    (append (flatten (car lst)) (flatten (cdr lst)))
-    (cons (car lst) (flatten (cdr lst))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
 
 
 (report-time-us "def zipping funs               "
