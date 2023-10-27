d3a1590adaa78466bb7736d7768ba4b0928e6a3a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 22:38:37 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -2,46 +2,8 @@
 ;; 'standard library', such as it is:                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
 (require 'std-minimal)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; time evaluation:                                                                      ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro time exprs
- "Return how long it takes to evaluate EXPRS in milliseconds."
- $('let $($('begin $('now)))
-   (cons 'progn exprs)
-   $('elapsed 'begin)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro report-time (msg . exprs)
- (unless (eq? :STRING (type msg)) (error "MSG must be a string"))
- $('progn
-   $('princ msg)
-   $('let $($('time-taken (cons 'time exprs)))
-     $('princ '" in ")
-     $('princ 'time-taken)
-     $('princ '" us.")
-     $('nl))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro time-us exprs
- "Return how long it takes to evaluate EXPRS in microseconds."
- $('let $($('begin $('now-us)))
-   (cons 'progn exprs)
-   $('elapsed-us 'begin)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro report-time-us (msg . exprs)
- (unless (eq? :STRING (type msg)) (error "MSG must be a string"))
- $('progn
-   $('princ msg)
-   $('let $($('time-taken (cons 'time-us exprs)))
-     $('princ '" in ")
-     $('princ 'time-taken)
-     $('princ '" us.")
-     $('nl))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(require 'measure-time)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; simpler-version of std load time measuerement:                                        ;;
