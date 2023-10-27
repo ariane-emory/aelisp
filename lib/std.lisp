0380ca576068510f59d095c6533bc8974234efbb
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 23:29:58 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -40,34 +40,7 @@
 (require 'split-list)
 (require 'print-macros)
 (require 'std-misc)
-
-
-(report-time-us "def tail chasers               "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro make-member-pred (pred?)
-  `(make-chase-fun (obj lst)
-    ((,pred? obj head) t)
-    (position (chase obj))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro make-remove-fun (pred?)
-  `(make-chase-fun (obj lst)
-    ((,pred? obj head) tail)
-    (position (cons head (chase obj)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro make-index-fun (pred?)
-  `(make-chase-fun (obj lst)
-    ((,pred? obj head) (car rest))
-    (position (chase obj (if rest (1+ (car rest)) 1)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (setq! indexq   (make-index-fun   eq?))
- (setq! memq?    (make-member-pred eq?))
- (setq! removeq  (make-remove-fun  eq?))
- (setq! indexql  (make-index-fun   eql?))
- (setq! memql?   (make-member-pred eql?))
- (setq! removeql (make-remove-fun  eql?))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
+(require 'tail-chaser-funs)
 
 
 (mapc* require
