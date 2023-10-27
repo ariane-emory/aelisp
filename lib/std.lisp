5911113596a8c700fdf8ee94b7072ef8ee35441f
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 23:28:11 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -2,6 +2,7 @@
 ;; 'standard library', such as it is:                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; simple std load time measuerement:                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -13,6 +14,7 @@
   (princ " us.")
   (nl)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (require 'std-minimal)
 (require 'measure-time)
@@ -38,6 +40,7 @@
 (require 'split-list)
 (require 'print-macros)
 (require 'std-misc)
+
 
 (report-time-us "def tail chasers               "
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -67,46 +70,8 @@
 
 
 
-
-(report-time-us "def selection sort parts       "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun remove-first! (pred? lst)
-  "Destructively remove the first item matching PRED? from LST."
-  (unless (fun? pred?) (error "PRED? must be a function"))
-  (unless (list? lst)  (error "LST must be a list"))
-  (if (pred? (car lst))
-   (if (cdr lst)
-    (progn 
-     (rplaca! lst (cadr lst))
-     (rplacd! lst (cddr lst)))
-    (error "can't remove last item"))
-   (let ((prev lst) (current (cdr lst)))
-    (while (and current (not (pred? (car current))))
-     (setq! prev current)
-     (setq! current (cdr current)))
-    (if current
-     (progn
-      (rplacd! prev (cdr current))
-      (car current))
-     (error "obj was not in lst")))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun select-and-move-to-front! (pred? lst)
-  "Move the first item in LST matching PRED? to its head."
-  (unless (fun? pred?) (error "PRED? must be a function"))
-  (unless (list? lst)  (error "LST must be a list"))
-  (let ((head (car lst)))
-   (if (pred? head)
-    head
-    (let ((obj      (remove-first! pred? lst))
-          (new-tail (cons (car lst) (cdr lst))))
-     (rplaca! lst obj)
-     (rplacd! lst new-tail)
-     obj))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
-
 (mapc* require
+ 'selection-sort
  'delq
  'doc-strings
  'primes
