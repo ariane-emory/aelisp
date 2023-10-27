31ac4f62818b11400ef645d8a2c85ab74f20287d
Ariane Emory <ariane.emory@protonmail.com>
2023-10-26 23:25:07 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -39,34 +39,6 @@
 (require 'print-macros)
 (require 'std-misc)
 
-
-(report-time-us "def list predicates            "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- ;; list funs (more unsorted):                                                           ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro defun-list-pred-fun (name combiner base-case)
-  `(defun ,name (pred? lst)
-    (if lst
-     (,combiner
-      (pred? (car lst))
-      (,name pred? (cdr lst)))
-     ,base-case)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun-list-pred-fun any? or  nil)
- (defun-list-pred-fun all? and t)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defmacro defun-list-transform-fun (name transformer)
-  `(defun ,name (lsts)
-    (when lsts
-     (cons (,transformer (car lsts)) (,name (cdr lsts))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (defun-list-transform-fun heads caar)
- (defun-list-transform-fun tails cdar)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- )
-
-
-
 (report-time-us "def tail chasers               "
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  (defmacro make-member-pred (pred?)
