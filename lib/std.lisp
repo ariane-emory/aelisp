838affebe620a686ffcb92ad01684f14ec8ec76f
Ariane Emory <ariane.emory@protonmail.com>
2023-10-28 00:59:58 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -153,61 +153,62 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(let ((module
-       (list
-        'basic-funs
-        'core-aliases
-        'compound-cars-and-cdrs
-        'append-and-nconc
-        'quasiquote
-        'list-access-funs
-        'tail-chaser-macros
-        'reduce
-        'map
-        'zip
-        'push-pop-funs
-        'flatten
-        'transform
-        'merge-sort
-        'list-member
-        'list-index
-        'list-remove
-        'union
-        'misc-list-funs
-        'misc-predicates
-        'log-toggle
-        'print-macros
-        'benchmark
-        'unsorted-funs
-        'vector-list
-        'split-list
-        'delq
-        'prime-funs
-        'selection-sort
-        'doc
-        'test-macros
-        'string-funs
-        'prog-macros
-        'plist-funs
-        'remove-prop-macro
-        'scheme-compat-aliases
-        'elisp-compat-aliases
-        'std-aliases
-        )))
-      (while module
-       (require (car module))
-       (setq! module (cdr module))))
+(when *std-modules*
+ (let ((module
+        (list
+         'basic-funs
+         'core-aliases
+         'compound-cars-and-cdrs
+         'append-and-nconc
+         'quasiquote
+         'list-access-funs
+         'tail-chaser-macros
+         'reduce
+         'map
+         'zip
+         'push-pop-funs
+         'flatten
+         'transform
+         'merge-sort
+         'list-member
+         'list-index
+         'list-remove
+         'union
+         'misc-list-funs
+         'misc-predicates
+         'log-toggle
+         'print-macros
+         'benchmark
+         'unsorted-funs
+         'vector-list
+         'split-list
+         'delq
+         'prime-funs
+         'selection-sort
+         'doc
+         'test-macros
+         'string-funs
+         'prog-macros
+         'plist-funs
+         'remove-prop-macro
+         'scheme-compat-aliases
+         'elisp-compat-aliases
+         'std-aliases
+         )))
+  (while module
+   (require (car module))
+   (setq! module (cdr module)))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (provide 'std)
+(provide 'std)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- (nl)
- (princ "Loaded in   ")
- (princ (elapsed-us *before-std*))
- (princ " us.")
- (nl)
+(nl)
+(princ "Loaded in   ")
+(princ (elapsed-us *before-std*))
+(princ " us.")
+(nl)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
