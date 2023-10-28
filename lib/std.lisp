63270f6293846a93b26370d28317a3aeedd7db20
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 21:59:56 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -898,30 +898,10 @@
 
 (require 'log-toggle-funs)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; fancy output macros:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro princn  exprs           $('progn (cons 'princ exprs) $('nl)))
-(defmacro printn  exprs           $('progn (cons 'print exprs) $('nl)))
-(defmacro putn    exprs           $('progn (cons 'put  exprs)  $('nl)))
-(defmacro writen  exprs           $('progn (cons 'write exprs) $('nl)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro princni (delim . exprs) $('apply 'princn $('intercalate delim $('quote exprs))))
-(defmacro printi  (delim . exprs) $('apply 'print  $('intercalate delim $('quote exprs))))
-(defmacro putni   (delim . exprs) $('apply 'putn   $('intercalate delim $('quote exprs))))
-(defmacro writeni (delim . exprs) $('apply 'writen $('intercalate delim $('quote exprs))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro princns exprs           $('princni " " . exprs))
-(defmacro prints  exprs           $('princti " " . exprs))
-(defmacro putns   exprs           $('putni   " " . exprs))
-(defmacro writens exprs           $('writeni " " . exprs))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'print-macros)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (setq! *std-modules*
- $(
+ $('print-macros
    'benchmark
    'unsorted-funs
    'vector-list-funs
