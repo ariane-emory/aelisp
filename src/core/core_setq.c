697d7ce08545e130805c75617d3d929e2032edfd
Ariane Emory <ariane.emory@protonmail.com>
2023-10-28 16:31:25 -0400

    ...

--- src/core/core_setq.c
+++ src/core/core_setq.c
@@ -17,10 +17,10 @@ ae_obj_t * ae_core_setq(ae_obj_t * const env, ae_obj_t * const args, __attribute
   /* if (! SETTABLEP(sym)) */
   /*   LOG(sym, "not SETTABLEP:"); */
 
-  REQUIRE(env, args, ! NILP(sym), "nil may not be set");
-  REQUIRE(env, args, ! TRUEP(sym), "t may not be set");
-  REQUIRE(env, args, SYMBOLP(sym), "sym is not a symbol");
-  REQUIRE(env, args, ! KEYWORDP(sym), "keyword symbols may not be set");
+  REQUIRE(env, args, ! NILP(sym),                 "nil may not be set");
+  REQUIRE(env, args, ! TRUEP(sym),                "t may not be set");
+  REQUIRE(env, args, SYMBOLP(sym),                "sym is not a symbol");
+  REQUIRE(env, args, ! KEYWORDP(sym),             "keyword symbols may not be set");
   REQUIRE(env, args, ! HAS_PROP("constant", sym), "constant symbols may not be set");
 
   // REQUIRE(env, args, SETTABLEP(sym), "sym is not a settable symbol");
