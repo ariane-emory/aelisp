5011ce87f24742aa39561989495cee3a0d2b1d53
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 22:13:48 -0400

    ...

--- lib/std.lisp
+++ lib/std.lisp
@@ -642,71 +642,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; list funs (flattening):
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun flatten1 (lst)
- (unless (list? lst) (error "LST must be a list"))
- (cond
-  ((nil? lst) nil)
-  ((list? (car lst))
-   (append (car lst) (flatten1 (cdr lst))))
-  (t (cons (car lst) (flatten1 (cdr lst))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun flatten-left (lst)
- "Flatten a left-nested list structure LST."
- (unless (list? lst) (error "LST must be a list"))
- (if (cons? (car lst))i)
- (append (flatten-left (car lst)) $(cadr lst))
- lst)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun flatten (lst)
- (unless (list? lst) (error "LST must be a list"))
- (when lst
-  (if (cons? (car lst))
-   (append (flatten (car lst)) (flatten (cdr lst)))
-   (cons (car lst) (flatten (cdr lst))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'flatten)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; list funs (zipping):
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun zip2 (lst1 lst2)
- "Zip LST1 and LST2."
- (unless (list? lst1) (error "LST1 must be a list"))
- (unless (list? lst2) (error "LST2 must be a list"))
- (cond
-  ((∨ (nil? lst1) (nil? lst2)) nil)
-  (t  (cons  $((car lst1) (car lst2))
-       (zip2   (cdr lst1) (cdr lst2))))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun zip3 (l1 l2 l3)
- "Zip the three lists LST1, LST2 and LST3."
- (unless (list? l1) (error "LST1 must be a list"))
- (unless (list? l2) (error "LST2 must be a list"))
- (unless (list? l3) (error "LST3 must be a list"))
- (mapcar flatten1 (reduce zip2 $(l2 l3) l1)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! left-nested-zip (reduced* zip2))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro zip lists
- "Zip many lists. This might not flatten properly if the zipped elements are
-  themselves lists."
- (unless (list? lists) (error "LISTS must be a list"))
- (if (cdr lists)
-  $('mapcar 'flatten (cons 'left-nested-zip lists))
-  $('mapcar 'list  (car lists))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'zip)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
 (setq! *std-modules*
  $(
+   'flatten-funs
    'transform
    'merge-sort
    'union
