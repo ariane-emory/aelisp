7bf4275e1253ffd523406787c9aab4924a39782b
Ariane Emory <ariane.emory@protonmail.com>
2023-10-28 01:53:24 -0400

    ...

--- lib/std-aliases.lisp
+++ lib/std-aliases.lisp
@@ -4,8 +4,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; simple aliases:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq! pkeys     plist-keys)
-(setq! pvals     plist-values)
 (setq! premove   plist-remove)
 (setq! premoveq  plist-removeq)
 (setq! premoveql plist-removeql)
