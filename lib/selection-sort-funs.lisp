aea95b8574ae1d07ce9d6667be24e656ba4ed01a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-27 20:54:23 -0400

    ...

---
+++ lib/selection-sort-funs.lisp
@@ -0,0 +1,38 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; selection sort parts;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun remove-first! (pred? lst)
+ "Destructively remove the first item matching PRED? from LST."
+ (unless (fun? pred?) (error "PRED? must be a function"))
+ (unless (list? lst)  (error "LST must be a list"))
+ (if (pred? (car lst))
+  (if (cdr lst)
+   (progn 
+    (rplaca! lst (cadr lst))
+    (rplacd! lst (cddr lst)))
+   (error "can't remove last item"))
+  (let ((prev lst) (current (cdr lst)))
+   (while (and current (not (pred? (car current))))
+    (setq! prev current)
+    (setq! current (cdr current)))
+   (if current
+    (progn
+     (rplacd! prev (cdr current))
+     (car current))
+    (error "obj was not in lst")))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun select-and-move-to-front! (pred? lst)
+ "Move the first item in LST matching PRED? to its head."
+ (unless (fun? pred?) (error "PRED? must be a function"))
+ (unless (list? lst)  (error "LST must be a list"))
+ (let ((head (car lst)))
+  (if (pred? head)
+   head
+   (let ((obj      (remove-first! pred? lst))
+         (new-tail (cons (car lst) (cdr lst))))
+    (rplaca! lst obj)
+    (rplacd! lst new-tail)
+    obj))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'selection-sort-funs)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
