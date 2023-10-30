18aca43732339bdfa204513192ba5ee66ce0cf52
Ariane Emory <ariane.emory@protonmail.com>
2023-10-30 15:10:08 -0400

    ...

--- data/tests.lisp
+++ data/tests.lisp
@@ -1,12 +1,12 @@
 (setq! *random-seed* 1)
 
-(defun random-int-range rest
- (let ((val  (setq! *random-seed* 1)))
-  *random-seed*))
+(defun bugged rest
+ (let ((val  (setq! random-seed 1)))
+  random-seed))
 
-(princ (random-int-range)) (nl)
-(princ (random-int-range 10)) (nl)
-(princ (random-int-range -10 10)) (nl)
+(princ (bugged)) (nl)
+(princ (bugged 10)) (nl)
+(princ (bugged -10 10)) (nl)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'tests)
