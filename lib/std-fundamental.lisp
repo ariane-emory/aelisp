0fe5c3ce507714b71b5930a08912cd601384ca2c
Ariane Emory <ariane.emory@protonmail.com>
2023-10-29 21:06:49 -0400

    ...

--- lib/std-fundamental.lisp
+++ lib/std-fundamental.lisp
@@ -2,6 +2,7 @@
 ;; std config:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq! *use-safe-provide*        nil)
+(setq! *use-soft-rationals*      nil)
 (setq! *microbench-defmacros*    t)
 (setq! *microbench-defuns*       t)
 (setq! *microbench-provides*     t)
