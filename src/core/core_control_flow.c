fca7c852bf7d151888f8a4dd8d2aee2df954251f
Ariane Emory <ariane.emory@protonmail.com>
2023-10-30 13:54:23 -0400

    remove crappy return construct, let's build a better one

--- src/core/core_control_flow.c
+++ src/core/core_control_flow.c
@@ -18,15 +18,7 @@ ae_obj_t * ae_core_progn(ae_obj_t * const env, ae_obj_t * const args, __attribut
     if (log_core)
       LOG(elem, "eval progn arg  #%d/%d", ctr, args_length);
 
-    if (CAR(elem) == SYM("return")) {
-      REQUIRE(env, args, LENGTH(elem) == 2, "return requires exactly one argument");
-      
-      RETURN(EVAL(env, CADR(elem)));
-    }
-    
-    ret = EVAL(env, elem);
-    
-    RETURN_IF_ERRORP(ret);
+    ret = RETURN_IF_ERRORP(EVAL(env, elem));
         
     if (log_core)
       LOG(ret, "progn arg #%d/%d evaluated to", ctr, args_length);
