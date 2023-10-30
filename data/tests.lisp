4308551ac4fbea4138beeba7a7873dff56d79dab
Ariane Emory <ariane.emory@protonmail.com>
2023-10-30 14:54:42 -0400

    hacky fix: disable GLOBAL lookup until fixed. FIX sooneleme

--- data/tests.lisp
+++ data/tests.lisp
@@ -1,38 +1,3 @@
-(setq! *random-m* 4611686018427387904) ; 2^62
-(setq! *random-a* 1664525)
-(setq! *random-c* 1013904223)
-(setq! *random-seed* (now-us))
-
-(defun randomize (new-seed)
- "Set a new seed for the RNG."
- (setq! *random-seed* new-seed))
-
-(defun abs (n)
- "Return the absolute value of N."
- (if (< n 0)
-  (- n)
-  n))
-
-(defun random-inner ()
- "Return a random integer."
- (setq! *random-seed* (% (+ (mul *random-a* *random-seed*) *random-c*) *random-m*)))
-
-(defun random-int-range rest
- "Return a random integer between MIN (inclusive) and MAX (exclusive)."
- (unless (or (nil? rest) (<= (length rest) 2))         (error "random-int-range takes either 0, 1 or 2 arguments"))
- (unless (or (nil? (car rest)) (integer? (car rest)))  (error "if provided, first rest argument must be an integer"))
- (unless (or (nil? (cdr rest)) (integer? (cadr rest))) (error "if provided, second rest argument must be an integer"))
- (if (nil? rest)
-  (random-inner)
-  (let ((min (if (cadr rest) (car  rest) 0))
-        (max (if (cadr rest) (cadr rest) (car rest))))
-   (let ((range (- max min)))
-    (+ min (mod (abs (random-inner)) range))))))
-
-(repeat 10 (princ (random-int-range))         (nl))
-(repeat 10 (princ (random-int-range 10))      (nl))
-(repeat 10 (princ (random-int-range -10 10))  (nl))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (nl)
@@ -361,4 +326,39 @@ Write some tests for bitwise operators
 ;;(add-to-list *load-path* (concat *ae-home* "/3p/tinyclos")) (load "support.scm")
 
 
+
+(setq! *random-m* 4611686018427387904) ; 2^62
+(setq! *random-a* 1664525)
+(setq! *random-c* 1013904223)
+(setq! *random-seed* (now-us))
+
+(defun randomize (new-seed)
+ "Set a new seed for the RNG."
+ (setq! *random-seed* new-seed))
+
+(defun abs (n)
+ "Return the absolute value of N."
+ (if (< n 0)
+  (- n)
+  n))
+
+(defun random-inner ()
+ "Return a random integer."
+ (setq! *random-seed* (% (+ (mul *random-a* *random-seed*) *random-c*) *random-m*)))
+
+(defun random-int-range rest
+ "Return a random integer between MIN (inclusive) and MAX (exclusive)."
+ (unless (or (nil? rest) (<= (length rest) 2))         (error "random-int-range takes either 0, 1 or 2 arguments"))
+ (unless (or (nil? (car rest)) (integer? (car rest)))  (error "if provided, first rest argument must be an integer"))
+ (unless (or (nil? (cdr rest)) (integer? (cadr rest))) (error "if provided, second rest argument must be an integer"))
+ (if (nil? rest)
+  (random-inner)
+  (let ((min (if (cadr rest) (car  rest) 0))
+        (max (if (cadr rest) (cadr rest) (car rest))))
+   (let ((range (- max min)))
+    (+ min (mod (abs (random-inner)) range))))))
+
+(repeat 10 (princ (random-int-range))         (nl))
+(repeat 10 (princ (random-int-range 10))      (nl))
+(repeat 10 (princ (random-int-range -10 10))  (nl))
 
