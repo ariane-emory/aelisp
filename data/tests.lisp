2db9cfc425d7833a012d54dd299791262fae962a
Ariane Emory <ariane.emory@protonmail.com>
2023-10-30 22:17:22 -0400

    ...

--- data/tests.lisp
+++ data/tests.lisp
@@ -439,19 +439,19 @@ Write some tests for random.
 
 
 ;; Well, we were working towards making this work, to test whether randomness is maintained properly between successive die rolls:
-(repeat 20000
- (let ((roll1 (random 1 7))
-       (roll2 (random 1 7)))
-  (unless (and (>= roll1 1) (<= roll1 6) (>= roll2 1) (<= roll2 6))
-   (princ "Unexpected rolls: roll1=" roll1 " roll2=" roll2) (nl))
+;; (repeat 20000
+;;  (let ((roll1 (random 1 7))
+;;        (roll2 (random 1 7)))
+;;   (unless (and (>= roll1 1) (<= roll1 6) (>= roll2 1) (<= roll2 6))
+;;    (princ "Unexpected rolls: roll1=" roll1 " roll2=" roll2) (nl))
 
-  ;;(princ roll1 " " roll2) (nl)
-  ;; Let's break it down step-by-step
-  (let* ((sublist (nth (- roll1 1) *matrix*))
-         (current-val (nth (- roll2 1) sublist))
-         (new-val (1+ current-val)))
-   (list-set! sublist (- roll2 1) new-val)
-   )))
+;;   ;;(princ roll1 " " roll2) (nl)
+;;   ;; Let's break it down step-by-step
+;;   (let* ((sublist (nth (- roll1 1) *matrix*))
+;;          (current-val (nth (- roll2 1) sublist))
+;;          (new-val (1+ current-val)))
+;;    (list-set! sublist (- roll2 1) new-val)
+;;    )))
 
 (princ *matrix*) (nl)
 
@@ -463,6 +463,29 @@ Write some tests for random.
 ;;(repeat 100 (princ (random)) (nl))
 
 
+;;(defun massaged-random (min max) (let ((substitute-max (- (<< max 1) 1))) (>> (+ (random min substitute-max) 1) 1))) (repeat 100 (princ (massaged-random 1 7)) (nl))
 
 
-(defun massaged-random (min max) (let ((substitute-max (- (<< max 1) 1))) (>> (+ (random min substitute-max) 1) 1))) (repeat 100 (princ (massaged-random 1 7)) (nl))
+
+; Initialize the seed (any non-zero value will do)
+
+(setq! *xorshift-seed* (now-us))
+
+(defun xorshift64 ()
+  (unless *xorshift-seed* 
+    (error "Seed cannot be zero for xorshift!"))
+  
+  ; Xorshift algorithm
+  (setq! *xorshift-seed* (^ *xorshift-seed* (<< *xorshift-seed* 13)))
+  (setq! *xorshift-seed* (^ *xorshift-seed* (>> *xorshift-seed* 7)))
+  (setq! *xorshift-seed* (^ *xorshift-seed* (<< *xorshift-seed* 17)))
+  
+  (if (< *xorshift-seed* 0)
+      (- *xorshift-seed*)
+      *xorshift-seed*))
+
+(defun random-range (min max)
+  (let ((range (+ 1 (- max min))))
+    (+ min (mod (xorshift64) range))))
+
+(repeat 100 (princ (random-range 1 6)) (nl))
