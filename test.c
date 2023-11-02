a848fd96598bff244868ee42bae5ad5eb82daecf
Ariane Emory <ariane.emory@protonmail.com>
2023-11-02 17:06:44 -0400

    ...

--- test.c
+++ test.c
@@ -1310,6 +1310,12 @@ void plist(void) {
   ae_obj_t * plist = CONS(SYM("a"), CONS(NEW_INT(1), CONS(SYM("b"), CONS(NEW_INT(2), CONS(SYM("c"), CONS(NEW_INT(3), NIL))))));
   ae_plist_set_mut(plist, SYM("d"), NEW_INT(4));
   T(shitty_princ_based_equality_predicate(plist, "(d 4 a 1 b 2 c 3)"));
+  ae_plist_set_mut(plist, SYM("d"), NEW_INT(8));
+  T(shitty_princ_based_equality_predicate(plist, "(d 8 a 1 b 2 c 3)"));
+  ae_plist_set_mut(plist, SYM("a"), NEW_INT(10));
+  T(shitty_princ_based_equality_predicate(plist, "(d 8 a 10 b 2 c 3)"));
+  ae_plist_set_mut(plist, SYM("c"), NEW_INT(20));
+  T(shitty_princ_based_equality_predicate(plist, "(d 8 a 10 b 2 c 20)"));
 }                
                  
 void tailp(void) {
