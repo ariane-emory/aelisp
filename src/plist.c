3224afccbd6bc69bddc0b8374c81f0f50a7410e2
Ariane Emory <ariane.emory@protonmail.com>
2023-11-02 17:55:53 -0400

    ...

--- src/plist.c
+++ src/plist.c
@@ -10,24 +10,28 @@
 // clone_list_up_to helper
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ae_obj_t *clone_list_up_to(ae_obj_t * const pos, ae_obj_t * const list) {
-    ae_obj_t *new_list = NIL, *tail = NIL, *cur;
+ae_obj_t * clone_list_up_to(ae_obj_t * const pos, ae_obj_t * const list) {
+  assert(pos);
+  assert(list);
+  assert(TAILP(list));
   
-    for (cur = list; cur != pos; cur = CDR(cur)) {
-        if (cur == NIL) {
-            // If cur is NIL, then pos was not part of the list, which is an error.
-            // Handle error accordingly, maybe return NIL or log an error message.
-            return NIL;
-        }
-        ae_obj_t *new_elem = CONS(CAR(cur), NIL);
-        if (new_list == NIL) {
-            new_list = new_elem;
-        } else {
-            CDR(tail) = new_elem;
-        }
-        tail = new_elem;
-    }
-    return new_list;
+  ae_obj_t * new_list = NIL;
+  ae_obj_t * tail     = NIL;
+  
+  for (ae_obj_t * cur = list; cur != pos; cur = CDR(cur)) {
+    assert(! NILP(cur)); // If cur is NIL, then pos was not part of the list, which is an error.
+      
+    ae_obj_t * const new_elem = CONS(CAR(cur), NIL);
+
+    if (new_list == NIL)
+      new_list = new_elem;
+    else
+      CDR(tail) = new_elem;
+    
+    tail = new_elem;
+  }
+  
+  return new_list;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,20 +45,20 @@ ae_obj_t * ae_plist_set_immutable(ae_obj_t * const plist, ae_obj_t * const key, 
   assert(key);
   assert(value);
 
-  if (plist == NIL) {
-    // If the list is empty, return a new key-value pair.
+  if (NILP(plist))
     return CONS(key, CONS(value, NIL));
-  }
   
-  ae_obj_t *pos, *prev = NIL, *new_plist = NIL;
+  ae_obj_t * prev      = NIL;
+  ae_obj_t * new_plist = NIL;
   
   // Search for the key in the plist.
-  for (pos = plist; pos != NIL; pos = CDR(CDR(pos))) {
+  for (ae_obj_t * pos = plist; pos != NIL; pos = CDR(CDR(pos))) {
     if (EQL(CAR(pos), key)) {
       // If the key is found, clone the list up to this point and update the value.
       new_plist = clone_list_up_to(pos, plist);
       // Create a new pair with the updated value.
-      ae_obj_t *updated_pair = CONS(key, CONS(value, CDDR(pos)));
+      ae_obj_t * const updated_pair = CONS(key, CONS(value, CDDR(pos)));
+
       // If prev is NIL, the key was in the first pair.
       if (prev == NIL) {
         return updated_pair;
@@ -64,6 +68,7 @@ ae_obj_t * ae_plist_set_immutable(ae_obj_t * const plist, ae_obj_t * const key, 
         return new_plist;
       }
     }
+    
     prev = CDR(pos); // Keep track of the previous pair to be able to link the list.
   }
 
@@ -81,20 +86,16 @@ void ae_plist_set_mut(ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * c
   assert(key);
   assert(value);
   
-  // Search for the key in the plist.
-  for (ae_obj_t *pos = plist; pos != NIL; pos = CDR(CDR(pos))) {
+  for (ae_obj_t * pos = plist; pos != NIL; pos = CDR(CDR(pos)))
     if (EQL(CAR(pos), key)) {
-      // If key is found, just update the value.
       CADR(pos) = value;
-      return; // Mutation done; early exit.
-    }
-  }
 
-  // Key wasn't found, so prepend it by mutating in place.
-  ae_obj_t *new_tail = CONS(CAR(plist), CONS(CADR(plist), CDR(CDR(plist))));
+      return; 
+    }
+
+  ae_obj_t * const new_tail = CONS(CAR(plist), CONS(CADR(plist), CDR(CDR(plist))));
   CAR(plist) = key;
-  // After setting the new key, we also need to set the new value here.
-  CDR(plist) = CONS(value, new_tail); // This line is changed.
+  CDR(plist) = CONS(value, new_tail);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
