111545fca45aefff674b4071045a827d4932b6ba
Ariane Emory <ariane.emory@protonmail.com>
2023-11-03 17:29:09 -0400

    ...

--- include/plist.h
+++ include/plist.h
@@ -33,16 +33,16 @@ typedef struct ae_plist_split_around_kvp_t {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ae_obj_t * ae_plist_get                              (ae_obj_t * const plist, ae_obj_t * const key);
-bool       ae_plist_contains_key                     (ae_obj_t * const plist, ae_obj_t * const key);
+ae_plist_split_around_kvp_t ae_plist_split_around_kvp(ae_obj_t * const plist, ae_obj_t * const key);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ae_obj_t * ae_plist_set                              (ae_obj_t *       plist, ae_obj_t * const key, ae_obj_t * const value);
-void       ae_plist_set_mutating                     (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
-ae_obj_t * ae_plist_set_nonmutating                  (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
-ae_obj_t * ae_plist_set_internal                     (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
+bool       ae_plist_contains_key                     (ae_obj_t * const plist, ae_obj_t * const key);
+ae_obj_t * ae_plist_get                              (ae_obj_t * const plist, ae_obj_t * const key);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void       ae_plist_remove_mutating                  (ae_obj_t * const plist, ae_obj_t * const key);
 ae_obj_t * ae_plist_remove_nonmutating               (ae_obj_t * const plist, ae_obj_t * const key);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ae_plist_split_around_kvp_t ae_plist_split_around_kvp(ae_obj_t * const plist, ae_obj_t * const key);
+ae_obj_t * ae_plist_set                              (ae_obj_t *       plist, ae_obj_t * const key, ae_obj_t * const value);
+ae_obj_t * ae_plist_set_internal                     (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
+void       ae_plist_set_mutating                     (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
+ae_obj_t * ae_plist_set_nonmutating                  (ae_obj_t * const plist, ae_obj_t * const key, ae_obj_t * const value);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
