2ee8d760a1725a47492206b370833e95692d999d
Ariane Emory <ariane.emory@protonmail.com>
2023-11-03 17:32:27 -0400

    ...

--- include/plist.h
+++ include/plist.h
@@ -21,9 +21,9 @@ typedef struct ae_plist_split_around_kvp_t {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // convenience macros
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define PSET(plist, key, value)             (ae_plist_set ((plist), (key), (value)))
-#define PGET(plist, key)                    (ae_plist_get ((plist), (key)))
 #define PHAS(plist, key)                    (ae_plist_contains_key((plist), (key)))
+#define PGET(plist, key)                    (ae_plist_get((plist), (key)))
+#define PSET(plist, key, value)             (ae_plist_set((plist), (key), (value)))
 #define PSET_INTERNAL(plist, key, value)    (ae_plist_set_internal((plist), (key), (value)))
 #define PSET_MUTATING(plist, key, value)    (ae_plist_set_mutating((plist), (key), (value)))
 #define PSET_NONMUTATING(plist, key, value) (ae_plist_set_nonmutating((plist), (key), (value)))
