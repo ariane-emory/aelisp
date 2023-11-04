5c0618af61d6c906572b9a817e7d90e38a8c31a9
Ariane Emory <ariane.emory@protonmail.com>
2023-11-03 23:53:12 -0400

    ...

--- lib/mono-std.lisp
+++ lib/mono-std.lisp
@@ -232,17 +232,27 @@
 (defun eleventh (lst)  (caddr (cddddr (cddddr lst))))
 (defun twelfth  (lst) (cadddr (cddddr (cddddr lst))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defun nth (index lst)
+;;  "Get the nth item in LST."
+;;  (unless (integer? index) (error "INDEX must be an integer"))
+;;  (unless (list? lst)      (error "LST must be a list"))
+;;  (unless (>= index 0)     (error "INDEX must be non-negative"))
+;;  (let ((len (length lst)))
+;;   (if (>= index len)
+;;    (error "INDEX is out of range")
+;;    (cond
+;;     ((zero? index) (car lst))
+;;     (lst           (nth (- index 1) (cdr lst)))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun nth (index lst)
  "Get the nth item in LST."
  (unless (integer? index) (error "INDEX must be an integer"))
  (unless (list? lst)      (error "LST must be a list"))
  (unless (>= index 0)     (error "INDEX must be non-negative"))
- (let ((len (length lst)))
-  (if (>= index len)
-   (error "INDEX is out of range")
-   (cond
-    ((zero? index) (car lst))
-    (lst           (nth (- index 1) (cdr lst)))))))
+ (while (> index 0)
+  (setq! lst   (cdr lst))
+  (setq! index (- index 1)))
+ (car lst))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; (defun nthcdr (index lst)
 ;;  "Get the INDEXth cdr of LST."
