d56e3e5929035f4c26fbc0da94ada64d5fbd60a8
Ariane Emory <ariane.emory@protonmail.com>
2023-11-04 17:57:45 -0400

    ...

--- src/core/core_math_ops.c
+++ src/core/core_math_ops.c
@@ -5,39 +5,39 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // This only deals with AE_INTEGERS for now. It mutates its first argument.
-#define DEF_MATH_OP(name, oper, default)                                                           \
-ae_obj_t * ae_core_ ## name(ae_obj_t * const env, ae_obj_t * const args, __attribute__((unused)) int args_length) {                         \
-  CORE_BEGIN(#name);                                                                               \
-  assert(CONSP(args));                                                                             \
-                                                                                                   \
-  long long int  accum  = 0;                                                                       \
-  ae_obj_t * rest  = NIL;                                                                          \
-                                                                                                   \
-  if (NILP(CDR(args))) {                                                                           \
-    accum = default;                                                                               \
-    rest  = args;                                                                                  \
-  }                                                                                                \
-  else {                                                                                           \
-    if (! INTEGERP(CAR(args)))                                                                     \
-      LOG(CAR(args), "NOT AN INTEGER");                                                            \
-                                                                                                   \
-    REQUIRE(env, args, INTEGERP(CAR(args)));                                                       \
-                                                                                                   \
-    accum = INT_VAL(CAR(args));                                                                    \
-    rest  = CDR(args);                                                                             \
-  }                                                                                                \
-                                                                                                   \
-  FOR_EACH(elem, rest) {                                                                           \
-    if (! INTEGERP(CAR(args)))                                                                     \
-      LOG(CAR(args), "NOT AN INTEGER");                                                            \
-                                                                                                   \
-    REQUIRE(env, args, INTEGERP(elem));                                                            \
-                                                                                                   \
-    accum = accum oper INT_VAL(elem);                                                              \
-  }                                                                                                \
-                                                                                                   \
-  CORE_RETURN(#name, NEW_INT(accum));                                                              \
-}
+#define DEF_MATH_OP(name, oper, default)                                                                              \
+  ae_obj_t * ae_core_ ## name(ae_obj_t * const env, ae_obj_t * const args, __attribute__((unused)) int args_length) { \
+    CORE_BEGIN(#name);                                                                                                \
+    assert(CONSP(args));                                                                                              \
+                                                                                                                      \
+    long long int  accum  = 0;                                                                                        \
+    ae_obj_t * rest  = NIL;                                                                                           \
+                                                                                                                      \
+    if (NILP(CDR(args))) {                                                                                            \
+      accum = default;                                                                                                \
+      rest  = args;                                                                                                   \
+    }                                                                                                                 \
+    else {                                                                                                            \
+      if (! INTEGERP(CAR(args)))                                                                                      \
+        LOG(CAR(args), "NOT AN INTEGER");                                                                             \
+                                                                                                                      \
+      REQUIRE(env, args, INTEGERP(CAR(args)));                                                                        \
+                                                                                                                      \
+      accum = INT_VAL(CAR(args));                                                                                     \
+      rest  = CDR(args);                                                                                              \
+    }                                                                                                                 \
+                                                                                                                      \
+    FOR_EACH(elem, rest) {                                                                                            \
+      if (! INTEGERP(CAR(args)))                                                                                      \
+        LOG(CAR(args), "NOT AN INTEGER");                                                                             \
+                                                                                                                      \
+      REQUIRE(env, args, INTEGERP(elem));                                                                             \
+                                                                                                                      \
+      accum = accum oper INT_VAL(elem);                                                                               \
+    }                                                                                                                 \
+                                                                                                                      \
+    CORE_RETURN(#name, NEW_INT(accum));                                                                               \
+  }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
