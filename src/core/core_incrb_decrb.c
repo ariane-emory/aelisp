c8477c6cacc80b60c9b61c8997301c4a42253379
Ariane Emory <ariane.emory@protonmail.com>
2023-11-05 22:07:40 -0500

    wip

--- src/core/core_incrb_decrb.c
+++ src/core/core_incrb_decrb.c
@@ -16,11 +16,14 @@ ae_obj_t * ae_core_incr(__attribute__((unused)) ae_obj_t * const env,
   REQUIRE(env, args, SETTABLEP(sym) && ENV_BOUNDP(env, sym),
           "incr only works on bound and settable symbols");
   
-  ae_obj_t * const integer = RETURN_IF_ERRORP(EVAL(env, sym));
+  ae_obj_t * const integer        = RETURN_IF_ERRORP(EVAL(env, sym));
+  ae_obj_t * const addend_expr    = CADR(args);
+  ae_obj_t * const addend_integer = RETURN_IF_ERRORP(EVAL(env, addend_expr));
 
   REQUIRE(env, args, INTEGERP(integer));
+  REQUIRE(env, args, NILP(addend_integer) || INTEGERP(addend_integer));
 
-  ret = NEW_INT(1 + INT_VAL(integer));
+  ret = NEW_INT(INT_VAL(integer) + (NILP(addend_integer) ? 1 : INT_VAL(addend_integer)));
 
   ENV_SET(env, sym, ret);
   
@@ -43,11 +46,14 @@ ae_obj_t * ae_core_decr(__attribute__((unused)) ae_obj_t * const env,
   REQUIRE(env, args, SETTABLEP(sym) && ENV_BOUNDP(env, sym),
           "decr only works on bound and settable symbols");
   
-  ae_obj_t * const integer = RETURN_IF_ERRORP(EVAL(env, sym));
+  ae_obj_t * const integer            = RETURN_IF_ERRORP(EVAL(env, sym));
+  ae_obj_t * const subtrahend_expr    = CADR(args);
+  ae_obj_t * const subtrahend_integer = RETURN_IF_ERRORP(EVAL(env, subtrahend_expr));
 
   REQUIRE(env, args, INTEGERP(integer));
+  REQUIRE(env, args, NILP(subtrahend_integer) || INTEGERP(subtrahend_integer));
 
-  ret = NEW_INT(INT_VAL(integer) - 1);
+  ret = NEW_INT(INT_VAL(integer) - (NILP(subtrahend_integer) ? 1 : INT_VAL(subtrahend_integer)));
 
   ENV_SET(env, sym, ret);
   
