cdb1b9c32717ea137f1d7f766a6e444831bcf9e2
Ariane Emory <ariane.emory@protonmail.com>
2023-11-05 21:42:11 -0500

    ...

--- lib/tests.lisp
+++ lib/tests.lisp
@@ -2,7 +2,7 @@
 
 (nl)
 (confirm that (feature? 'std)                  returns t)
-(setq!   lst  (unionql '(1 2 3) '(4 5 6)))
+(setq   lst  (unionql '(1 2 3) '(4 5 6)))
 (confirm that lst                              returns '(6 5 4 3 2 1))
 (confirm that (memql?   3 lst)                 returns '(3 2 1))
 (confirm that (removeql 4 lst)                 returns '(6 5 3 2 1))
@@ -21,7 +21,7 @@
 (confirm that (reverse (butlast '(1 2 3 4 5))) returns '(4 3 2 1))
 (confirm that (unionql '(1 2 3) '(4 5 6))      returns '(6 5 4 3 2 1))
 
-(setq!   lst  (make-list 6 200))
+(setq   lst  (make-list 6 200))
 (confirm that lst                              returns '(200 200 200 200 200 200))
 (confirm that (list-set! lst 0 100)            returns 100)
 (confirm that (list-ref  lst 0)                returns 100)
@@ -57,7 +57,7 @@
      (princ "Result of applying " 'lshift4 " was " result ".") (nl)
      result))))
 
-(setq! lst $(2 4 1 5 3 7 9 6 8))
+(setq lst $(2 4 1 5 3 7 9 6 8))
 (confirm that (progn (select-and-move-to-front! (lambda (o) (eql? o 9)) lst) lst) returns '(9 2 4 1 5 3 7 6 8))
 (confirm that (progn (select-and-move-to-front! (lambda (o) (eql? o 8)) lst) lst) returns '(8 9 2 4 1 5 3 7 6))
 (confirm that (progn (select-and-move-to-front! (lambda (o) (eql? o 7)) lst) lst) returns '(7 8 9 2 4 1 5 3 6))
@@ -71,13 +71,13 @@
 (confirm that (nthcdr 0 lst) returns '(1 2 3 4 5 6 7 8 9))
 (confirm that (nthcdr 1 lst) returns '(2 3 4 5 6 7 8 9))
 (confirm that (nthcdr 2 lst) returns '(3 4 5 6 7 8 9))
-(setq! c (curry1 nthcdr 3))
+(setq c (curry1 nthcdr 3))
 (confirm that (c lst) returns '(4 5 6 7 8 9))
 (confirm that (pop lst)      returns 1)
 (confirm that lst            returns '(2 3 4 5 6 7 8 9))
 (confirm that (push 99 lst)  returns '(99 2 3 4 5 6 7 8 9))
-(setq! lst '("asdw" "erer" "rerw" 1 nil (lambda (x) x) zoop z (1 2 . 3) 8))
-(setq! split (split-list string? lst))
+(setq lst '("asdw" "erer" "rerw" 1 nil (lambda (x) x) zoop z (1 2 . 3) 8))
+(setq split (split-list string? lst))
 (confirm that (cadr split) returns '(1 nil (lambda (x) x) zoop z (1 2 . 3) 8))
 
 (ignore
@@ -113,7 +113,7 @@
 (confirm that (zip  '((a b c) (1 2 3) (x y z)))       returns '((a 1 x) (b 2 y) (c 3 z)))
 (confirm that (zip* '(1 2 3 4) '(a b c) '(x y z p q)) returns '((1 a x) (2 b y) (3 c z)))
 
-(setq!   lst                                               '(7 5 4 6 3 9 1 6 2 8 6))
+(setq   lst                                               '(7 5 4 6 3 9 1 6 2 8 6))
 (confirm that (sort!! lst <)                       returns '(1 2 3 4 5 6 6 6 7 8 9))
 
 (confirm that ((reduced   +  ) $(1 2 3 4))         returns 10)
@@ -129,15 +129,15 @@
 (confirm that ((rreduced* +  )   1 2 3 4 )         returns 10)
 (confirm that ((rreduced* + 4)   1 2 3   )         returns 10)
 
-(setq! lst '(1 nil 2 3 nil 4 nil))
+(setq lst '(1 nil 2 3 nil 4 nil))
 (confirm that (delq! nil lst)                      returns '(1 2 3 4))
-(setq! lst '(1 2 3 2 4 2 5))
+(setq lst '(1 2 3 2 4 2 5))
 (confirm that (delq! 2 lst)                        returns '(1 2 3 2 4 2 5))
-(setq! lst '(1 2 3 2 4 2 5))
+(setq lst '(1 2 3 2 4 2 5))
 (confirm that (delql! 2 lst)                       returns '(1 3 4 5))
-(setq! lst '(1 2 3 2 4 2 5))
+(setq lst '(1 2 3 2 4 2 5))
 (confirm that (delql! 1 lst)                       returns '(2 3 2 4 2 5))
-(setq! lst '((1 2) (3 4) (5 6)))
+(setq lst '((1 2) (3 4) (5 6)))
 (defun filter-evens (lst) (delq! nil (mapcar (lambda (x) (when (even? x) x)) lst)))
 (confirm that (mapcan filter-evens lst) returns '(2 4 6))
 (confirm that (apply concat (intercalate " " '("These"  "are" "words")))  returns "These are words")
@@ -151,12 +151,12 @@
 (confirm that (prime? 7)                                                  returns t)
 (confirm that (prime? 8)                                                  returns nil)
 (confirm that (primes 8)                                                  returns '(2 3 5 7 11 13 17 19))
-(setq! lst '(1 2 3 4))
+(setq lst '(1 2 3 4))
 (confirm that (mapcar! double lst)                                        returns '(2 4 6 8))
 (confirm that lst                                                         returns '(2 4 6 8))
 (confirm that (subst '(1 2 3 4 5 6 7 (8 5 9 5 10)) 5 'five)               returns '(1 2 3 4 five 6 7 (8 five 9 five 10)))
 (confirm that (transform-tree even? double '(1 2 3 4 5 6 7 (8 5 9 5 10))) returns '(1 4 3 8 5 12 7 (16 5 9 5 20)))
-(setq!   lst  '(a 2 3 (b 4 x (y 5)) (6 nil 7)))
+(setq   lst  '(a 2 3 (b 4 x (y 5)) (6 nil 7)))
 (confirm that (transform-tree  integer? double lst)                       returns '(a 4 6 (b 8 x (y 10)) (12 nil 14)))
 (confirm that lst                                                         returns '(a 2 3 (b 4 x (y  5)) (6  nil  7)))
 (confirm that (transform-tree! integer? double lst)                       returns '(a 4 6 (b 8 x (y 10)) (12 nil 14)))
@@ -167,7 +167,7 @@
 (confirm that (mapconcat id '("hello" "to" "the" "world") " ")            returns "hello to the world")
 (confirm that (mapconcat id '("hello" "to" "the" "world"))                returns "hellototheworld")
 (confirm that (sort!! '(1 2 3 4 5 6) (lambda (x y) (and (even? x) (not (even? y))))) returns '(6 4 2 5 3 1))
-(setq!   lst  (sort!! '(1 2 3 4 5 6) (lambda (x y) (and (even? x) (not (even? y))))))
+(setq   lst  (sort!! '(1 2 3 4 5 6) (lambda (x y) (and (even? x) (not (even? y))))))
 (confirm that (filter even? lst) returns '(6 4 2))
 (confirm that (funcall + 1 2 3 4 5 6) returns 21)
 (confirm that (apply concat (mapcar* (curry1 ljust 20) "hello" "to" "the" "world"))
@@ -175,7 +175,7 @@
 (confirm that (prog1 1 2 3) returns 1)
 (confirm that (prog2 1 2 3) returns 2)
 
-(confirm that (setq! pl '(a 10 b 20 c 30 d 40 e 50))   returns '(a 10 b 20 c 30 d 40  e 50))
+(confirm that (setq pl '(a 10 b 20 c 30 d 40 e 50))   returns '(a 10 b 20 c 30 d 40  e 50))
 
 (confirm that (plist-set  pl 'a 1)                     returns '(a 1  b 20 c 30 d 40  e 50))
 (confirm that pl                                       returns '(a 10 b 20 c 30 d 40  e 50))
@@ -356,9 +356,9 @@
 (confirm that (plist-to-alist (alist-to-plist '((a . 1) (b . 2) (c . 3)))) returns '((a . 1) (b . 2) (c . 3)))
 (confirm that (concat* :this " sentence " 'contains " " 5 " words.")       returns ":this sentence contains 5 words.")
 (confirm that (concat* :this " sentence " 'contains " a list: " '(1 2 3))  returns ":this sentence contains a list: (1 2 3)")
-(setq! l1 '(1 2 3 4 5 6 7 8 9 10))
-(setq! l2 '(a b c d e f g h i j))
-(setq! l3 '(q r s t u v w x y z))
+(setq l1 '(1 2 3 4 5 6 7 8 9 10))
+(setq l2 '(a b c d e f g h i j))
+(setq l3 '(q r s t u v w x y z))
 (confirm that (heads (list l1 l2 l3)) returns '(1 a q))
 (confirm that (tails (list l1 l2 l3)) returns '((2 3 4 5 6 7 8 9 10) (b c d e f g h i j) (r s t u v w x y z)))
 (confirm that (copy-tree '(1 2 (3 (4 5)) (6 7) 8 9)) returns '(1 2 (3 (4 5)) (6 7) 8 9))
@@ -366,7 +366,7 @@
 ;; matrix tests:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq! matrix (make-matrix 6 6 0))
+(setq matrix (make-matrix 6 6 0))
 
 (matrix-set! matrix 0 3 3)
 (matrix-set! matrix 0 5 5)
@@ -396,8 +396,8 @@
    (8 5 2)
    (9 6 3)))
 
-(setq! matrix (make-matrix 6 6 0))
-(setq! ctr 5)
+(setq matrix (make-matrix 6 6 0))
+(setq ctr 5)
 
 (until (zero? ctr)
  (matrix-set! matrix ctr ctr (* 100 ctr))
@@ -445,13 +445,13 @@
 ;;(log-eval t)
 (write-matrix matrix 3) (nl)
 
-(setq! l '(a 1 b 2 c 3 d 4 e 5))
+(setq l '(a 1 b 2 c 3 d 4 e 5))
 (confirm that (removeql! 'c l) returns '(a 1 b 2 3 d 4 e 5))
 (confirm that (removeql! 'e l) returns '(a 1 b 2 3 d 4 5))
 (confirm that (removeql! 'a l) returns '(1 b 2 3 d 4 5))
 (confirm that l                returns '(1 b 2 3 d 4 5))
 
-(setq! l '(a 1 b 2 c 3 d 4 e 5))
+(setq l '(a 1 b 2 c 3 d 4 e 5))
 (confirm that (removeq!  'c l) returns '(a 1 b 2 3 d 4 e 5))
 (confirm that (removeq!  'e l) returns '(a 1 b 2 3 d 4 5))
 (confirm that (removeq!  'a l) returns '(1 b 2 3 d 4 5))
