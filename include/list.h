403b5761f8cca7250878c2a88974a6137e3c8d63
Ariane Emory <ariane.emory@protonmail.com>
2023-11-07 12:10:46 -0500

    ...

--- include/list.h
+++ include/list.h
@@ -72,12 +72,13 @@ typedef struct ae_obj_t * (*ae_list_map_fun )(const struct ae_obj_t * const);
 ae_obj_t *    ae_list_cons         (      ae_obj_t *  const head,  ae_obj_t *  const  tail                            );
 ae_obj_t *    ae_list_intern_string(      ae_obj_t ** const plist, const char * const string                          );
 ae_obj_t *    ae_list_join3        (      ae_obj_t *        front, ae_obj_t * const middle, ae_obj_t * const  back    );
-ae_obj_t *    ae_list_pop          (      ae_obj_t ** const plist                                                     );
-ae_obj_t *    ae_list_push         (      ae_obj_t ** const plist, ae_obj_t *  const  member                          );
-ae_obj_t *    ae_list_push_back    (      ae_obj_t ** const plist, ae_obj_t *  const  member                          );
 bool          ae_list_has_member   (const ae_obj_t *  const list,  ae_obj_t *  const  member                          );
 bool          ae_list_is_proper    (const ae_obj_t *  const list                                                      );
 int           ae_list_length       (const ae_obj_t *  const list                                                      );
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ae_obj_t *    ae_list_pop          (      ae_obj_t ** const plist                                                     );
+ae_obj_t *    ae_list_push         (      ae_obj_t ** const plist, ae_obj_t *  const  member                          );
+ae_obj_t *    ae_list_push_back    (      ae_obj_t ** const plist, ae_obj_t *  const  member                          );
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifdef AE_LIST_REMOVE
 ae_obj_t *    ae_list_remove_member(      ae_obj_t *  const list,  ae_obj_t *  const  member                          );
