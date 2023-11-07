38c72a37d581f1f08c4411e67d3d7a053c60178f
Ariane Emory <ariane.emory@protonmail.com>
2023-11-07 12:58:26 -0500

    wip moving read from fd

--- include/sys_funcs.h
+++ include/sys_funcs.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <stddef.h>
+
+char * read_from_fd(int fd, size_t * const size);
--- src/sys_funcs.c
+++ src/sys_funcs.c
@@ -1,1 +1,43 @@
+#include <stdbool.h>
+
 #include "sys_funcs.h"
+
+#define BUFFER_SIZE 4096
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// read_from_fd
+////////////////////////////////////////////////////////////////////////////////////////////////////
+char * read_from_fd(int fd, size_t * const size) {
+  char buffer[BUFFER_SIZE];
+  char * output     = NULL;
+  size_t total_read = 0;
+
+  while (true) {
+    int bytes_read = read(fd, buffer, sizeof(buffer) - 1); // -1 to leave space for null terminator
+    
+    if (bytes_read <= 0) // Nothing more to read or an error occurred
+      break;
+
+    buffer[bytes_read] = '\0'; // Null-terminate the buffer
+
+    char * const new_output = free_list_malloc(total_read + bytes_read + 1); // +1 for null terminator
+    
+    if (output) {
+      memcpy(new_output, output, total_read);
+      free_list_free(output);
+    }
+
+    memcpy(new_output + total_read, buffer, bytes_read);
+    output      = new_output;
+    total_read += bytes_read;
+  }
+
+  if (output) {
+    output[total_read] = '\0'; // Ensure null-termination
+  }
+
+  *size = total_read;
+  
+  return output;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
