4adb413a3fe830f907cda64745bfb64c81d5a138
Ariane Emory <ariane.emory@protonmail.com>
2023-11-07 17:37:35 -0500

    ...

--- src/core/core_read.c
+++ src/core/core_read.c
@@ -15,7 +15,11 @@ ae_obj_t * ae_core_read(ae_obj_t * const env, ae_obj_t * const args, __attribute
 
   REQUIRE(env, args, ! read_error, "Could not read");
   REQUIRE(env, args, NILP(CDDR(program)), "Muliple s-exps in input");
+
+  RETURN(CADR(program));
   
-  CORE_RETURN("read", CADR(program));
+end:
+  
+  CORE_RETURN("read");
 }
 
