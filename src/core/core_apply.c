99bd7f9cfc5ae796a53f8f7cb0b89274b944eb13
Ariane Emory <ariane.emory@protonmail.com>
2023-11-07 20:06:25 -0500

    ...

--- src/core/core_apply.c
+++ src/core/core_apply.c
@@ -28,7 +28,7 @@ ae_obj_t * ae_core_apply(ae_obj_t * const env,
   ae_obj_t *       pos           = CDR(args);
   ae_obj_t *       evaluated_arg = NIL;
   
-  while (!NILP(CDR(pos))) {
+  UNTIL_NIL(CDR(pos)) {
     ae_obj_t * arg = CAR(pos);
 
     if (is_quote_form(arg))
